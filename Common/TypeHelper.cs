e930643f89d6b45bcc7ac80d4517e3cf4a8285cc
mikhail <mikhail.mitkevich@gmail.com>
2013-01-29 20:24:36 +0400

    баг с GetEntryAssembly()==null при логах в юнит тестах

--- Common/TypeHelper.cs
+++ Common/TypeHelper.cs
@@ -135,6 +135,8 @@ namespace Ecng.Common
 		private static readonly Lazy<string> _applicationName = new Lazy<string>(() =>
 		{
 			var asm = Assembly.GetEntryAssembly();
+			if (asm == null)
+				return "None";
 			var attr = asm.GetAttribute<AssemblyProductAttribute>();
 			return attr != null ? attr.Product : asm.GetName().Name;
 		});
