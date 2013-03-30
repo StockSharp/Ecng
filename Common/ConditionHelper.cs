a878dd4a71a607a21d1cdeaf7c9dd932d2ae6a40
devlanfear <devlanfear@gmail.com>
2013-03-30 13:40:51 +0400

    refactor client

---
+++ Common/ConditionHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ecng.Common
+{
+	public class ConditionHelper
+	{
+		public static void CheckNotNullOrEmpty(string value, string name)
+		{
+			if(value.IsEmpty())
+				throw new ArgumentNullException("name");
+		}
+	}
+}
