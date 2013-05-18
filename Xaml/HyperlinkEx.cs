c10b1c3ab337bc59b92c1affbc4289aa1b07b2bc
Mika Soukhov <mika@soukhov.com>
2013-05-18 08:38:35 +0400

    ServerCredentials. Убрал Instance

---
+++ Xaml/HyperlinkEx.cs
@@ -0,0 +1,20 @@
+﻿namespace Ecng.Xaml
+{
+	using System.Diagnostics;
+	using System.Windows.Documents;
+	using System.Windows.Navigation;
+
+	public class HyperlinkEx : Hyperlink
+	{
+		public HyperlinkEx()
+		{
+			RequestNavigate += Hyperlink_OnRequestNavigate;
+		}
+
+		private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+		{
+			Process.Start(new ProcessStartInfo(NavigateUri.ToString()));
+			e.Handled = true;
+		}
+	}
+}
