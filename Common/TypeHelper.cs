25735d01f07daedefae4f62b0ce616c168a4fb31
esper <a-belich@yandex.ru>
2013-08-23 17:33:04 +0400

    Studio. PartDiagramElement.

--- Common/TypeHelper.cs
+++ Common/TypeHelper.cs
@@ -72,6 +72,27 @@ namespace Ecng.Common
 					);
 		}
 
+		public static bool IsNumeric(this Type type)
+		{
+			switch (Type.GetTypeCode(type))
+			{
+				case TypeCode.Byte:
+				case TypeCode.SByte:
+				case TypeCode.UInt16:
+				case TypeCode.UInt32:
+				case TypeCode.UInt64:
+				case TypeCode.Int16:
+				case TypeCode.Int32:
+				case TypeCode.Int64:
+				case TypeCode.Decimal:
+				case TypeCode.Double:
+				case TypeCode.Single:
+					return true;
+				default:
+					return false;
+			}
+		}
+
 		public static string GetTypeAsString(this Type type)
 		{
 			if (type == null)
