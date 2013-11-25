3f05f5f98e7a4fc66109b175aa9ae390aa7a7e69
esper <a-belich@yandex.ru>
2013-11-25 13:12:35 +0400

    Ecng.Collections BlockingDeque.

---
+++ Collections/BaseBlockingQueue.cs
@@ -0,0 +1,233 @@
+﻿namespace Ecng.Collections
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Threading;
+
+	using Ecng.Common;
+
+	// http://stackoverflow.com/questions/530211/creating-a-blocking-queuet-in-net
+	public abstract class BaseBlockingQueue<T, TF> : ISynchronizedCollection<T>
+		where TF : ICollection<T>
+	{
+		protected BaseBlockingQueue(TF innerCollection)
+		{
+			InnerCollection = innerCollection;
+		}
+
+		protected TF InnerCollection { get; private set; }
+
+		// -1 is unlimited
+		private int _maxSize = -1;
+
+		public int MaxSize
+		{
+			get { return _maxSize; }
+			set
+			{
+				if (value == 0 || value < -1)
+					throw new ArgumentOutOfRangeException("value");
+
+				_maxSize = value;
+			}
+		}
+
+		private readonly SyncObject _syncRoot = new SyncObject();
+
+		public SyncObject SyncRoot
+		{
+			get { return _syncRoot; }
+		}
+
+		public int Count
+		{
+			get { return InnerCollection.Count; }
+		}
+
+		private bool _isClosed;
+
+		public bool IsClosed { get { return _isClosed; } }
+
+		public void Close()
+		{
+			lock (SyncRoot)
+			{
+				_isClosed = true;
+				Monitor.PulseAll(_syncRoot);
+			}
+		}
+
+		public void Open()
+		{
+			lock (SyncRoot)
+			{
+				_isClosed = false;
+			}
+		}
+
+		private void WaitWhileFull()
+		{
+			while (InnerCollection.Count >= _maxSize && !_isClosed)
+			{
+				Monitor.Wait(_syncRoot);
+			}
+		}
+
+		public void WaitUntilEmpty()
+		{
+			lock (_syncRoot)
+			{
+				while (InnerCollection.Count > 0 && !_isClosed)
+					Monitor.Wait(_syncRoot);
+			}
+		}
+
+		public void Enqueue(T item, bool force = false)
+		{
+			lock (_syncRoot)
+			{
+				if (_isClosed)
+					return;
+
+				if (!force && _maxSize != -1)
+				{
+					if (InnerCollection.Count >= _maxSize)
+						WaitWhileFull();
+				}
+
+				OnEnqueue(item, force);
+
+				if (InnerCollection.Count == 1)
+				{
+					// wake up any blocked dequeue
+					Monitor.PulseAll(_syncRoot);
+				}
+			}
+		}
+
+		protected abstract void OnEnqueue(T item, bool force);
+		protected abstract T OnDequeue();
+		protected abstract T OnPeek();
+
+		public T Dequeue()
+		{
+			T retVal;
+			TryDequeue(out retVal, false);
+			return retVal;
+		}
+
+		public bool TryDequeue(out T value)
+		{
+			return TryDequeue(out value, true);
+		}
+
+		private bool WaitWhileEmpty(bool exitOnClose)
+		{
+			while (InnerCollection.Count == 0)
+			{
+				if (exitOnClose && _isClosed)
+					return false;
+
+				Monitor.Wait(_syncRoot);
+			}
+
+			return true;
+		}
+
+		public bool TryDequeue(out T value, bool exitOnClose)
+		{
+			lock (_syncRoot)
+			{
+				if (!WaitWhileEmpty(exitOnClose))
+				{
+					value = default(T);
+					return false;
+				}
+
+				value = OnDequeue();
+
+				if (InnerCollection.Count == (_maxSize - 1) || InnerCollection.Count == 0)
+				{
+					// wake up any blocked enqueue
+					Monitor.PulseAll(_syncRoot);
+				}
+
+				return true;
+			}
+		}
+
+		public T Peek()
+		{
+			T retVal;
+			TryPeek(out retVal, false);
+			return retVal;
+		}
+
+		public bool TryPeek(out T value)
+		{
+			return TryPeek(out value, true);
+		}
+
+		private bool TryPeek(out T value, bool exitOnClose)
+		{
+			lock (SyncRoot)
+			{
+				if (!WaitWhileEmpty(exitOnClose))
+				{
+					value = default(T);
+					return false;
+				}
+
+				value = OnPeek();
+
+				return true;
+			}
+		}
+
+		public void Clear()
+		{
+			lock (SyncRoot)
+			{
+				InnerCollection.Clear();
+				Monitor.PulseAll(SyncRoot);
+			}
+		}
+
+		bool ICollection<T>.Remove(T item)
+		{
+			throw new NotSupportedException();
+		}
+
+		bool ICollection<T>.IsReadOnly
+		{
+			get { return false; }
+		}
+
+		void ICollection<T>.Add(T item)
+		{
+			Enqueue(item);
+		}
+
+		bool ICollection<T>.Contains(T item)
+		{
+			return InnerCollection.Contains(item);
+		}
+
+		void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+		{
+			lock (SyncRoot)
+				InnerCollection.CopyTo(array, arrayIndex);
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return InnerCollection.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
--- Collections/BlockingQueue.cs
+++ Collections/BlockingQueue.cs
@@ -1,223 +1,53 @@
 ﻿namespace Ecng.Collections
 {
-	using System;
-	using System.Collections;
-	using System.Collections.Generic;
-	using System.Threading;
+	using Wintellect.PowerCollections;
 
-	using Ecng.Common;
-
-	// http://stackoverflow.com/questions/530211/creating-a-blocking-queuet-in-net
-	public sealed class BlockingQueue<T> : ISynchronizedCollection<T>
+	public sealed class BlockingQueue<T> : BaseBlockingQueue<T, QueueEx<T>>
 	{
-		private readonly Queue<T> _queue = new Queue<T>();
-
-		// -1 is unlimited
-		private int _maxSize = -1;
-
-		public int MaxSize
+		public BlockingQueue()
+			: base(new QueueEx<T>())
 		{
-			get { return _maxSize; }
-			set
-			{
-				if (value == 0 || value < -1)
-					throw new ArgumentOutOfRangeException("value");
-
-				_maxSize = value;
-			}
 		}
 
-		private readonly SyncObject _syncRoot = new SyncObject();
-
-		public SyncObject SyncRoot
+		protected override void OnEnqueue(T item, bool force)
 		{
-			get { return _syncRoot; }
+			InnerCollection.Enqueue(item);
 		}
 
-		public int Count
+		protected override T OnDequeue()
 		{
-			get { return _queue.Count; }
+			return InnerCollection.Dequeue();
 		}
 
-		private bool _isClosed;
-
-		public bool IsClosed { get { return _isClosed; } }
-
-		public void Close()
+		protected override T OnPeek()
 		{
-			lock (SyncRoot)
-			{
-				_isClosed = true;
-				Monitor.PulseAll(_syncRoot);
-			}
+			return InnerCollection.Peek();
+		}
+	}
+
+	public sealed class BlockingDeque<T> : BaseBlockingQueue<T, Deque<T>>
+	{
+		public BlockingDeque()
+			: base(new Deque<T>())
+		{
 		}
 
-		public void Open()
+		protected override void OnEnqueue(T item, bool force)
 		{
-			lock (SyncRoot)
-			{
-				_isClosed = false;
-			}
+			if (force)
+				InnerCollection.AddToFront(item);
+			else
+				InnerCollection.AddToBack(item);
 		}
 
-		private void WaitWhileFull()
+		protected override T OnDequeue()
 		{
-			while (_queue.Count >= _maxSize && !_isClosed)
-			{
-				Monitor.Wait(_syncRoot);
-			}
+			return InnerCollection.RemoveFromFront();
 		}
 
-		public void WaitUntilEmpty()
+		protected override T OnPeek()
 		{
-			lock (_syncRoot)
-			{
-				while (_queue.Count > 0 && !_isClosed)
-					Monitor.Wait(_syncRoot);
-			}
-		}
-
-		public void Enqueue(T item, bool force = false)
-		{
-			lock (_syncRoot)
-			{
-				if (_isClosed)
-					return;
-
-				if (!force && _maxSize != -1)
-				{
-					if (_queue.Count >= _maxSize)
-						WaitWhileFull();
-				}
-
-				_queue.Enqueue(item);
-
-				if (_queue.Count == 1)
-				{
-					// wake up any blocked dequeue
-					Monitor.PulseAll(_syncRoot);
-				}
-			}
-		}
-
-		public T Dequeue()
-		{
-			T retVal;
-			TryDequeue(out retVal, false);
-			return retVal;
-		}
-
-		public bool TryDequeue(out T value)
-		{
-			return TryDequeue(out value, true);
-		}
-
-		private bool WaitWhileEmpty(bool exitOnClose)
-		{
-			while (_queue.Count == 0)
-			{
-				if (exitOnClose && _isClosed)
-					return false;
-
-				Monitor.Wait(_syncRoot);
-			}
-
-			return true;
-		}
-
-		public bool TryDequeue(out T value, bool exitOnClose)
-		{
-			lock (_syncRoot)
-			{
-				if (!WaitWhileEmpty(exitOnClose))
-				{
-					value = default(T);
-					return false;
-				}
-
-				value = _queue.Dequeue();
-
-				if (_queue.Count == (_maxSize - 1) || _queue.Count == 0)
-				{
-					// wake up any blocked enqueue
-					Monitor.PulseAll(_syncRoot);
-				}
-
-				return true;
-			}
-		}
-
-		public T Peek()
-		{
-			T retVal;
-			TryPeek(out retVal, false);
-			return retVal;
-		}
-
-		public bool TryPeek(out T value)
-		{
-			return TryPeek(out value, true);
-		}
-
-		private bool TryPeek(out T value, bool exitOnClose)
-		{
-			lock (SyncRoot)
-			{
-				if (!WaitWhileEmpty(exitOnClose))
-				{
-					value = default(T);
-					return false;
-				}
-
-				value = _queue.Peek();
-
-				return true;
-			}
-		}
-
-		public void Clear()
-		{
-			lock (SyncRoot)
-			{
-				_queue.Clear();
-				Monitor.PulseAll(SyncRoot);
-			}
-		}
-
-		bool ICollection<T>.Remove(T item)
-		{
-			throw new NotSupportedException();
-		}
-
-		bool ICollection<T>.IsReadOnly
-		{
-			get { return false; }
-		}
-
-		void ICollection<T>.Add(T item)
-		{
-			Enqueue(item);
-		}
-
-		bool ICollection<T>.Contains(T item)
-		{
-			return _queue.Contains(item);
-		}
-
-		void ICollection<T>.CopyTo(T[] array, int arrayIndex)
-		{
-			lock (SyncRoot)
-				_queue.CopyTo(array, arrayIndex);
-		}
-
-		public IEnumerator<T> GetEnumerator()
-		{
-			return _queue.GetEnumerator();
-		}
-
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
+			return InnerCollection.GetAtFront();
 		}
 	}
 }
