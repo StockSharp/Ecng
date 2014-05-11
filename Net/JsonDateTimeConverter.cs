2b839609b84ec0ab3c5498a77c507bf4159a8620
Mika Soukhov <mika@soukhov.com>
2014-05-11 18:31:59 +0400

    BitStamp. Initial commit.

---
+++ Net/JsonDateTimeConverter.cs
@@ -0,0 +1,36 @@
+namespace Ecng.Net
+{
+	using System;
+
+	using Ecng.Common;
+
+	using Newtonsoft.Json;
+
+	public class JsonDateTimeConverter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return typeof(DateTime) == objectType;
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			//if (reader.TokenType != JsonToken.String)
+			//	throw new JsonReaderException("Unexcepted token '{0}'.".Put(reader.TokenType));
+
+			try
+			{
+				return Converter.GregorianStart.AddSeconds(reader.Value.To<long>());
+			}
+			catch (Exception ex)
+			{
+				throw new JsonReaderException(ex.Message, ex);
+			}
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
