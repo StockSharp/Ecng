deb487c6b578bd9fefbbc2762a400b311b616d4a
Sergey Sokolov <sokolovsa@gmail.com>
2014-05-21 20:17:46 +0400

    ExchangeEditorPanel

---
+++ Xaml/DelegateCommand.cs
@@ -0,0 +1,36 @@
+﻿namespace Ecng.Xaml
+{
+	using System;
+	using System.Windows.Input;
+
+	public class DelegateCommand : ICommand
+	{
+		private readonly Predicate<object> _canExecute;
+		private readonly Action<object> _execute;
+
+		public DelegateCommand(Action<object> execute, Predicate<object> canExecute = null)
+		{
+			if (execute == null)
+				throw new ArgumentNullException("execute");
+
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public void Execute(object parameter)
+		{
+			_execute(parameter);
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return _canExecute == null || _canExecute(parameter);
+		}
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+	}
+}
---
+++ Xaml/ViewModelBase.cs
@@ -0,0 +1,61 @@
+﻿namespace Ecng.Xaml
+{
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Linq.Expressions;
+	using Ecng.Common;
+
+	/// <summary>
+	/// Базовый класс для реализации View-Model в модели MVVM
+	/// </summary>
+	public abstract class ViewModelBase : Disposable, INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string name)
+		{
+			var eventHandler = PropertyChanged;
+			if (eventHandler != null) eventHandler(this, new PropertyChangedEventArgs(name));
+		}
+
+		protected void OnPropertyChanged<T>(Expression<Func<T>> selectorExpression)
+		{
+			OnPropertyChanged(ViewModelBase.PropertyName(selectorExpression));
+		}
+
+		protected bool SetField<T>(ref T field, T value, Expression<Func<T>> selectorExpression)
+		{
+			return SetField(ref field, value, ViewModelBase.PropertyName(selectorExpression));
+		}
+
+		// установка требуемого поля в определенное значение и вызов события PropertyChanged при необходимости
+		protected virtual bool SetField<T>(ref T field, T value, string name)
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+			field = value;
+			OnPropertyChanged(name);
+
+			return true;
+		}
+
+		public static string PropertyName<T>(Expression<Func<T>> property)
+		{
+			var lambda = (LambdaExpression) property;
+
+			MemberExpression memberExpression;
+			if (lambda.Body is UnaryExpression)
+			{
+				var unaryExpression = (UnaryExpression) lambda.Body;
+				memberExpression = (MemberExpression) unaryExpression.Operand;
+			}
+			else
+			{
+				memberExpression = (MemberExpression) lambda.Body;
+			}
+
+			return memberExpression.Member.Name;
+		}
+	}
+}
