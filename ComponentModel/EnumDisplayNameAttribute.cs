c074ca852ddd386d33039eb93c5723e39f13ef72
Mika Soukhov <mika@soukhov.com>
2014-06-14 23:41:33 +0400

    Extensions GetDisplayName + GetDescription

--- ComponentModel/EnumDisplayNameAttribute.cs
+++ ComponentModel/EnumDisplayNameAttribute.cs
@@ -3,8 +3,6 @@
 	using System;
 	using System.ComponentModel;
 
-	using Ecng.Common;
-
 #if SILVERLIGHT
 	[AttributeUsage(AttributeTargets.Event | AttributeTargets.Property | AttributeTargets.Method | AttributeTargets.Class)]
 	public class DisplayNameAttribute : Attribute
@@ -32,31 +30,6 @@
 		public EnumDisplayNameAttribute(string displayName)
 			: base(displayName)
 		{
-		}
-	}
-
-	public static class EnumDisplayNameAttributeExtensions
-	{
-		public static string GetDisplayName(this object field)
-		{
-			if (field == null)
-				throw new ArgumentNullException("field");
-
-			if (!(field is Enum))
-				throw new ArgumentException("field");
-
-			var fieldInfo = field.GetType().GetField(field.ToString());
-
-			if (fieldInfo == null)
-				throw new ArgumentException(field.ToString(), "field");
-
-			var name = fieldInfo.Name;
-
-			var attr = fieldInfo.GetAttribute<EnumDisplayNameAttribute>();
-			if (attr != null && !attr.DisplayName.IsEmpty())
-				name = attr.DisplayName;
-
-			return name;
 		}
 	}
 }
---
+++ ComponentModel/Extensions.cs
@@ -0,0 +1,44 @@
+namespace Ecng.ComponentModel
+{
+	using System;
+	using System.ComponentModel;
+
+	using Ecng.Common;
+
+	public static class Extensions
+	{
+		public static string GetDisplayName(this Type type, string defaultValue = null)
+		{
+			var attr = type.GetAttribute<DisplayNameAttribute>();
+			return attr == null ? defaultValue ?? type.Name : attr.DisplayName;
+		}
+
+		public static string GetDescription(this Type type, string defaultValue = null)
+		{
+			var attr = type.GetAttribute<DescriptionAttribute>();
+			return attr == null ? defaultValue : attr.Description;
+		}
+
+		public static string GetDisplayName(this object field)
+		{
+			if (field == null)
+				throw new ArgumentNullException("field");
+
+			if (!(field is Enum))
+				throw new ArgumentException("field");
+
+			var fieldInfo = field.GetType().GetField(field.ToString());
+
+			if (fieldInfo == null)
+				throw new ArgumentException(field.ToString(), "field");
+
+			var name = fieldInfo.Name;
+
+			var attr = fieldInfo.GetAttribute<EnumDisplayNameAttribute>();
+			if (attr != null && !attr.DisplayName.IsEmpty())
+				name = attr.DisplayName;
+
+			return name;
+		}
+	}
+}
