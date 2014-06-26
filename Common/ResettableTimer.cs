64aaaeb40387d70208e099893305a9349bb5b2df
esper <a-belich@yandex.ru>
2014-06-26 09:50:39 +0400

    ResettableTimer flush fix.

--- Common/ResettableTimer.cs
+++ Common/ResettableTimer.cs
@@ -1,7 +1,7 @@
 ﻿namespace Ecng.Common
 {
 	using System;
-	using System.Timers;
+	using System.Threading;
 
 	public class ResettableTimer
 	{
@@ -21,9 +21,20 @@
 		{
 			if (_timer == null)
 			{
-				_timer = new Timer(_period.TotalMilliseconds);
-				_timer.Elapsed += OnTimerElapsed;
-				_timer.Start();
+				_timer = ThreadingHelper
+					.Timer(() =>
+					{
+						if (!_changed)
+						{
+							_timer.Dispose();
+							_timer = null;
+
+							Elapsed.SafeInvoke();
+						}
+						else
+							_changed = false;
+					})
+					.Interval(_period);
 			}
 			else
 				_changed = true;
@@ -34,21 +45,9 @@
 			if (_timer == null)
 				return;
 
-			_timer.Stop();
-			_timer.Elapsed -= OnTimerElapsed;
-			_timer = null;
+			_changed = false;
 
-			Elapsed.SafeInvoke();
-		}
-
-		private void OnTimerElapsed(object sender, ElapsedEventArgs e)
-		{
-			if (!_changed)
-			{
-				Flush();
-			}
-			else
-				_changed = false;
+			_timer.Change(TimeSpan.Zero, _period);
 		}
 	}
 }
