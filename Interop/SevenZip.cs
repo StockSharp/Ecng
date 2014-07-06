62de3a2869a9e8d72509e166e5838a29102b6311
Mika Soukhov <mika@soukhov.com>
2014-07-06 16:44:27 +0400

    Ecng.Interop.SevenZip

---
+++ Interop/SevenZip.cs
@@ -0,0 +1,85 @@
+﻿namespace Ecng.Interop
+{
+	using System;
+	using System.IO;
+
+	using Ecng.Common;
+	using Ecng.Serialization;
+
+	using global::SevenZip;
+
+	public static class SevenZip
+	{
+		private static bool _isInitialized;
+
+		private static string _dllDir = Directory.GetCurrentDirectory();
+
+		public static string DllDir
+		{
+			get { return _dllDir; }
+			set
+			{
+				if (value.IsEmpty())
+					throw new ArgumentNullException("value");
+
+				_dllDir = value;
+
+				var fileName = Environment.Is64BitProcess ? "7z64.dll" : "7z.dll";
+				(Environment.Is64BitProcess ? Properties.Resources._7z64 : Properties.Resources._7z).Save(Path.Combine(value, fileName));
+				SevenZipBase.SetLibraryPath(fileName);
+
+				_isInitialized = true;
+			}
+		}
+
+		private static void Init()
+		{
+			if (_isInitialized)
+				return;
+
+			DllDir = Directory.GetCurrentDirectory();
+		}
+
+		public static byte[] Extract(byte[] from)
+		{
+			var to = new MemoryStream();
+			Extract(from.To<Stream>(), to);
+			return to.GetBuffer();
+		}
+
+		public static void Extract(Stream from, Stream to, string password = null)
+		{
+			if (from == null)
+				throw new ArgumentNullException("from");
+
+			if (to == null)
+				throw new ArgumentNullException("to");
+
+			Init();
+
+			using (var e = new SevenZipExtractor(from, password))
+				e.ExtractFile(0, to);
+		}
+
+		public static byte[] Compress(byte[] from)
+		{
+			var to = new MemoryStream();
+			Compress(from.To<Stream>(), to);
+			return to.GetBuffer();
+		}
+
+		public static void Compress(Stream from, Stream to, string password = null)
+		{
+			if (from == null)
+				throw new ArgumentNullException("from");
+
+			if (to == null)
+				throw new ArgumentNullException("to");
+
+			Init();
+
+			var e = new SevenZipCompressor();
+			e.CompressStream(from, to, password);
+		}
+	}
+}
