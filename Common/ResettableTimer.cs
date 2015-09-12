ef116ef7a7ab3d6c40a9553363b0c77852c7ff49
mika@stocksharp.com <mika@stocksharp.com>
2015-09-12 22:46:05 +0300

    ResettableTimer refactoring.

--- Common/ResettableTimer.cs
+++ Common/ResettableTimer.cs
@@ -1,69 +1,99 @@
 ﻿namespace Ecng.Common
 {
 	using System;
-	using System.Threading;
 
-	public class ResettableTimer
+	public class ResettableTimer : Disposable
 	{
 		private readonly SyncObject _sync = new SyncObject();
+		private readonly SyncObject _finish = new SyncObject();
+		private bool _isActivated;
+		private bool _isFinished = true;
+		private bool _isCancelled;
+
 		private readonly TimeSpan _period;
+		private readonly string _name;
 
-		private Timer _timer;
-		private bool _changed;
-
-		public event Action Elapsed;
-
-		public ResettableTimer(TimeSpan period)
+		public ResettableTimer(TimeSpan period, string name)
 		{
 			_period = period;
+			_name = name;
 		}
 
-		public void Reset()
+		public event Action<Func<bool>> Elapsed;
+
+		public void Activate()
 		{
 			lock (_sync)
 			{
-				if (_timer == null)
-				{
-					_timer = ThreadingHelper
-						.Timer(OnTimer)
-						.Interval(_period);
-				}
-				else
-					_changed = true;
+				_isActivated = true;
+
+				if (!_isFinished)
+					return;
+
+				_isFinished = false;
+				_isCancelled = false;
 			}
+
+			ThreadingHelper.Thread(() =>
+			{
+				try
+				{
+					while (!IsDisposed)
+					{
+						lock (_sync)
+						{
+							_isCancelled = false;
+
+							if (_isActivated)
+								_isActivated = false;
+							else
+							{
+								_isFinished = true;
+								break;
+							}
+						}
+
+						Elapsed.SafeInvoke(CanProcess);
+						_period.Sleep();
+					}
+				}
+				finally
+				{
+					_finish.PulseAll();
+				}
+			}).Name(_name).Launch();
 		}
 
-		private void OnTimer()
+		public void Cancel()
 		{
-			var elapsed = false;
-
 			lock (_sync)
 			{
-				if (!_changed)
-				{
-					_timer.Dispose();
-					_timer = null;
+				if (_isFinished)
+					return;
 
-					elapsed = true;
-				}
-				else
-					_changed = false;
+				_isActivated = false;
+				_isCancelled = true;
 			}
-
-			if (elapsed)
-				Elapsed.SafeInvoke();
 		}
 
 		public void Flush()
 		{
-			lock (_sync)
+			lock (_finish)
 			{
-				if (_timer == null)
-					return;
-
-				_changed = false;
-				_timer.Change(TimeSpan.Zero, _period);	
+				Activate();
+				_finish.Wait();	
 			}
+		}
+
+		private bool CanProcess()
+		{
+			return !_isCancelled && !IsDisposed;
+		}
+
+		protected override void DisposeManaged()
+		{
+			Cancel();
+			base.DisposeManaged();
 		}
 	}
 }
