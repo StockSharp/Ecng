629f5f6b44050bc41534e877e97130e3a99d7289
mika@stocksharp.com <mika@stocksharp.com>
2015-09-20 16:20:06 +0300

    TypeHelper.Throw

--- Common/TypeHelper.cs
+++ Common/TypeHelper.cs
@@ -237,5 +237,24 @@ namespace Ecng.Common
 		{
 			return type.IsValueType ? Activator.CreateInstance(type) : null;
 		}
+
+		// http://stackoverflow.com/questions/57383/in-c-how-can-i-rethrow-innerexception-without-losing-stack-trace
+		public static void Throw(this Exception ex)
+		{
+			if (ex == null)
+				throw new ArgumentNullException("ex");
+
+			var ctx = new StreamingContext(StreamingContextStates.CrossAppDomain);
+			var mgr = new ObjectManager(null, ctx);
+			var si = new SerializationInfo(ex.GetType(), new FormatterConverter());
+
+			ex.GetObjectData(si, ctx);
+			mgr.RegisterObject(ex, 1, si); // prepare for SetObjectData
+			mgr.DoFixups(); // ObjectManager calls SetObjectData
+
+			// voila, e is unmodified save for _remoteStackTraceString
+
+			throw ex;
+		}
 	}
 }
