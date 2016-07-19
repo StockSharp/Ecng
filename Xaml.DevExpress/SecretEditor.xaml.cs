baf2c965c2ba4be3c2c880ddd9e01abc3550c5a4
Belich Aleksandr <a-belich@yandex.ru>
2016-07-19 14:31:45 +0700

    SecretEditor added.

---
+++ Xaml.DevExpress/SecretEditor.xaml.cs
@@ -0,0 +1,51 @@
+﻿namespace Ecng.Xaml.DevExp
+{
+	using System.Security;
+	using System.Windows;
+
+	using DevExpress.Xpf.Editors;
+
+	using Ecng.Common;
+
+	public partial class SecretEdit
+	{
+		private const string _fakeMask = "5mmdfxfo56";
+
+		/// <summary>
+		/// <see cref="DependencyProperty"/> для <see cref="Secret"/>.
+		/// </summary>
+		public static readonly DependencyProperty SecretProperty =
+			DependencyProperty.Register("Secret", typeof(SecureString), typeof(SecretEdit), new PropertyMetadata(default(SecureString),
+				(o, args) =>
+				{
+					var picker = (SecretEdit)o;
+					var secret = (SecureString)args.NewValue;
+
+					if (picker.PasswordCtrl.Password.IsEmpty() && secret != null && secret.Length > 0)
+					{
+						// заполняем поле пароля звездочками
+						picker.PasswordCtrl.Password = _fakeMask;
+					}
+				}));
+
+		/// <summary>
+		/// Секрет.
+		/// </summary>
+		public SecureString Secret
+		{
+			get { return (SecureString)GetValue(SecretProperty); }
+			set { SetValue(SecretProperty, value); }
+		}
+
+		public SecretEdit()
+		{
+			InitializeComponent();
+		}
+
+		private void BaseEdit_OnEditValueChanged(object sender, EditValueChangedEventArgs e)
+		{
+			if (PasswordCtrl.Password != _fakeMask)
+				Secret = PasswordCtrl.Password.To<SecureString>();
+		}
+	}
+}
