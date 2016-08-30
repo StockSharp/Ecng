34d05698c862da63f20cd0de3e46369dce136b1b
mika@stocksharp.com <mika@stocksharp.com>
2016-08-30 03:38:06 +0300

    SitemapNode.ToString

--- Web/Sitemap/SitemapNode.cs
+++ Web/Sitemap/SitemapNode.cs
@@ -87,5 +87,16 @@
 		/// The URL of the page.
 		/// </value>
 		public string Url { get; }
+
+		/// <summary>
+		/// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+		/// </returns>
+		public override string ToString()
+		{
+			return Url;
+		}
 	}
 }
