834e56d4d9a9bd5a297d8c268d2c71a8b3901afe
mika@stocksharp.com <mika@stocksharp.com>
2016-09-14 00:44:58 +0300

    ResourseDictExtension

---
+++ Xaml/ResourseDictExtension.cs
@@ -0,0 +1,51 @@
+namespace Ecng.Xaml
+{
+	using System;
+	using System.Reflection;
+	using System.Windows;
+	using System.Windows.Markup;
+
+	public class ResourseDictExtension : MarkupExtension
+	{
+		public ResourseDictExtension()
+		{
+			
+		}
+
+		public ResourseDictExtension(object assemblyName, object path, object key)
+		{
+			AssemblyName = assemblyName;
+			Path = path;
+			Key = key;
+		}
+
+		[ConstructorArgument("assemblyName")]
+		public object AssemblyName { get; set; }
+
+		[ConstructorArgument("path")]
+		public object Path { get; set; }
+
+		[ConstructorArgument("key")]
+		public object Key { get; set; }
+
+		protected static ResourceDictionary InnerDict { get; private set; }
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			if (InnerDict == null)
+			{
+				var resName = AssemblyName + "." + ((string)Path).Replace('/', '.');
+
+				using (var s = Assembly.Load((string)AssemblyName).GetManifestResourceStream(resName))
+					InnerDict = (ResourceDictionary)XamlReader.Load(s);
+			}
+
+			return InnerDict[Key];
+		}
+
+		protected void ClearCache()
+		{
+			InnerDict = null;
+		}
+	}
+}
