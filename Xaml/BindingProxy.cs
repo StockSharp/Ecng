a8209a75b3420641b1fec54258b2a89556df7d31
Belich Aleksandr <a-belich@yandex.ru>
2016-09-21 11:56:55 +0700

    BindingProxy added.

---
+++ Xaml/BindingProxy.cs
@@ -0,0 +1,20 @@
+namespace Ecng.Xaml
+{
+	using System.Windows;
+
+	public class BindingProxy : Freezable
+	{
+		public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+
+		public object Data
+		{
+			get { return GetValue(DataProperty); }
+			set { SetValue(DataProperty, value); }
+		}
+
+		protected override Freezable CreateInstanceCore()
+		{
+			return new BindingProxy();
+		}
+	}
+}
