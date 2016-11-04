042c0ac0ed1c16f20bf8d675f4dec56eb52abec8
mika@stocksharp.com <mika@stocksharp.com>
2016-11-04 18:02:35 +0300

    BoolToImageConverter, BoolToStringConverter

---
+++ Xaml/Converters/BoolToStringConverter.cs
@@ -0,0 +1,23 @@
+namespace Ecng.Xaml.Converters
+{
+	using System;
+	using System.Globalization;
+	using System.Windows.Data;
+
+	public class BoolToStringConverter : IValueConverter
+	{
+		public string FalseValue { get; set; }
+
+		public string TrueValue { get; set; }
+
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (bool)value ? TrueValue : FalseValue;
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (string)value == TrueValue;
+		}
+	}
+}
