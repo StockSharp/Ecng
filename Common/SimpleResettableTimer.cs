c8a7d17fc445b91bbaa4f95ebd316c65124ce2df
Belich Aleksandr <a-belich@yandex.ru>
2016-11-24 20:25:17 +0700

    SimpleResettableTimer added.

---
+++ Common/SimpleResettableTimer.cs
@@ -0,0 +1,81 @@
+﻿namespace Ecng.Common
+{
+	using System;
+	using System.Threading;
+
+	public class SimpleResettableTimer
+	{
+		private readonly SyncObject _sync = new SyncObject();
+		private readonly TimeSpan _period;
+
+		private Timer _timer;
+		private bool _changed;
+
+		public event Action Elapsed;
+
+		public SimpleResettableTimer(TimeSpan period)
+		{
+			_period = period;
+		}
+
+		public void Reset()
+		{
+			lock (_sync)
+			{
+				if (_timer == null)
+				{
+					_timer = ThreadingHelper
+						.Timer(OnTimer)
+						.Interval(_period);
+				}
+				else
+					_changed = true;
+			}
+		}
+
+		private void OnTimer()
+		{
+			var elapsed = false;
+
+			lock (_sync)
+			{
+				if (!_changed)
+				{
+					_timer.Dispose();
+					_timer = null;
+
+					elapsed = true;
+				}
+				else
+					_changed = false;
+			}
+
+			if (elapsed)
+				Elapsed?.Invoke();
+		}
+
+		public void Flush()
+		{
+			lock (_sync)
+			{
+				if (_timer == null)
+					return;
+
+				_changed = false;
+				_timer.Change(TimeSpan.Zero, _period);
+			}
+		}
+
+		public void Dispose()
+		{
+			lock (_sync)
+			{
+				if (_timer == null)
+					return;
+
+				_changed = true;
+				_timer.Dispose();
+			}
+		}
+	}
+}
