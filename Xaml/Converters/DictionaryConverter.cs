4637b808bd04d9f02d276a7f1f7b38004299e50d
mika@stocksharp.com <mika@stocksharp.com>
2016-11-29 04:35:03 +0300

    DictionaryConverter. Nullable cases fix.

--- Xaml/Converters/DictionaryConverter.cs
+++ Xaml/Converters/DictionaryConverter.cs
@@ -4,6 +4,7 @@
 	using System.Collections;
 	using System.Collections.Generic;
 	using System.Globalization;
+	using System.Windows;
 	using System.Windows.Data;
 
 	using Ecng.Common;
@@ -15,12 +16,17 @@
 		{
 			var dictionary = (IDictionary)value;
 
-			var type = dictionary.GetType().GetGenericType(typeof(IDictionary<,>));
+			var type = dictionary?.GetType().GetGenericType(typeof(IDictionary<,>));
 
 			if (type == null)
-				return null;
+				return DependencyProperty.UnsetValue;
 
-			return dictionary[parameter.To(type.GetGenericArguments()[0])];
+			var key = parameter.To(type.GetGenericArguments()[0]);
+
+			if (!dictionary.Contains(key))
+				return DependencyProperty.UnsetValue;
+
+			return dictionary[key];
 		}
 
 		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
