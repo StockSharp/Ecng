87b469022baba419f2ac1ccb291a2c5591a22232
mika@stocksharp.com <mika@stocksharp.com>
2017-04-08 20:54:10 +0300

    IAuthorization. Edit users.

--- Security/IAuthorization.cs
+++ Security/IAuthorization.cs
@@ -1,6 +1,8 @@
 namespace Ecng.Security
 {
 	using System;
+	using System.Collections.Generic;
+	using System.Linq;
 	using System.Net;
 	using System.Security;
 
@@ -12,6 +14,26 @@ namespace Ecng.Security
 	/// </summary>
 	public interface IAuthorization
 	{
+		/// <summary>
+		/// Get all available users.
+		/// </summary>
+		IEnumerable<Tuple<string, IEnumerable<IPAddress>>> AllUsers { get; }
+
+		/// <summary>
+		/// Save user.
+		/// </summary>
+		/// <param name="login">Login.</param>
+		/// <param name="password">Password.</param>
+		/// <param name="possibleAddresses">Possible addresses.</param>
+		void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses);
+
+		/// <summary>
+		/// Delete user by login.
+		/// </summary>
+		/// <param name="login">Login.</param>
+		/// <returns>Returns <see langword="true"/>, if user was deleted, otherwise return <see langword="false"/>.</returns>
+		bool DeleteUser(string login);
+
 		/// <summary>
 		/// Check login and password.
 		/// </summary>
@@ -34,16 +56,25 @@ namespace Ecng.Security
 		{
 		}
 
-		/// <summary>
-		/// Check login and password.
-		/// </summary>
-		/// <param name="login">Login.</param>
-		/// <param name="password">Password.</param>
-		/// <param name="clientAddress">Remote network address.</param>
-		/// <returns>Session identifier.</returns>
+		/// <inheritdoc />
 		public virtual Guid ValidateCredentials(string login, SecureString password, IPAddress clientAddress)
 		{
 			return Guid.NewGuid();
+		}
+
+		/// <inheritdoc />
+		public virtual IEnumerable<Tuple<string, IEnumerable<IPAddress>>> AllUsers => Enumerable.Empty<Tuple<string, IEnumerable<IPAddress>>>();
+
+		/// <inheritdoc />
+		public virtual void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses)
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <inheritdoc />
+		public virtual bool DeleteUser(string login)
+		{
+			throw new NotSupportedException();
 		}
 	}
 
@@ -59,16 +90,31 @@ namespace Ecng.Security
 		{
 		}
 
-		/// <summary>
-		/// Check login and password.
-		/// </summary>
-		/// <param name="login">Login.</param>
-		/// <param name="password">Password.</param>
-		/// <param name="clientAddress">Remote network address.</param>
-		/// <returns>Session identifier.</returns>
+		/// <inheritdoc />
+		public virtual IEnumerable<Tuple<string, IEnumerable<IPAddress>>> AllUsers => Enumerable.Empty<Tuple<string, IEnumerable<IPAddress>>>();
+
+		/// <inheritdoc />
+		public virtual void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses)
+		{
+			if (WindowsIdentityManager.Validate(login, password))
+			{
+				if (!WindowsIdentityManager.ChangePassword(login, null, password))
+					throw new InvalidOperationException("User {0} not found.".Translate().Put(login));
+			}
+			else
+				WindowsIdentityManager.CreateUser(login, password);
+		}
+
+		/// <inheritdoc />
+		public virtual bool DeleteUser(string login)
+		{
+			return WindowsIdentityManager.DeleteUser(login);
+		}
+
+		/// <inheritdoc />
 		public virtual Guid ValidateCredentials(string login, SecureString password, IPAddress clientAddress)
 		{
-			if (!WindowsIdentityManager.Validate(login, password.To<string>()))
+			if (!WindowsIdentityManager.Validate(login, password))
 				throw new UnauthorizedAccessException("User {0} not found or password is incorrect.".Translate().Put(login));
 
 			return Guid.NewGuid();
