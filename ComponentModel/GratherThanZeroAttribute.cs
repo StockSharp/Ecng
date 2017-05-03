563b36764ccd8419194983c79b11d8fcbba9d239
Belich Aleksandr <a-belich@yandex.ru>
2017-05-03 13:35:34 +0700

    GratherThanZeroAttribute added.

---
+++ ComponentModel/GratherThanZeroAttribute.cs
@@ -0,0 +1,22 @@
+namespace Ecng.ComponentModel
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+
+	using Ecng.Common;
+
+	public  class GratherThanZeroAttribute : ValidationAttribute
+	{
+		public override bool IsValid(object value)
+		{
+			try
+			{
+				return value.To<decimal?>() > 0;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
