a2488b68310d644bc5976bafcbdf09dd9e3d9b2d
mika@stocksharp.com <mika@stocksharp.com>
2017-06-02 20:22:24 +0300

    RangeEditor

---
+++ Xaml.DevExpress/RangeEditor.cs
@@ -0,0 +1,52 @@
+﻿namespace Ecng.Xaml.DevExp
+{
+	using System;
+	using System.Windows;
+
+	using DevExpress.Xpf.Editors;
+
+	using Ecng.ComponentModel;
+	using Ecng.Localization;
+
+	/// <summary>
+	/// <see cref="Range{T}"/> editor.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public sealed class RangeEditor<T> : ButtonEdit
+		where T : IComparable<T>
+	{
+		/// <summary>
+		/// Create <see cref="RangeEditor{T}"/>.
+		/// </summary>
+		public RangeEditor()
+		{
+			AllowDefaultButton = false;
+
+			var btnEdit = new ButtonInfo
+			{
+				GlyphKind = GlyphKind.Edit,
+				Content = "Create".Translate()
+			};
+			btnEdit.Click += BtnEdit_OnClick;
+			Buttons.Add(btnEdit);
+
+			var btnReset = new ButtonInfo
+			{
+				GlyphKind = GlyphKind.Cancel,
+				Content = "Cancel".Translate()
+			};
+			btnReset.Click += BtnReset_OnClick;
+			Buttons.Add(btnReset);
+		}
+
+		private void BtnEdit_OnClick(object sender, RoutedEventArgs e)
+		{
+			EditValue = new Range<T>(default(T), default(T));
+		}
+
+		private void BtnReset_OnClick(object sender, RoutedEventArgs e)
+		{
+			EditValue = null;
+		}
+	}
+}
