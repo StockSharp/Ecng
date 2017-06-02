5a51f97a94756139f2f724dad843fa6036dc3c7c
mika@stocksharp.com <mika@stocksharp.com>
2017-06-02 20:22:10 +0300

    INotifyPropertiesChanged

---
+++ ComponentModel/INotifyPropertiesChanged.cs
@@ -0,0 +1,15 @@
+namespace Ecng.ComponentModel
+{
+	using System;
+
+	/// <summary>
+	/// The interface describing the type with a variable number of properties.
+	/// </summary>
+	public interface INotifyPropertiesChanged
+	{
+		/// <summary>
+		/// The available properties change event.
+		/// </summary>
+		event Action PropertiesChanged;
+	}
+}
