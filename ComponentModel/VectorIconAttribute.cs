025973e4f4a9aec26ec96658f4813f9f95becc0c
mika@stocksharp.com <mika@stocksharp.com>
2017-08-22 01:00:03 +0300

    VectorIconAttribute

---
+++ ComponentModel/VectorIconAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace Ecng.ComponentModel
+{
+	using System;
+
+	using Ecng.Common;
+
+	public class VectorIconAttribute : Attribute
+	{
+		public string Icon { get; }
+
+		public VectorIconAttribute(string icon)
+		{
+			if (icon.IsEmpty())
+				throw new ArgumentNullException(nameof(icon));
+
+			Icon = icon;
+		}
+	}
+}
