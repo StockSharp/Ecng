4d86f4f751e4510d928432e4d14b3abe41aa56d2
mika@stocksharp.com <mika@stocksharp.com>
2017-09-29 14:54:34 +0300

    SystemMemory info.

---
+++ Interop/SystemMemory.cs
@@ -0,0 +1,36 @@
+﻿namespace Ecng.Interop
+{
+	using System;
+	using System.Diagnostics;
+
+	using Microsoft.VisualBasic.Devices;
+
+	public class SystemMemory
+	{
+		private SystemMemory()
+		{
+			try
+			{
+				var info = new ComputerInfo();
+
+				TotalPhysicalMemory = info.TotalPhysicalMemory;
+				TotalVirtualMemory = info.TotalVirtualMemory;
+				AvailableVirtualMemory = info.AvailableVirtualMemory;
+				AvailablePhysicalMemory = info.AvailablePhysicalMemory;
+			}
+			catch (Exception e)
+			{
+				Trace.WriteLine(e);
+			}
+		}
+
+		public ulong TotalPhysicalMemory { get; }
+		public ulong TotalVirtualMemory { get; }
+		public ulong AvailableVirtualMemory { get; }
+		public ulong AvailablePhysicalMemory { get; }
+
+		private static readonly Lazy<SystemMemory> _instance = new Lazy<SystemMemory>(() => new SystemMemory());
+
+		public static SystemMemory Instance = _instance.Value;
+	}
+}
