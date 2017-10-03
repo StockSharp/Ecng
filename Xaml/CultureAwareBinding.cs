f9d4918be1261730cb93bf390907c8c9914962dd
mika@stocksharp.com <mika@stocksharp.com>
2017-10-03 08:26:45 +0300

    CultureAwareBinding

---
+++ Xaml/CultureAwareBinding.cs
@@ -0,0 +1,20 @@
+﻿namespace Ecng.Xaml
+{
+	using System.Globalization;
+	using System.Windows.Data;
+
+	// http://stackoverflow.com/a/14163432/1086121
+	public class CultureAwareBinding : Binding
+	{
+		public CultureAwareBinding()
+		{
+			ConverterCulture = CultureInfo.CurrentCulture;
+		}
+
+		public CultureAwareBinding(string path)
+			: base(path)
+		{
+			ConverterCulture = CultureInfo.CurrentCulture;
+		}
+	}
+}
