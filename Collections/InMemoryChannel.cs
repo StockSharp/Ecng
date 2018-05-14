451f6224d4b32e91a74e3c33409e263d977c5c4c
mika@stocksharp.com <mika@stocksharp.com>
2018-05-14 10:56:10 +0300

    BaseInMemoryChannel.

---
+++ Collections/InMemoryChannel.cs
@@ -0,0 +1,19 @@
+﻿namespace Ecng.Collections
+{
+	using System;
+
+	public class InMemoryChannel<TItem> : BaseInMemoryChannel<TItem>
+	{
+		public InMemoryChannel(IBlockingQueue<TItem> queue, string name, Action<Exception> errorHandler)
+			: base(queue, name, errorHandler)
+		{
+		}
+
+		public event Action<TItem> NewOut;
+
+		protected override void OnNewOut(TItem item)
+		{
+			NewOut?.Invoke(item);
+		}
+	}
+}
