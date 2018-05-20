d14a3d3a25fc16318ae13105731f4ad16f388499
esper <esper@spectrum>
2018-05-20 20:11:01 +0700

    Get image for TreeWalkerToSvgImageConverter via convert method.

--- Xaml.DevExpress/TreeWalkerToSvgImageConverter.cs
+++ Xaml.DevExpress/TreeWalkerToSvgImageConverter.cs
@@ -17,12 +17,24 @@
 			_image = image ?? throw new ArgumentNullException(nameof(image));
 		}
 
+		public TreeWalkerToSvgImageConverter()
+		{
+		}
+
 		object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
 		{
 			var targetObject = values[0] as DependencyObject;
 			var inheritedPalette = values[2] as WpfSvgPalette;
 			var palette = (values[1] as ThemeTreeWalker)?.InplaceResourceProvider.GetSvgPalette(targetObject);
-			return ReplaceBrush(_image, inheritedPalette, palette);
+			var image = _image;
+
+			if (_image == null)
+				image = values[3] as DrawingImage;
+
+			if (image == null)
+				throw new ArgumentNullException(nameof(image));
+
+			return ReplaceBrush(image, inheritedPalette, palette);
 		}
 
 		object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
