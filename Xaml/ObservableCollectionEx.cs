01f6e27732530aa2627962aea55852f4fb3a1be6
esper <esper@spectrum>
2018-06-23 11:46:45 +0700

    Designer (task 719). CopyTo array for ObservableCollectionEx fixes.

--- Xaml/ObservableCollectionEx.cs
+++ Xaml/ObservableCollectionEx.cs
@@ -233,7 +233,10 @@ namespace Ecng.Xaml
 		/// <param name="array">The one-dimensional <see cref="T:System.Array"/> that is the destination of the elements copied from <see cref="T:System.Collections.ICollection"/>. The <see cref="T:System.Array"/> must have zero-based indexing. </param><param name="index">The zero-based index in <paramref name="array"/> at which copying begins. </param><exception cref="T:System.ArgumentNullException"><paramref name="array"/> is null. </exception><exception cref="T:System.ArgumentOutOfRangeException"><paramref name="index"/> is less than zero. </exception><exception cref="T:System.ArgumentException"><paramref name="array"/> is multidimensional.-or- The number of elements in the source <see cref="T:System.Collections.ICollection"/> is greater than the available space from <paramref name="index"/> to the end of the destination <paramref name="array"/>.-or-The type of the source <see cref="T:System.Collections.ICollection"/> cannot be cast automatically to the type of the destination <paramref name="array"/>.</exception>
 		void ICollection.CopyTo(Array array, int index)
 		{
-			CopyTo((TItem[])array, index);
+			if (!(array is TItem[] items))
+				items = array.Cast<TItem>().ToArray();
+
+			CopyTo(items, index);
 		}
 
 		/// <summary>
