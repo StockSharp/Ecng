1b325f112c90ced005ad68df96afb7104d9c97c1
mika@stocksharp.com <mika@stocksharp.com>
2019-05-13 01:34:15 +0300

    Ecng.IO project.

---
+++ IO/CompressionHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace Ecng.IO
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.IO.Compression;
+
+	using Ecng.Common;
+
+	public static class CompressionHelper
+    {
+	    public static IEnumerable<Stream> Unzip(this byte[] input, bool leaveOpen = false, Func<string, bool> filter = null)
+	    {
+		    return input.To<MemoryStream>().Unzip(leaveOpen, filter);
+	    }
+
+	    public static IEnumerable<Stream> Unzip(this Stream input, bool leaveOpen = false, Func<string, bool> filter = null)
+	    {
+		    using (var zip = new ZipArchive(input, ZipArchiveMode.Read, leaveOpen))
+		    {
+			    foreach (var entry in zip.Entries)
+			    {
+					if (filter?.Invoke(entry.Name) == false)
+						continue;
+
+					using (var stream = entry.Open())
+						yield return stream;
+			    }
+		    }
+	    }
+    }
+}
---
+++ IO/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4e2d9d8a-6e5a-4482-96f6-2544de6611b9")]
