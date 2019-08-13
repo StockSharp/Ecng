4867bbcfd480eb0ba7f93980367d9f3779e2ea63
mika@stocksharp.com <mika@stocksharp.com>
2019-08-14 02:12:32 +0300

    Ecng.Interop.NPOI.

---
+++ Interop.NPOI/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Interop.NPOI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c100a6c1-9d59-49a6-a06a-38498cf785dd")]
---
+++ Interop/IExcelWorker.cs
@@ -0,0 +1,20 @@
+﻿namespace Ecng.Interop
+{
+	using System;
+	using System.Windows.Media;
+
+	using Ecng.ComponentModel;
+
+	public interface IExcelWorker : IDisposable
+	{
+		IExcelWorker SetCell<T>(int col, int row, T value);
+		T GetCell<T>(int col, int row);
+
+		IExcelWorker SetStyle(int col, Type type);
+		IExcelWorker SetStyle(int col, string format);
+
+		IExcelWorker SetConditionalFormatting(int col, ComparisonOperator op, string condition, Color? bgColor, Color? fgColor);
+
+		IExcelWorker Save(string fileName, bool autoSizeColumns);
+	}
+}
