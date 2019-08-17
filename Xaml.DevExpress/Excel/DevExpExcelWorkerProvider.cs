2e266bf4b9c9b5f6f97d9a4cf59cfbc7d2d95563
mika@stocksharp.com <mika@stocksharp.com>
2019-08-17 19:11:59 +0300

    DevExpExcelWorkerProvider fixes.

--- Xaml.DevExpress/Excel/DevExpExcelWorkerProvider.cs
+++ Xaml.DevExpress/Excel/DevExpExcelWorkerProvider.cs
@@ -3,24 +3,108 @@
 	using System;
 	using System.Collections.Generic;
 	using System.IO;
+	using System.Linq;
 	using System.Windows.Media;
 
 	using DevExpress.Export.Xl;
 
 	using Ecng.Collections;
+	using Ecng.Common;
 	using Ecng.ComponentModel;
 	using Ecng.Interop;
-
-	using MoreLinq;
 
 	public class DevExpExcelWorkerProvider : IExcelWorkerProvider
 	{
 		private class DevExpExcelWorker : IExcelWorker
 		{
+			private class SheetData : IDisposable
+			{
+				private readonly DevExpExcelWorker _worker;
+				private readonly Dictionary<int, Dictionary<int, object>> _cells = new Dictionary<int, Dictionary<int, object>>();
+				
+				public readonly HashSet<int> Columns = new HashSet<int>();
+				public readonly HashSet<int> Rows = new HashSet<int>();
+
+				public SheetData(DevExpExcelWorker worker)
+				{
+					_worker = worker ?? throw new ArgumentNullException(nameof(worker));
+				}
+
+				public string Name { get; set; }
+
+				public void SetCell<T>(int col, int row, T value)
+				{
+					Columns.Add(col);
+					Rows.Add(row);
+					_cells.SafeAdd(row, key => new Dictionary<int, object>())[col] = value;
+				}
+
+				public T GetCell<T>(int col, int row)
+				{
+					return (T)_cells.SafeAdd(row, key => new Dictionary<int, object>()).TryGetValue(col);
+				}
+
+				public void Dispose()
+				{
+					using (var sheet = _worker._document.CreateSheet())
+					{
+						if (!Name.IsEmpty())
+							sheet.Name = Name;
+
+						foreach (var column in Columns)
+						{
+							using (sheet.CreateColumn(column))
+							{
+							}
+						}
+
+						foreach (var row in Rows)
+						{
+							using (var xlRow = sheet.CreateRow(row))
+							{
+								if (!_cells.TryGetValue(row, out var dict))
+									continue;
+
+								foreach (var pair in dict)
+								{
+									using (var cell = xlRow.CreateCell(pair.Key))
+									{
+										if (pair.Value == null)
+											continue;
+
+										XlVariantValue xlVal;
+
+										if (pair.Value is bool b)
+											xlVal = new XlVariantValue { BooleanValue = b };
+										else if (pair.Value is DateTime dt)
+											xlVal = new XlVariantValue { DateTimeValue = dt };
+										else if (pair.Value is string s)
+											xlVal = new XlVariantValue { TextValue = s };
+										else if (pair.Value.GetType().IsNumeric())
+											xlVal = new XlVariantValue { NumericValue = pair.Value.To<double>() };
+										//else if (typeof(T) == typeof(Exception))
+										//	xlVal = new XlVariantValue { ErrorValue = new NameError() };
+										else
+											throw new ArgumentOutOfRangeException(pair.Value?.ToString());
+
+										cell.Value = xlVal;
+									}
+								}	
+							}
+						}
+					}
+
+					Columns.Clear();
+					Rows.Clear();
+
+					_cells.Clear();
+				}
+			}
+
 			private readonly IXlExporter _exporter = XlExport.CreateExporter(XlDocumentFormat.Xlsx);
 			private readonly IXlDocument _document;
-			private readonly PairSet<string, IXlSheet> _sheets = new PairSet<string, IXlSheet>(StringComparer.InvariantCultureIgnoreCase);
-			private IXlSheet _currSheet;
+			private readonly List<SheetData> _sheets = new List<SheetData>();
+			private SheetData _currSheet;
 
 			public DevExpExcelWorker(Stream stream)
 			{
@@ -29,70 +113,61 @@
 
 			void IDisposable.Dispose()
 			{
-				_sheets.Values.ForEach(s => s.Dispose());
+				_sheets.ForEach(s => s.Dispose());
 				_sheets.Clear();
+
 				_document.Dispose();
 			}
 
 			IExcelWorker IExcelWorker.SetCell<T>(int col, int row, T value)
 			{
-				throw new NotImplementedException();
+				_currSheet.SetCell(col, row, value);
+				return this;
 			}
 
 			T IExcelWorker.GetCell<T>(int col, int row)
 			{
-				throw new NotImplementedException();
+				return _currSheet.GetCell<T>(col, row);
 			}
 
 			IExcelWorker IExcelWorker.SetStyle(int col, Type type)
 			{
-				throw new NotImplementedException();
+				return this;
 			}
 
 			IExcelWorker IExcelWorker.SetStyle(int col, string format)
 			{
-				throw new NotImplementedException();
+				return this;
 			}
 
 			IExcelWorker IExcelWorker.SetConditionalFormatting(int col, ComparisonOperator op, string condition, Color? bgColor, Color? fgColor)
 			{
-				throw new NotImplementedException();
+				return this;
 			}
 
 			IExcelWorker IExcelWorker.RenameSheet(string name)
 			{
-				_sheets.RemoveByValue(_currSheet);
 				_currSheet.Name = name;
-				_sheets.Add(name, _currSheet);
 				return this;
 			}
 
 			IExcelWorker IExcelWorker.AddSheet()
 			{
-				_currSheet = _document.CreateSheet();
+				_currSheet = new SheetData(this);
+				_sheets.Add(_currSheet);
 				return this;
 			}
 
-			bool IExcelWorker.ContainsSheet(string name)
-			{
-				return _sheets.ContainsKey(name);
-			}
+			bool IExcelWorker.ContainsSheet(string name) => _sheets.Any(s => s.Name.CompareIgnoreCase(name));
 
 			IExcelWorker IExcelWorker.SwitchSheet(string name)
 			{
-				_currSheet = _sheets[name];
+				_currSheet = _sheets.First(s => s.Name.CompareIgnoreCase(name));
 				return this;
 			}
 
-			int IExcelWorker.GetColumnsCount()
-			{
-				return _currSheet.ColumnRange.BottomRight.Column;
-			}
-
-			int IExcelWorker.GetRowsCount()
-			{
-				return _currSheet.ColumnRange.BottomRight.Row;
-			}
+			int IExcelWorker.GetColumnsCount() => _currSheet.Columns.Count;
+			int IExcelWorker.GetRowsCount() => _currSheet.Rows.Count;
 		}
 
 		IExcelWorker IExcelWorkerProvider.CreateNew(Stream stream, bool readOnly)
