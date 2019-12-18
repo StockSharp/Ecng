45d0f95d4b9596f957de537e86515ea1694256f2
mika@stocksharp.com <mika@stocksharp.com>
2019-12-18 15:00:14 +0300

    Net.SocketIO. RestSharp included as sources.

---
+++ Net.SocketIO/RestSharp/Serialization/IRestSerializer.cs
@@ -0,0 +1,14 @@
+using RestSharp.Deserializers;
+using RestSharp.Serializers;
+
+namespace RestSharp.Serialization
+{
+    public interface IRestSerializer : ISerializer, IDeserializer
+    {
+        string[] SupportedContentTypes { get; }
+        
+        DataFormat DataFormat { get; }
+
+        string Serialize(Parameter parameter);
+    }
+}
