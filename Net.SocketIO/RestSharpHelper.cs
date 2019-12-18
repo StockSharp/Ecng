f893cace5b2d6ec15815e5485b1a3c0993720e33
mika@stocksharp.com <mika@stocksharp.com>
2019-12-18 15:18:40 +0300

    IRestRequest. Invoke extension added.

--- Net.SocketIO/RestSharpHelper.cs
+++ Net.SocketIO/RestSharpHelper.cs
@@ -1,6 +1,11 @@
 ﻿namespace Ecng.Net
 {
 	using System;
+	using System.Linq;
+	using System.Net;
+	using System.Reflection;
+
+	using Ecng.Common;
 
 	using RestSharp;
 
@@ -12,6 +17,40 @@
 				throw new ArgumentNullException(nameof(request));
 
 			request.AddParameter(request.JsonSerializer.ContentType, bodyStr, ParameterType.RequestBody);
+		}
+
+		public static object Invoke(this IRestRequest request, Uri url, object caller, Action<string, object, object> logVerbose)
+		{
+			if (request == null)
+				throw new ArgumentNullException(nameof(request));
+
+			if (url == null)
+				throw new ArgumentNullException(nameof(url));
+
+			if (caller == null)
+				throw new ArgumentNullException(nameof(caller));
+
+			var asm = caller.GetType().Assembly;
+			var projName = asm.GetAttribute<AssemblyProductAttribute>()?.Product;
+
+			var client = new RestClient(url)
+			{
+				UserAgent = $"{projName}/" + asm.GetName().Version
+			};
+
+			logVerbose?.Invoke("Request '{0}' Args '{1}'.", url, request.Parameters.Select(p => $"{p.Name}={p.Value}").Join("&"));
+			var response = client.Execute(request);
+
+			var content = response.Content;
+			logVerbose?.Invoke("Response '{0}' (code {1}).", content, response.StatusCode);
+
+			if (response.StatusCode != HttpStatusCode.OK)
+				throw new InvalidOperationException(content.IsEmpty() ? (response.ErrorMessage.IsEmpty() ? response.StatusDescription : response.ErrorMessage) : content);
+
+			if (content.IsEmpty())
+				throw new InvalidOperationException();
+
+			return content.DeserializeObject<object>();
 		}
 	}
 }
