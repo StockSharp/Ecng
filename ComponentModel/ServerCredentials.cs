6afa7279348990553abaf67a94ec8cbed738f139
mika@stocksharp.com <mika@stocksharp.com>
2020-02-10 00:34:03 +0300

    ComponentModel. ServerCredentials.

---
+++ ComponentModel/ServerCredentials.cs
@@ -0,0 +1,86 @@
+namespace Ecng.ComponentModel
+{
+	using System.Security;
+
+	using Ecng.Serialization;
+
+	/// <summary>
+	/// The class that contains a login and password to access the services https://stocksharp.com .
+	/// </summary>
+	public class ServerCredentials : NotifiableObject, IPersistable
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ServerCredentials"/>.
+		/// </summary>
+		public ServerCredentials()
+		{
+		}
+
+		private string _email;
+
+		/// <summary>
+		/// Email.
+		/// </summary>
+		public string Email
+		{
+			get => _email;
+			set
+			{
+				_email = value;
+				NotifyChanged(nameof(Email));
+			}
+		}
+
+		private SecureString _password;
+
+		/// <summary>
+		/// Password.
+		/// </summary>
+		public SecureString Password
+		{
+			get => _password;
+			set
+			{
+				_password = value;
+				NotifyChanged(nameof(Password));
+			}
+		}
+
+		private bool _autoLogon = true;
+
+		/// <summary>
+		/// Auto login.
+		/// </summary>
+		public bool AutoLogon
+		{
+			get => _autoLogon;
+			set
+			{
+				_autoLogon = value;
+				NotifyChanged(nameof(AutoLogon));
+			}
+		}
+
+		/// <summary>
+		/// Load settings.
+		/// </summary>
+		/// <param name="storage">Settings storage.</param>
+		public virtual void Load(SettingsStorage storage)
+		{
+			Email = storage.GetValue<string>(nameof(Email));
+			Password = storage.GetValue<SecureString>(nameof(Password));
+			AutoLogon = storage.GetValue<bool>(nameof(AutoLogon));
+		}
+
+		/// <summary>
+		/// Save settings.
+		/// </summary>
+		/// <param name="storage">Settings storage.</param>
+		public virtual void Save(SettingsStorage storage)
+		{
+			storage.SetValue(nameof(Email), Email);
+			storage.SetValue(nameof(Password), Password);
+			storage.SetValue(nameof(AutoLogon), AutoLogon);
+		}
+	}
+}
