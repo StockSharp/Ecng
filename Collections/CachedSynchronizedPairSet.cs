41e81e3266d59f36ce4e0d99fcbd7c785d4f62a9
mika@stocksharp.com <mika@stocksharp.com>
2020-02-21 21:02:02 +0300

    CachedSynchronizedPairSet.

---
+++ Collections/CachedSynchronizedPairSet.cs
@@ -0,0 +1,113 @@
+﻿namespace Ecng.Collections
+{
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public class CachedSynchronizedPairSet<TKey, TValue> : SynchronizedPairSet<TKey, TValue>
+	{
+		public CachedSynchronizedPairSet()
+		{
+		}
+
+		public CachedSynchronizedPairSet(IEqualityComparer<TKey> comparer)
+			: base(comparer)
+		{
+		}
+
+		public CachedSynchronizedPairSet(IEqualityComparer<TKey> keyComparer, IEqualityComparer<TValue> valueComparer)
+			: base(keyComparer, valueComparer)
+		{
+		}
+
+		private TKey[] _cachedKeys;
+
+		public TKey[] CachedKeys
+		{
+			get
+			{
+				lock (SyncRoot)
+					return _cachedKeys ?? (_cachedKeys = Keys.ToArray());
+			}
+		}
+
+		private TValue[] _cachedValues;
+
+		public TValue[] CachedValues
+		{
+			get
+			{
+				lock (SyncRoot)
+					return _cachedValues ?? (_cachedValues = Values.ToArray());
+			}
+		}
+
+		private KeyValuePair<TKey, TValue>[] _cachedPairs;
+
+		public KeyValuePair<TKey, TValue>[] CachedPairs
+		{
+			get
+			{
+				lock (SyncRoot)
+					return _cachedPairs ?? (_cachedPairs = ToArray());
+			}
+		}
+
+		public override TValue this[TKey key]
+		{
+			set
+			{
+				lock (SyncRoot)
+				{
+					_cachedValues = null;
+					_cachedPairs = null;
+
+					if (_cachedKeys != null && !ContainsKey(key))
+						_cachedKeys = null;
+
+					base[key] = value;
+				}
+			}
+		}
+
+		public override void Add(TKey key, TValue value)
+		{
+			lock (SyncRoot)
+			{
+				base.Add(key, value);
+
+				_cachedKeys = null;
+				_cachedValues = null;
+				_cachedPairs = null;
+			}
+		}
+
+		public override bool Remove(TKey key)
+		{
+			lock (SyncRoot)
+			{
+				if (base.Remove(key))
+				{
+					_cachedKeys = null;
+					_cachedValues = null;
+					_cachedPairs = null;
+
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		public override void Clear()
+		{
+			lock (SyncRoot)
+			{
+				base.Clear();
+
+				_cachedKeys = null;
+				_cachedValues = null;
+				_cachedPairs = null;
+			}
+		}
+	}
+}
