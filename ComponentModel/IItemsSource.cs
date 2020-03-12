18aac443166b794500acc10f7ac78575d7f58aaf
mika@stocksharp.com <mika@stocksharp.com>
2020-03-12 10:17:04 +0300

    IItemsSource. Moved from Xaml.DevExpress to ComponentModel.

--- ComponentModel/IItemsSource.cs
+++ ComponentModel/IItemsSource.cs
@@ -1,10 +1,7 @@
-﻿namespace Ecng.Xaml.DevExp
+﻿namespace Ecng.ComponentModel
 {
 	using System;
 	using System.Collections.Generic;
-	using System.Collections.ObjectModel;
-
-	using DevExpress.Xpf.Editors.Settings;
 
 	using Ecng.Common;
 	using Ecng.Localization;
@@ -29,7 +26,7 @@
 		/// <summary>
 		/// Gets the type to use.
 		/// </summary>
-		public Type Type { get; private set; }
+		public Type Type { get; }
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ItemsSourceAttribute"/>.
@@ -45,26 +42,5 @@
 
 			Type = type;
 		}
-	}
-
-	/// <summary>
-	/// Contains settings specific to a combobox editor with uses <see cref="IItemsSource"/>.
-	/// </summary>
-	public class ItemsSourceEditSettings : ComboBoxEditSettings
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ItemsSourceEditSettings"/>.
-		/// </summary>
-		public ItemsSourceEditSettings()
-		{
-			DisplayMember = "Item1";
-			ValueMember = "Item2";
-			ItemsSource = ComboBoxItems;
-		}
-
-		/// <summary>
-		/// Items.
-		/// </summary>
-		public IList<Tuple<string, object>> ComboBoxItems { get; } = new ObservableCollection<Tuple<string, object>>();
 	}
 }
---
+++ Xaml.DevExpress/ItemsSourceEditSettings.cs
@@ -0,0 +1,31 @@
+﻿namespace Ecng.Xaml.DevExp
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+
+	using DevExpress.Xpf.Editors.Settings;
+
+	using Ecng.ComponentModel;
+
+	/// <summary>
+	/// Contains settings specific to a combobox editor with uses <see cref="IItemsSource"/>.
+	/// </summary>
+	public class ItemsSourceEditSettings : ComboBoxEditSettings
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ItemsSourceEditSettings"/>.
+		/// </summary>
+		public ItemsSourceEditSettings()
+		{
+			DisplayMember = "Item1";
+			ValueMember = "Item2";
+			ItemsSource = ComboBoxItems;
+		}
+
+		/// <summary>
+		/// Items.
+		/// </summary>
+		public IList<Tuple<string, object>> ComboBoxItems { get; } = new ObservableCollection<Tuple<string, object>>();
+	}
+}
