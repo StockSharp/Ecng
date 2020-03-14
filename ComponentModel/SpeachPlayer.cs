664028d49298947d57b03746997f6a7eff848a40
mika@stocksharp.com <mika@stocksharp.com>
2020-03-14 12:23:47 +0300

    SpeachPlayer.

---
+++ ComponentModel/SpeachPlayer.cs
@@ -0,0 +1,26 @@
+﻿namespace Ecng.ComponentModel
+{
+	using Ecng.Common;
+
+	public class SpeachPlayer : Disposable
+	{
+#if NETCOREAPP
+		public int Volume { get; set; }
+
+		public void Say(string text) { }
+#else
+		private readonly System.Speech.Synthesis.SpeechSynthesizer _synthesizer = new System.Speech.Synthesis.SpeechSynthesizer();
+
+		public int Volume
+		{
+			get => _synthesizer.Volume;
+			set => _synthesizer.Volume = value;
+		}
+
+		public void Say(string text)
+		{
+			_synthesizer.Speak(text);
+		}
+#endif
+	}
+}
