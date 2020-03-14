f307c6267fe9b603acfd1a599ed58604eb6f0ecd
mika@stocksharp.com <mika@stocksharp.com>
2020-03-14 21:46:56 +0300

    NativeLibrary -> DllLibrary.

---
+++ Interop/Interop.Core/DllLibrary.cs
@@ -0,0 +1,36 @@
+namespace Ecng.Interop
+{
+	using System;
+	using System.Diagnostics;
+
+	using Ecng.Common;
+
+	public abstract class DllLibrary : Disposable
+	{
+		protected DllLibrary(string dllPath)
+		{
+			if (dllPath.IsEmpty())
+				throw new ArgumentNullException(nameof(dllPath));
+
+			DllPath = Marshaler.FixLibraryExtension(dllPath);
+			DllVersion = FileVersionInfo.GetVersionInfo(DllPath).ProductVersion?.Replace(',', '.')?.RemoveSpaces()?.To<Version>();
+
+			Handler = Marshaler.LoadLibrary(DllPath);
+		}
+
+		public string DllPath { get; private set; }
+
+		public Version DllVersion { get; private set; }
+
+		protected IntPtr Handler { get; }
+
+		protected T GetHandler<T>(string procName) => Marshaler.GetDelegateForFunctionPointer<T>(Marshaler.GetProcAddress(Handler, procName));
+
+		protected override void DisposeNative()
+		{
+			Marshaler.FreeLibrary(Handler);
+			base.DisposeNative();
+		}
+
+	}
+}
