858b21d115b07d4fa44be72099dca82b58c6cb27
mika@stocksharp.com <mika@stocksharp.com>
2020-03-16 00:53:21 +0300

    Build fix.

--- EcngTest/Properties/AssemblyInfo.cs
+++ EcngTest/Properties/AssemblyInfo.cs
@@ -5,29 +5,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("EcngTest")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Fantazy")]
-[assembly: AssemblyProduct("EcngTest")]
-[assembly: AssemblyCopyright("Copyright © Fantazy 2006")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0d6f48e0-b95a-48f2-acf6-3022c0d9013b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
