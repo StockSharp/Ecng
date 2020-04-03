863ca8cdd5adff4705d8c7742dc993f78f34baba
mika@stocksharp.com <mika@stocksharp.com>
2020-04-03 10:32:23 +0300

    LazyHelper. Fix .NET Core.

--- ComponentModel/LazyHelper.cs
+++ ComponentModel/LazyHelper.cs
@@ -7,7 +7,10 @@
 
 	public static class LazyHelper
 	{
-		private static readonly SynchronizedDictionary<object, Delegate> _delegates = new SynchronizedDictionary<object, Delegate>(); 
+		private static readonly SynchronizedDictionary<object, Delegate> _factories = new SynchronizedDictionary<object, Delegate>(); 
+#if NETCOREAPP || NETSTANDARD
+		private static readonly SynchronizedDictionary<object, object> _states = new SynchronizedDictionary<object, object>(); 
+#endif
 
 		public static Lazy<T> Track<T>(this Lazy<T> lazy)
 		{
@@ -17,7 +20,12 @@
 			if (lazy.IsValueCreated)
 				throw new ArgumentException("lazy");
 
-			_delegates.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, Func<T>>("m_valueFactory", null));
+#if !NETCOREAPP && !NETSTANDARD
+			_factories.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, Func<T>>("m_valueFactory", null));
+#else
+			_states.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, object>("_state", null));
+			_factories.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, Func<T>>("_factory", null));
+#endif
 			return lazy;
 		}
 
@@ -26,9 +34,15 @@
 			if (lazy == null)
 				throw new ArgumentNullException(nameof(lazy));
 
-			var initFunc = _delegates[lazy];
+			var factory = _factories[lazy];
+
+#if !NETCOREAPP && !NETSTANDARD
 			lazy.SetValue("m_boxed", (object)null);
-			lazy.SetValue("m_valueFactory", initFunc);
+			lazy.SetValue("m_valueFactory", factory);
+#else
+			lazy.SetValue("_state", _states[lazy]);
+			lazy.SetValue("_factory", factory);
+#endif
 		}
 	}
 }
