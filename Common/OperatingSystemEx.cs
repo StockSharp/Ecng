370737ac43ea308476a0f82799111351c32b4ca4
mika@stocksharp.com <mika@stocksharp.com>
2020-04-25 00:57:02 +0300

    OperatingSystemEx. IsOSPlatform extension.

--- Common/OperatingSystemEx.cs
+++ Common/OperatingSystemEx.cs
@@ -4,13 +4,13 @@
 
 	public static class OperatingSystemEx
 	{
-		public static bool IsWindows() =>
-			RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+		public static bool IsWindows() => OSPlatform.Windows.IsOSPlatform();
 
-		public static bool IsMacOS() =>
-			RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+		public static bool IsMacOS() =>	OSPlatform.OSX.IsOSPlatform();
 
-		public static bool IsLinux() =>
-			RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+		public static bool IsLinux() =>	OSPlatform.Linux.IsOSPlatform();
+
+		public static bool IsOSPlatform(this OSPlatform platform)
+			=> RuntimeInformation.IsOSPlatform(platform);
 	}
 }
