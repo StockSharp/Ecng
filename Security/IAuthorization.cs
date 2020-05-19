012b8d3ac9353857c6198d38ca63df587552a679
mika@stocksharp.com <mika@stocksharp.com>
2020-05-19 16:26:47 +0300

    IAuthorization. SaveUser, ChangePassword, DeleteUser removed. SimpleAuthorization added.

--- Security/IAuthorization.cs
+++ Security/IAuthorization.cs
@@ -1,7 +1,6 @@
 namespace Ecng.Security
 {
 	using System;
-	using System.Collections.Generic;
 	using System.Net;
 	using System.Security;
 
@@ -13,28 +12,28 @@ namespace Ecng.Security
 	/// </summary>
 	public interface IAuthorization
 	{
-		/// <summary>
-		/// Save user.
-		/// </summary>
-		/// <param name="login">Login.</param>
-		/// <param name="password">Password.</param>
-		/// <param name="possibleAddresses">Possible addresses.</param>
-		void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses);
+		///// <summary>
+		///// Save user.
+		///// </summary>
+		///// <param name="login">Login.</param>
+		///// <param name="password">Password.</param>
+		///// <param name="possibleAddresses">Possible addresses.</param>
+		//void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses);
 
-		/// <summary>
-		/// Change password.
-		/// </summary>
-		/// <param name="login">Login.</param>
-		/// <param name="oldPassword">Old password.</param>
-		/// <param name="newPassword">New password.</param>
-		void ChangePassword(string login, SecureString oldPassword, SecureString newPassword);
+		///// <summary>
+		///// Change password.
+		///// </summary>
+		///// <param name="login">Login.</param>
+		///// <param name="oldPassword">Old password.</param>
+		///// <param name="newPassword">New password.</param>
+		//void ChangePassword(string login, SecureString oldPassword, SecureString newPassword);
 
-		/// <summary>
-		/// Delete user by login.
-		/// </summary>
-		/// <param name="login">Login.</param>
-		/// <returns>Returns <see langword="true"/>, if user was deleted, otherwise return <see langword="false"/>.</returns>
-		bool DeleteUser(string login);
+		///// <summary>
+		///// Delete user by login.
+		///// </summary>
+		///// <param name="login">Login.</param>
+		///// <returns>Returns <see langword="true"/>, if user was deleted, otherwise return <see langword="false"/>.</returns>
+		//bool DeleteUser(string login);
 
 		/// <summary>
 		/// Check login and password.
@@ -64,23 +63,23 @@ namespace Ecng.Security
 			return Guid.NewGuid();
 		}
 
-		/// <inheritdoc />
-		public virtual void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses)
-		{
-			throw new NotSupportedException();
-		}
+		///// <inheritdoc />
+		//public virtual void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses)
+		//{
+		//	throw new NotSupportedException();
+		//}
 
-		/// <inheritdoc />
-		public void ChangePassword(string login, SecureString oldPassword, SecureString newPassword)
-		{
-			throw new NotSupportedException();
-		}
+		///// <inheritdoc />
+		//public void ChangePassword(string login, SecureString oldPassword, SecureString newPassword)
+		//{
+		//	throw new NotSupportedException();
+		//}
 
-		/// <inheritdoc />
-		public virtual bool DeleteUser(string login)
-		{
-			throw new NotSupportedException();
-		}
+		///// <inheritdoc />
+		//public virtual bool DeleteUser(string login)
+		//{
+		//	throw new NotSupportedException();
+		//}
 	}
 
 	/// <summary>
@@ -95,33 +94,33 @@ namespace Ecng.Security
 		{
 		}
 
-		/// <inheritdoc />
-		public virtual void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses)
-		{
-			if (WindowsIdentityManager.Validate(login, password))
-			{
-				if (!WindowsIdentityManager.ChangePassword(login, null, password))
-					throw new InvalidOperationException("User {0} not found.".Translate().Put(login));
-			}
-			else
-				WindowsIdentityManager.CreateUser(login, password);
-		}
+		///// <inheritdoc />
+		//public virtual void SaveUser(string login, SecureString password, IEnumerable<IPAddress> possibleAddresses)
+		//{
+		//	if (WindowsIdentityManager.Validate(login, password))
+		//	{
+		//		if (!WindowsIdentityManager.ChangePassword(login, null, password))
+		//			throw new InvalidOperationException("User {0} not found.".Translate().Put(login));
+		//	}
+		//	else
+		//		WindowsIdentityManager.CreateUser(login, password);
+		//}
 
-		/// <inheritdoc />
-		public void ChangePassword(string login, SecureString oldPassword, SecureString newPassword)
-		{
-			if (!WindowsIdentityManager.Validate(login, oldPassword))
-				throw new InvalidOperationException("User {0} not found.".Translate().Put(login));
+		///// <inheritdoc />
+		//public void ChangePassword(string login, SecureString oldPassword, SecureString newPassword)
+		//{
+		//	if (!WindowsIdentityManager.Validate(login, oldPassword))
+		//		throw new InvalidOperationException("User {0} not found.".Translate().Put(login));
 
-			if (!WindowsIdentityManager.ChangePassword(login, oldPassword, newPassword))
-				throw new InvalidOperationException("User {0} not found or password is incorrect.".Translate().Put(login));
-		}
+		//	if (!WindowsIdentityManager.ChangePassword(login, oldPassword, newPassword))
+		//		throw new InvalidOperationException("User {0} not found or password is incorrect.".Translate().Put(login));
+		//}
 
-		/// <inheritdoc />
-		public virtual bool DeleteUser(string login)
-		{
-			return WindowsIdentityManager.DeleteUser(login);
-		}
+		///// <inheritdoc />
+		//public virtual bool DeleteUser(string login)
+		//{
+		//	return WindowsIdentityManager.DeleteUser(login);
+		//}
 
 		/// <inheritdoc />
 		public virtual Guid ValidateCredentials(string login, SecureString password, IPAddress clientAddress)
@@ -130,6 +129,40 @@ namespace Ecng.Security
 				throw new UnauthorizedAccessException("User {0} not found or password is incorrect.".Translate().Put(login));
 
 			return Guid.NewGuid();
+		}
+	}
+
+	/// <summary>
+	/// The connection access check module which provides access by simple login and password set.
+	/// </summary>
+	public class SimpleAuthorization : AnonymousAuthorization
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SimpleAuthorization"/>.
+		/// </summary>
+		public SimpleAuthorization()
+		{
+		}
+
+		/// <summary>
+		/// Login.
+		/// </summary>
+		public string Login { get; set; }
+
+		/// <summary>
+		/// Password.
+		/// </summary>
+		public SecureString Password { get; set; }
+
+		/// <inheritdoc />
+		public override Guid ValidateCredentials(string login, SecureString password, IPAddress clientAddress)
+		{
+			if (Login.IsEmpty())
+				return base.ValidateCredentials(login, password, clientAddress);
+			else if (login.CompareIgnoreCase(Login) && password != null && Password != null && password.IsEqualTo(Password))
+				return Guid.NewGuid();
+
+			throw new UnauthorizedAccessException();
 		}
 	}
 }
