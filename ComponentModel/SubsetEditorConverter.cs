d940bf066e50e638b85c2ce66128de118fd88eed
Sergey Sokolov <sokolovsa@gmail.com>
2020-05-30 05:49:09 +0300

    task #119: type converter for subsetcombobox

---
+++ ComponentModel/SubsetEditorConverter.cs
@@ -0,0 +1,54 @@
+﻿namespace Ecng.ComponentModel
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Globalization;
+	using System.Linq;
+	using Ecng.Common;
+
+	/// <summary>
+	/// Converts from SubsetComboBox EditValue (<see cref="IEnumerable{Object}"/>) to source property type (<see cref="IEnumerable{T}"/>)
+	/// </summary>
+	public class SubsetEditorConverter : TypeConverter
+	{
+		/// <inheritdoc />
+		public override bool CanConvertFrom(ITypeDescriptorContext ctx, Type sourceType)
+		{
+			if(!typeof(IEnumerable).IsAssignableFrom(sourceType) || ctx.PropertyDescriptor == null)
+				return base.CanConvertFrom(ctx, sourceType);
+
+			var argType = TryGetIEnumerableArg(ctx.PropertyDescriptor.PropertyType);
+
+			return argType != null ||  base.CanConvertFrom(ctx, sourceType);
+		}
+
+		/// <inheritdoc />
+		public override object ConvertFrom(ITypeDescriptorContext ctx, CultureInfo culture, object value)
+		{
+			var argType = TryGetIEnumerableArg(ctx.PropertyDescriptor?.PropertyType);
+
+			if(value == null || argType == null)
+				return null;
+
+			var objects = ((IEnumerable)value).Cast<object>().ToHashSet().ToArray();
+			var arr = argType.CreateArray(objects.Length);
+
+			var isConvertible = typeof(IConvertible).IsAssignableFrom(argType);
+
+			for (var i = 0; i < arr.Length; ++i)
+				arr.SetValue(isConvertible ? Convert.ChangeType(objects[i], argType) : objects[i], i);
+
+			return arr;
+		}
+
+		static Type TryGetIEnumerableArg(Type type)
+		{
+			if(type == null || !type.IsGenericType || type.GetGenericTypeDefinition() != typeof(IEnumerable<>))
+				return null;
+
+			return type.GetGenericArguments()[0];
+		}
+	}
+}
