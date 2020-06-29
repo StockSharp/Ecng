e1476478d326c7c4efc2457f17ecc0cf69e2a9c9
mika@stocksharp.com <mika@stocksharp.com>
2020-06-30 02:28:30 +0700

    FileBrowserPicker. Xaml.Database reverted.

---
+++ Xaml/Database/DatabaseConnectionCreateWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿namespace Ecng.Xaml.Database
+{
+	using System;
+	using System.Windows;
+
+	using Ecng.Common;
+	using Ecng.Data;
+	using Ecng.Localization;
+
+	public partial class DatabaseConnectionCreateWindow
+	{
+		public DatabaseConnectionCreateWindow()
+		{
+			InitializeComponent();
+
+			Title = Title.Translate();
+			CheckCtrl.Content = ((string)CheckCtrl.Content).Translate();
+			Ok.Content = ((string)Ok.Content).Translate();
+
+			Connection = new DatabaseConnectionPair
+			{
+				Provider = new SqlServerDatabaseProvider()
+			};
+		}
+
+		public DatabaseConnectionPair Connection
+		{
+			get => SettingsGrid.Connection;
+			set => SettingsGrid.Connection = value;
+		}
+
+		private void TestCtrl_Click(object sender, RoutedEventArgs e)
+		{
+			Ok.IsEnabled = TestConnection();
+		}
+
+		private bool TestConnection(bool showMessageBox = true)
+		{
+			if (Connection.ConnectionString.IsEmpty())
+			{
+				if (showMessageBox)
+				{
+					new MessageBoxBuilder()
+						.Text("Cannot create a connection, because some data was not entered.".Translate())
+						.Error()
+						.Owner(this)
+						.Show();
+				}
+
+				return false;
+			}
+
+			using (var db = new Database("Test", Connection.ConnectionString) { Provider = Connection.Provider })
+			{
+				try
+				{
+					using (db.CreateConnection()) { }
+
+					if (showMessageBox)
+					{
+						new MessageBoxBuilder()
+							.Text("Connection successfully checked.".Translate())
+							.Owner(this)
+							.Show();
+					}
+
+					return true;
+				}
+				catch (Exception ex)
+				{
+					if (showMessageBox)
+					{
+						new MessageBoxBuilder()
+							.Text("Cannot connect.".Translate() + Environment.NewLine + ex)
+							.Error()
+							.Owner(this)
+							.Show();
+					}
+
+					return false;
+				}
+			}
+		}
+	}
+}
---
+++ Xaml/Database/DatabaseProviderEditor.cs
@@ -0,0 +1,18 @@
+namespace Ecng.Xaml.Database
+{
+	using DevExpress.Xpf.Editors.Settings;
+
+	/// <summary>
+	/// <see cref="ComboBoxEditSettings"/> for <see cref="DatabaseProviderComboBox"/>.
+	/// </summary>
+	public class DatabaseProviderEditor : ComboBoxEditSettings
+	{
+		private readonly DatabaseProviderComboBox _cb = new DatabaseProviderComboBox();
+
+		public DatabaseProviderEditor()
+		{
+			DisplayMember = _cb.DisplayMemberPath;
+			ItemsSource = _cb.ItemsSource;
+		}
+	}
+}
