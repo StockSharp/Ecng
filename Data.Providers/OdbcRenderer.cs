e8327a6e4867019a4cf9a9e17940edecee7021cb
mika@stocksharp.com <mika@stocksharp.com>
2020-07-25 18:04:17 +0700

    OdbcRenderer.

---
+++ Data.Providers/OdbcRenderer.cs
@@ -0,0 +1,24 @@
+﻿namespace Ecng.Data.Providers
+{
+	using Ecng.Common;
+	using Ecng.Serialization;
+
+	public class OdbcRenderer : SqlRenderer
+	{
+		public OdbcRenderer()
+			: base(typeof(OdbcRenderer).Name)
+		{
+		}
+
+		protected override string ParameterPrefix => "[?";
+
+		protected override string ParameterSuffix => "]";
+
+		public override string GetIdentitySelect(Schema schema)
+		{
+			return "@@identity as " + schema.Identity.Name;
+		}
+
+		protected override string[] ReservedWords => ArrayHelper.Empty<string>();
+	}
+}
