a6253f68fad190aabe5645906b43eb480a8e05d9
mika@stocksharp.com <mika@stocksharp.com>
2020-08-10 01:05:33 +0700

    NotifiableObject. Uses CallerMemberNameAttribute.

--- ComponentModel/NotifiableObject.cs
+++ ComponentModel/NotifiableObject.cs
@@ -3,6 +3,7 @@ namespace Ecng.ComponentModel
 	using System;
 	using System.ComponentModel;
 	using System.Runtime.Serialization;
+	using System.Runtime.CompilerServices;
 
 	using Ecng.Common;
 
@@ -17,17 +18,17 @@ namespace Ecng.ComponentModel
 		/// Raise event <see cref="INotifyPropertyChanged.PropertyChanged"/>.
 		/// </summary>
 		/// <param name="propertyName">Property name.</param>
-		public void NotifyPropertyChanged(string propertyName)
+		public void NotifyPropertyChanged([CallerMemberName]string propertyName = null)
 		{
 			PropertyChanged?.Invoke(this, propertyName);
 		}
 
-		protected void NotifyChanged(string propertyName)
+		protected void NotifyChanged([CallerMemberName]string propertyName = null)
 		{
 			this.Notify(propertyName);
 		}
 
-		protected void NotifyChanging(string propertyName)
+		protected void NotifyChanging([CallerMemberName]string propertyName = null)
 		{
 			PropertyChanging?.Invoke(this, propertyName);
 		}
