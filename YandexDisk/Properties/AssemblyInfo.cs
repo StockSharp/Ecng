e1795b0a3ef033c5ae2eb9d5da8db8d5c562230a
mika@stocksharp.com <mika@stocksharp.com>
2021-01-24 14:48:10 +0300

    YandexDisk

---
+++ YandexDisk/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ecng.YandexDisk")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55ED6439-B69D-4FCB-AF9C-5D790800C930")]
