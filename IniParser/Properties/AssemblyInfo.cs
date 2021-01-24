8180949db81b933fb81931a61f028f81e20c2390
mika@stocksharp.com <mika@stocksharp.com>
2021-01-24 13:56:45 +0300

    IniParser

---
+++ IniParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+#region Using Directives
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ecng.IniParser")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("DE5E71ED-C211-4937-BABB-153C15A61140")]
