ad39dc9a69d9e9cd4ae868bd677b78c6ca687f8d
Sergey Sokolov <sokolovsa@gmail.com>
2021-03-05 00:00:17 +0300

    maybe

---
+++ Common/Maybe.cs
@@ -0,0 +1,65 @@
+﻿namespace Ecng.Common
+{
+	using System;
+
+	public static class MayBe
+	{
+		public static TR With<TI, TR>(this TI input, Func<TI, TR> evaluator)
+			where TI : class
+			where TR : class
+		{
+			if (input == null)
+				return null;
+			return evaluator(input);
+		}
+
+		public static TR WithString<TR>(this string input, Func<string, TR> evaluator)
+			where TR : class
+		{
+			if (string.IsNullOrEmpty(input))
+				return null;
+			return evaluator(input);
+		}
+
+		public static TR Return<TI, TR>(this TI? input, Func<TI?, TR> evaluator, Func<TR> fallback) where TI : struct
+		{
+			if (!input.HasValue)
+				return fallback != null ? fallback() : default;
+			return evaluator(input.Value);
+		}
+
+		public static TR Return<TI, TR>(this TI input, Func<TI, TR> evaluator, Func<TR> fallback) where TI : class
+		{
+			if (input == null)
+				return fallback != null ? fallback() : default;
+			return evaluator(input);
+		}
+
+		public static bool ReturnSuccess<TI>(this TI input) where TI : class
+		{
+			return input != null;
+		}
+
+		public static TI If<TI>(this TI input, Func<TI, bool> evaluator) where TI : class
+		{
+			if (input == null)
+				return null;
+			return evaluator(input) ? input : null;
+		}
+
+		public static TI IfNot<TI>(this TI input, Func<TI, bool> evaluator) where TI : class
+		{
+			if (input == null)
+				return null;
+			return evaluator(input) ? null : input;
+		}
+
+		public static TI Do<TI>(this TI input, Action<TI> action) where TI : class
+		{
+			if (input == null)
+				return null;
+			action(input);
+			return input;
+		}
+	}
+}
