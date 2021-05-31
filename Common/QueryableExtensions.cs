513da53161126c94f5370d747f18e39d67f1387d
mika@stocksharp.com <mika@stocksharp.com>
2021-05-31 20:11:54 +0300

    QueryableExtensions

---
+++ Common/QueryableExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Ecng.Common
+{
+	using System;
+	using System.Linq;
+	using System.Linq.Expressions;
+
+	public static class QueryableExtensions
+	{
+		public static IOrderedQueryable<T> OrderByAscending<T>(this IQueryable<T> source, string propertyName)
+        {
+            return source.OrderBy(ToLambda<T>(propertyName));
+        }
+
+        public static IOrderedQueryable<T> OrderByDescending<T>(this IQueryable<T> source, string propertyName)
+        {
+            return source.OrderByDescending(ToLambda<T>(propertyName));
+        }
+
+        private static Expression<Func<T, object>> ToLambda<T>(string propertyName)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+            var property = Expression.Property(parameter, propertyName);
+            var propAsObject = Expression.Convert(property, typeof(object));
+
+            return Expression.Lambda<Func<T, object>>(propAsObject, parameter);            
+        }
+	}
+}
