705445112c0e8a66adac67d2e9ecc0eed6662904
mika@stocksharp.com <mika@stocksharp.com>
2021-08-06 02:15:09 +0300

    Removed old code.

--- UnitTesting/Properties/AssemblyInfo.cs
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Runtime.InteropServices;
-using System.Security;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b5663fb4-8f9d-4cd2-b8d0-e8c7aa93a2ea")]
-[assembly: SecurityRules(SecurityRuleSet.Level1)]
