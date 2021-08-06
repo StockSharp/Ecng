224b4760d70e19de2f16b4a06a60db89969ebe9a
mika@stocksharp.com <mika@stocksharp.com>
2021-08-06 13:09:07 +0300

    Fix commit 382c09fccfaae04f2a2b4f91f88a13b47ecd7601

--- Reflection.Aspects/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("86c9d85b-9623-46d5-94c4-54e1528e94e9")]
