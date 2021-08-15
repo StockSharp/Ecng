59a2b8284c1dff0aa4030096f6bcee8698182582
mika@stocksharp.com <mika@stocksharp.com>
2021-08-15 19:21:15 +0300

    SpeechPlayer is obsolete.

--- ComponentModel/SpeechPlayer.cs
+++ ComponentModel/SpeechPlayer.cs
@@ -2,25 +2,11 @@
 {
 	using Ecng.Common;
 
+	[System.Obsolete]
 	public class SpeechPlayer : Disposable
 	{
-#if NETCOREAPP || NETSTANDARD
 		public int Volume { get; set; }
 
 		public void Say(string text) { }
-#else
-		private readonly System.Speech.Synthesis.SpeechSynthesizer _synthesizer = new System.Speech.Synthesis.SpeechSynthesizer();
-
-		public int Volume
-		{
-			get => _synthesizer.Volume;
-			set => _synthesizer.Volume = value;
-		}
-
-		public void Say(string text)
-		{
-			_synthesizer.Speak(text);
-		}
-#endif
 	}
 }
