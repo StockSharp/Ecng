4331592b3c0d7c15d8eeabf0fc67a1f8c99950c3
mika@stocksharp.com <mika@stocksharp.com>
2021-08-31 21:33:48 +0300

    LazyTests

---
+++ EcngTest/ComponentModel/LazyTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Ecng.Test.ComponentModel
+{
+	using System;
+	
+	using Ecng.Common;
+	using Ecng.ComponentModel;
+	using Ecng.UnitTesting;
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class LazyTests
+	{
+		[TestMethod]
+		public void TrackReset()
+		{
+			var invokeCount = 0;
+
+			string GetValue()
+				=> (++invokeCount).To<string>();
+
+			var lazy = new Lazy<string>(GetValue);
+			lazy.Track();
+			lazy.Value.AssertEqual("1");
+			lazy.Value.AssertEqual("1");
+
+			lazy.Reset();
+			lazy.Value.AssertEqual("2");
+		}
+	}
+}
