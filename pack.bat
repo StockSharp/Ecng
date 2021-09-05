64914a687345f36fa1d77883903ee3efbcbb2111
mika@stocksharp.com <mika@stocksharp.com>
2021-09-05 10:33:28 +0300

    Removed NuGet.Config

--- pack.bat
+++
@@ -1,6 +0,0 @@
-"c:\program files\dotnet\dotnet.exe" pack -o pub /p:Version=1.0.31 -c Release --no-build Ecng.sln
-rd /S /Q pub\out
-pub\nuget.exe init pub pub\out -Expand
-xcopy "pub\out\*.*" "..\StockSharp (GitHub)\packages\" /K /H /Y /E
-rd /S /Q pub\out
-del "pub\*.nupkg"
