82f1ee8d3fb72d6054164feb6fef5c359c6dc360
mika@stocksharp.com <mika@stocksharp.com>
2021-09-05 21:45:55 +0300

    IniParser removed.

--- IniParser/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("DE5E71ED-C211-4937-BABB-153C15A61140")]
