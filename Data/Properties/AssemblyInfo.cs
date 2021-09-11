ab5b8bfaf33af8a27f2fd2a6a647d1aa3a0b867f
mika@stocksharp.com <mika@stocksharp.com>
2021-09-11 12:52:20 +0300

    Fix commit 0489e4de2e3a984a09b17ed70000a64c3412ff50

---
+++ Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0F5D95EF-F816-447A-B517-DD8E1DA6987F")]
