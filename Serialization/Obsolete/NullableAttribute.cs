7d82cad6d6865f7fbd0b7aeabcdfb155922e31dd
mika@stocksharp.com <mika@stocksharp.com>
2021-09-28 11:44:36 +0300

    Restored NullableAttribute for backward compilation.

---
+++ Serialization/Obsolete/NullableAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecng.Serialization
+{
+	using System;
+
+	using Ecng.Reflection;
+
+	[Obsolete("NullableAttribute no more required.")]
+	[AttributeUsage(ReflectionHelper.Members)]
+	public class NullableAttribute : Attribute
+	{
+	}
+}
