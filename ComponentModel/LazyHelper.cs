0b533575b40ecc1f07ab4804e982843b93814566
mika@stocksharp.com <mika@stocksharp.com>
2021-09-28 12:52:37 +0300

    LazyHelper supported .NET FW https://stocksharp.com/topic/16115/exception-in-ecngreflectiondll/

--- ComponentModel/LazyHelper.cs
+++ ComponentModel/LazyHelper.cs
@@ -2,15 +2,14 @@
 {
 	using System;
 
+	using Ecng.Common;
 	using Ecng.Collections;
 	using Ecng.Reflection;
 
 	public static class LazyHelper
 	{
 		private static readonly SynchronizedDictionary<object, Delegate> _factories = new(); 
-#if NETCOREAPP || NETSTANDARD
 		private static readonly SynchronizedDictionary<object, object> _states = new(); 
-#endif
 
 		public static Lazy<T> Track<T>(this Lazy<T> lazy)
 		{
@@ -20,12 +19,14 @@
 			if (lazy.IsValueCreated)
 				throw new ArgumentException(nameof(lazy));
 
-#if !NETCOREAPP && !NETSTANDARD
-			_factories.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, Func<T>>("m_valueFactory", null));
-#else
-			_states.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, object>("_state", null));
-			_factories.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, Func<T>>("_factory", null));
-#endif
+			if (OperatingSystemEx.IsFramework)
+				_factories.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, Func<T>>("m_valueFactory", null));
+			else
+			{
+				_states.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, object>("_state", null));
+				_factories.Add(lazy, lazy.GetValue<Lazy<T>, VoidType, Func<T>>("_factory", null));
+			}
+
 			return lazy;
 		}
 
@@ -36,13 +37,16 @@
 
 			var factory = _factories[lazy];
 
-#if !NETCOREAPP && !NETSTANDARD
-			lazy.SetValue("m_boxed", (object)null);
-			lazy.SetValue("m_valueFactory", factory);
-#else
-			lazy.SetValue("_state", _states[lazy]);
-			lazy.SetValue("_factory", factory);
-#endif
+			if (OperatingSystemEx.IsFramework)
+			{
+				lazy.SetValue("m_boxed", (object)null);
+				lazy.SetValue("m_valueFactory", factory);
+			}
+			else
+			{
+				lazy.SetValue("_state", _states[lazy]);
+				lazy.SetValue("_factory", factory);
+			}
 		}
 	}
 }
