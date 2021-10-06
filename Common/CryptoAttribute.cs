bd3b436c81dafc1fd2ea2bb5cbcb36a9229664e5
mika@stocksharp.com <mika@stocksharp.com>
2021-10-06 15:06:45 +0300

    CurrencyTypes enum.

---
+++ Common/CryptoAttribute.cs
@@ -0,0 +1,11 @@
+namespace Ecng.Common
+{
+	using System;
+
+	/// <summary>
+	/// The member marked as associated with crypto currency.
+	/// </summary>
+	public class CryptoAttribute : Attribute
+	{
+	}
+}
