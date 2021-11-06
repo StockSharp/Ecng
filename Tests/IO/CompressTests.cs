ef9b658ab14ad89c4ea439d06e040fc10fa123f7
mika@stocksharp.com <mika@stocksharp.com>
2021-11-06 16:57:23 +0300

    Fix commit 81400a1a9b43649cf567d3f959e6fb3682f24f2f

---
+++ Tests/IO/CompressTests.cs
@@ -0,0 +1,90 @@
+﻿namespace Ecng.Tests.IO
+{
+	using System.IO;
+	using System.IO.Compression;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	using Ecng.Common;
+	using Ecng.IO;
+	using Ecng.UnitTesting;
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class CompressTests
+	{
+		[TestMethod]
+		public void Deflate()
+		{
+			var bytes = RandomGen.GetBytes(1024 * 1024);
+
+			bytes.DeflateTo().DeflateFrom().SequenceEqual(bytes).AssertTrue();
+			bytes.Compress<DeflateStream>().DeflateFrom().SequenceEqual(bytes).AssertTrue();
+			bytes.Compress<DeflateStream>().Uncompress<DeflateStream>().SequenceEqual(bytes).AssertTrue();
+		}
+
+		[TestMethod]
+		public void GZip()
+		{
+			var bytes = RandomGen.GetBytes(1024 * 1024);
+
+			bytes.Compress<GZipStream>().Uncompress<GZipStream>().SequenceEqual(bytes).AssertTrue();
+		}
+
+		[TestMethod]
+		public void GZipStr()
+		{
+			var str = "Hello world";
+			str.UTF8().Compress<GZipStream>().UnGZip().AssertEqual(str);
+		}
+
+		[TestMethod]
+		public void GZipToBuffer()
+		{
+			var bytes = RandomGen.GetBytes(1024);
+			var rangeCount = 100;
+			var destination = new byte[rangeCount * 2];
+			var range = bytes.Compress<GZipStream>(count: rangeCount);
+			var count = range.UnGZip(0, range.Length, destination);
+			count.AssertEqual(rangeCount);
+			bytes.Take(count).SequenceEqual(destination.Take(rangeCount)).AssertTrue();
+		}
+
+		[TestMethod]
+		public void UnDeflateToBuffer()
+		{
+			var bytes = RandomGen.GetBytes(1024);
+			var rangeCount = 100;
+			var destination = new byte[rangeCount * 2];
+			var range = bytes.Compress<DeflateStream>(count: rangeCount);
+			var count = range.UnDeflate(0, range.Length, destination);
+			count.AssertEqual(rangeCount);
+			bytes.Take(count).SequenceEqual(destination.Take(rangeCount)).AssertTrue();
+		}
+
+		[TestMethod]
+		public void Zip7()
+		{
+			var bytes = RandomGen.GetBytes(1024 * 1024);
+
+			// TODO 7Zip compression not implemented
+			//bytes.Do7Zip().Un7Zip().SequenceEqual(bytes).AssertTrue();
+			//bytes.Compress<Lzma.LzmaStream>().Uncompress<Lzma.LzmaStream>().SequenceEqual(bytes).AssertTrue();
+		}
+
+		[TestMethod]
+		public async Task Async()
+		{
+			var bytes = RandomGen.GetBytes(1024 * 1024);
+
+			async Task Do<TCompress>()
+				where TCompress : Stream
+				=> (await (await bytes.CompressAsync<TCompress>()).UncompressAsync<TCompress>()).SequenceEqual(bytes).AssertTrue();
+
+			await Do<DeflateStream>();
+			await Do<GZipStream>();
+			//await Do<Lzma.LzmaStream>();
+		}
+	}
+}
