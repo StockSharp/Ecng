1cc3bd32543c3f621a470e357774cbb830b47ffb
mika@stocksharp.com <mika@stocksharp.com>
2022-03-27 23:38:50 +0300

    Fix commit b8faac55bf756b987420bd50ed302aac647ae25d

--- Serialization/ISerializerExtensions.cs
+++ Serialization/ISerializerExtensions.cs
@@ -8,26 +8,17 @@
 
 	public static class ISerializerExtensions
 	{
-		public static byte[] Serialize<T>(this ISerializer<T> serializer, object graph)
-			=> serializer.CheckOnNull(nameof(serializer)).Serialize((T)graph);
-
-		public static void Serialize<T>(this ISerializer<T> serializer, object graph, string fileName)
-			=> serializer.CheckOnNull(nameof(serializer)).Serialize((T)graph, fileName);
-
-		public static void Serialize<T>(this ISerializer<T> serializer, object graph, Stream stream)
-			=> serializer.CheckOnNull(nameof(serializer)).Serialize((T)graph, stream);
-
-		public static void Serialize<T>(this ISerializer<T> serializer, T graph, string fileName)
+		public static void Serialize(this ISerializer serializer, object graph, string fileName)
 			=> File.WriteAllBytes(fileName, serializer.CheckOnNull(nameof(serializer)).Serialize(graph));
 
-		public static byte[] Serialize<T>(this ISerializer<T> serializer, T graph)
+		public static byte[] Serialize(this ISerializer serializer, object graph)
 		{
 			var stream = new MemoryStream();
 			serializer.CheckOnNull(nameof(serializer)).Serialize(graph, stream);
 			return stream.To<byte[]>();
 		}
 
-		public static void Serialize<T>(this ISerializer<T> serializer, T graph, Stream stream)
+		public static void Serialize(this ISerializer serializer, object graph, Stream stream)
 			=> AsyncContext.Run(() => serializer.CheckOnNull(nameof(serializer)).SerializeAsync(graph, stream, default));
 
 		public static T Deserialize<T>(this ISerializer<T> serializer, string fileName)
@@ -45,6 +36,13 @@
 		public static T Deserialize<T>(this ISerializer<T> serializer, Stream stream)
 			=> AsyncContext.Run(() => serializer.CheckOnNull(nameof(serializer)).DeserializeAsync(stream, default));
 
+		public static object Deserialize(this ISerializer serializer, byte[] data)
+		{
+			var stream = new MemoryStream(data);
+			return serializer.CheckOnNull(nameof(serializer)).Deserialize(stream);
+		}
 
+		public static object Deserialize(this ISerializer serializer, Stream stream)
+			=> AsyncContext.Run(() => serializer.CheckOnNull(nameof(serializer)).DeserializeAsync(stream, default));
 	}
 }
