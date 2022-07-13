5dd170c13681a7988e8fe7f2d343e9aa8512434a
mika@stocksharp.com <mika@stocksharp.com>
2022-07-13 15:50:13 +0300

    LikeCompares

---
+++ ComponentModel/LikeCompares.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecng.ComponentModel
+{
+	public enum LikeCompares
+	{
+		Contains,
+		StartWith,
+		EndWith,
+		Equals,
+		NotContains,
+	}
+}
---
+++ ComponentModel/LikeComparesExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace Ecng.ComponentModel
+{
+	using System;
+
+	using Ecng.Common;
+
+	public static class LikeComparesExtensions
+	{
+		public static bool Like(this string value, string like, LikeCompares? likeCompare)
+		{
+			if (value is null)
+				throw new ArgumentNullException(nameof(value));
+
+			if (like.IsEmpty())
+				return true;
+
+			return likeCompare switch
+			{
+				LikeCompares.Contains or null => value.ContainsIgnoreCase(like),
+				LikeCompares.StartWith => value.StartsWithIgnoreCase(like),
+				LikeCompares.EndWith => value.EndsWithIgnoreCase(like),
+				LikeCompares.Equals => value.EqualsIgnoreCase(like),
+				LikeCompares.NotContains => !value.ContainsIgnoreCase(like),
+				_ => throw new ArgumentOutOfRangeException(nameof(likeCompare)),
+			};
+		}
+
+		public static string ToExpression(this string like, LikeCompares? likeCompare = default)
+		{
+			if (like.IsEmpty())
+				throw new ArgumentNullException(nameof(like));
+
+			return likeCompare switch
+			{
+				LikeCompares.Contains or null => $"%{like}%",
+				LikeCompares.StartWith => $"{like}%",
+				LikeCompares.EndWith => $"%{like}",
+				LikeCompares.Equals => like,
+				LikeCompares.NotContains => throw new NotSupportedException(),
+				_ => throw new ArgumentOutOfRangeException(nameof(likeCompare)),
+			};
+		}
+	}
+}
