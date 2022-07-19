75151f9975220759c74747d62b515a55823671cf
mika@stocksharp.com <mika@stocksharp.com>
2022-07-19 11:16:21 +0300

    RandomGen. GetLong, GetString, GetDate.

--- Common/RandomGen.cs
+++ Common/RandomGen.cs
@@ -4,6 +4,7 @@ namespace Ecng.Common
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Globalization;
+	using System.Runtime.CompilerServices;
 
 	public static class RandomGen
 	{
@@ -47,39 +48,100 @@ namespace Ecng.Common
 				return _value.Next(min, max + 1);
 		}
 
-		public static bool GetBool()
+		public static long GetLong() => GetLong(long.MinValue, long.MaxValue);
+
+		public static long GetLong(long min, long max)
 		{
-			return GetInt(1) == 1;
+			//if (min >= int.MinValue && max <= int.MaxValue)
+			//	return GetInt((int)min, (int)max);
+
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			static int Log2Ceiling(ulong value)
+			{
+				[MethodImpl(MethodImplOptions.AggressiveInlining)]
+				static int NumberOfSetBits(ulong i)
+				{
+					i -= (i >> 1) & 0x5555555555555555UL;
+					i = (i & 0x3333333333333333UL) + ((i >> 2) & 0x3333333333333333UL);
+					return (int)(unchecked(((i + (i >> 4)) & 0xF0F0F0F0F0F0F0FUL) * 0x101010101010101UL) >> 56);
+				}
+
+				var result = (int)Math.Log(value, 2);
+
+				if (NumberOfSetBits(value) != 1)
+					result++;
+
+				return result;
+			}
+
+			static ulong GetULong()
+			{
+				lock (_sync)
+				{
+					return ((ulong)(uint)_value.Next(1 << 22)) |
+					(((ulong)(uint)_value.Next(1 << 22)) << 22) |
+					(((ulong)(uint)_value.Next(1 << 20)) << 44);
+				}
+			}
+
+			var exclusiveRange = (ulong)(max - min);
+
+			if (exclusiveRange > 1)
+			{
+				// Narrow down to the smallest range [0, 2^bits] that contains maxValue - minValue
+				// Then repeatedly generate a value in that outer range until we get one within the inner range.
+				var bits = Log2Ceiling(exclusiveRange);
+
+				while (true)
+				{
+					var result = GetULong() >> (sizeof(long) * 8 - bits);
+
+					if (result < exclusiveRange)
+						return (long)result + min;
+				}
+			}
+
+			return min + GetInt((int)exclusiveRange);
 		}
+
+		public static bool GetBool() => GetInt(1) == 1;
 
 		public static T GetEnum<T>()
 			where T : struct
-		{
-			return GetEnum(Enumerator.GetValues<T>());
-		}
+			=> GetEnum(Enumerator.GetValues<T>());
 
 		public static T GetEnum<T>(IEnumerable<T> values)
 			where T : struct
-		{
-			return GetInt(0, values.Max(value => value.To<int>())).To<T>();
-		}
+			=> GetEnum(default, values.Max(value => value.To<long>()).To<T>());
+
+		public static T GetEnum<T>(T min, T max)
+			where T : struct
+			=> GetLong(min.To<long>(), max.To<long>()).To<T>();
 
 		public static T GetElement<T>(IEnumerable<T> array)
-		{
-			return array.ElementAt(GetInt(0, array.Count() - 1));
-		}
+			=> array.ElementAt(GetInt(0, array.Count() - 1));
+
+		public static string GetString(int min, int max)
+			=> TypeHelper.GenerateSalt(GetInt(min, max)).Base64();
+
+		public static DateTime GetDate()
+			=> GetDate(DateTime.MinValue, DateTime.MaxValue);
+
+		public static DateTime GetDate(DateTime min, DateTime max)
+			=> min + GetTime(default, max - min);
+
+		public static TimeSpan GetTime()
+			=> GetTime(TimeSpan.MinValue, TimeSpan.MaxValue);
 
 		public static TimeSpan GetTime(TimeSpan min, TimeSpan max)
-		{
-			return TimeSpan.FromTicks(GetInt(0, (int)(max.Ticks - min.Ticks)) + min.Ticks);
-		}
+			=> GetLong(min.Ticks, max.Ticks).To<TimeSpan>();
 
 		public static decimal GetDecimal(int integer = 8, int fractional = 8)
 		{
 			for (var k = 0; k < 10; k++)
 			{
-				var i1 = Enumerable.Repeat(9, RandomGen.GetInt(1, integer)).Select(i => RandomGen.GetInt(9).ToString()).Join("").To<long>();
-				var i2 = Enumerable.Repeat(9, RandomGen.GetInt(1, integer)).Select(i => RandomGen.GetInt(9).ToString()).Join("").To<long>();
+				var i1 = Enumerable.Repeat(9, GetInt(1, integer)).Select(i => GetInt(9).ToString()).Join(string.Empty).To<long>();
+				var i2 = Enumerable.Repeat(9, GetInt(1, integer)).Select(i => GetInt(9).ToString()).Join(string.Empty).To<long>();
 				var value = decimal.Parse(i1 + "." + i2, CultureInfo.InvariantCulture);
 
 				if (value != 0)
---
+++ Tests/Common/RandomTests.cs
@@ -0,0 +1,89 @@
+﻿namespace Ecng.Tests.Common
+{
+	using System;
+	using System.Linq;
+
+	using Ecng.Common;
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class RandomTests
+	{
+		[TestMethod]
+		public void Int()
+		{
+			for (int i = 0; i < 10000; i++)
+			{
+				RandomGen.GetInt();
+				RandomGen.GetInt(0, 1000);
+				RandomGen.GetInt(int.MinValue, 0);
+				RandomGen.GetInt(int.MinValue, int.MaxValue);
+			}
+		}
+
+		[TestMethod]
+		public void Long()
+		{
+			for (int i = 0; i < 10000; i++)
+			{
+				RandomGen.GetLong();
+				RandomGen.GetLong(0, 1000);
+				RandomGen.GetLong(long.MinValue, 0);
+				RandomGen.GetLong(long.MinValue, long.MaxValue);
+			}
+		}
+
+		[TestMethod]
+		public void Date()
+		{
+			for (int i = 0; i < 10000; i++)
+			{
+				RandomGen.GetDate();
+				RandomGen.GetDate(new DateTime(2020, 1, 1), DateTime.UtcNow);
+			}
+		}
+
+		[TestMethod]
+		public void Time()
+		{
+			for (int i = 0; i < 10000; i++)
+			{
+				RandomGen.GetTime();
+				RandomGen.GetTime(TimeSpan.Zero, TimeSpan.MaxValue);
+				RandomGen.GetTime(TimeSpan.MinValue, TimeSpan.Zero);
+				RandomGen.GetTime(TimeSpan.MinValue, TimeSpan.MaxValue);
+			}
+		}
+
+		[TestMethod]
+		public void String()
+		{
+			for (int i = 0; i < 10000; i++)
+			{
+				RandomGen.GetString(0, RandomGen.GetInt(0, 1000));
+			}
+		}
+
+		[TestMethod]
+		public void Decimal()
+		{
+			for (int i = 0; i < 10000; i++)
+			{
+				RandomGen.GetDecimal(RandomGen.GetInt(1, 8), RandomGen.GetInt(0, 8));
+			}
+		}
+
+		[TestMethod]
+		public void Enum()
+		{
+			var seed = Enumerator.GetValues<CurrencyTypes>();
+			var max = seed.Last();
+
+			for (int i = 0; i < 10000; i++)
+			{
+				RandomGen.GetEnum(default, max);
+			}
+		}
+	}
+}
