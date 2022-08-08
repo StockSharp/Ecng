0dace916ef2ad2bba166b2b62f56a3565e89705d
mika@stocksharp.com <mika@stocksharp.com>
2022-08-08 13:12:01 +0300

    Nuget project.

---
+++ Nuget/Extensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Ecng.Nuget
+{
+	using System.Linq;
+
+	using NuGet.Packaging;
+
+	public static class Extensions
+	{
+		public static string[] GetTargetFrameworks(this PackageArchiveReader reader)
+		{
+			var targetFrameworks = reader
+				.GetSupportedFrameworks()
+				.Select(f => f.GetShortFolderName())
+				.ToList();
+
+			// Default to the "any" framework if no frameworks were found.
+			if (targetFrameworks.Count == 0)
+			{
+				targetFrameworks.Add("any");
+			}
+
+			return targetFrameworks.ToArray();
+		}
+	}
+}
