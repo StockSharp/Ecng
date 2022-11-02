3f4253b9838e14d37550ab998a9714adf820a2ee
mika@stocksharp.com <mika@stocksharp.com>
2022-11-02 20:36:00 +0300

    FileSizesExtensions

---
+++ Common/FileSizesExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecng.Common;
+
+public static class FileSizesExtensions
+{
+	public static long ToKB(this long bytes) => (long)((double)bytes / FileSizes.KB);
+	public static long ToMB(this long bytes) => (long)((double)bytes / FileSizes.MB);
+
+	public static long FromKB(this long kbytes) => kbytes * FileSizes.KB;
+	public static long FromMB(this long mbytes) => mbytes * FileSizes.MB;
+}
