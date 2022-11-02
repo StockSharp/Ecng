8b393d0e0f5f1439bfce0bd1c7f35a3892eadddd
mika@stocksharp.com <mika@stocksharp.com>
2022-11-02 20:37:45 +0300

    DisableNugetConfig method.

--- Nuget/Extensions.cs
+++ Nuget/Extensions.cs
@@ -2,11 +2,14 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Collections.Generic;
 
 using NuGet.Common;
 using NuGet.Versioning;
 using NuGet.Packaging;
 using NuGet.Protocol.Core.Types;
+using NuGet.Configuration;
 
 namespace Ecng.Nuget;
 
@@ -37,5 +40,49 @@ public static class Extensions
 		Func<NuGetVersion, bool> cond = allowPreview ? _ => true : v => !v.IsPrerelease;
 
 		return versions.LastOrDefault(cond);
+	}
+
+	private class DummySettings : ISettings
+	{
+		private class MockSettingSection : SettingSection
+		{
+			public MockSettingSection(string name, IReadOnlyDictionary<string, string> attributes, IEnumerable<SettingItem> children)
+				: base(name, attributes, children)
+			{
+			}
+
+			public MockSettingSection(string name, params SettingItem[] children)
+				: base(name, attributes: null, children: new HashSet<SettingItem>(children))
+			{
+			}
+
+			public override SettingBase Clone()
+				=> throw new NotSupportedException();
+		}
+
+		event EventHandler ISettings.SettingsChanged
+		{
+			add { }
+			remove { }
+		}
+
+		void ISettings.AddOrUpdate(string sectionName, SettingItem item) => throw new NotSupportedException();
+		IList<string> ISettings.GetConfigFilePaths() => throw new NotSupportedException();
+		IList<string> ISettings.GetConfigRoots() => throw new NotSupportedException();
+		SettingSection ISettings.GetSection(string sectionName) => new MockSettingSection(sectionName);
+		void ISettings.Remove(string sectionName, SettingItem item) => throw new NotSupportedException();
+		void ISettings.SaveToDisk() => throw new NotSupportedException();
+	}
+
+	public static void DisableNugetConfig()
+	{
+		// disable access nuget.config file
+
+		var proxy = new ProxyCache(new DummySettings(), EnvironmentVariableWrapper.Instance);
+
+		var f = typeof(ProxyCache).GetField("_instance", BindingFlags.Static | BindingFlags.NonPublic);
+		var lazy = (Lazy<ProxyCache>)f.GetValue(null);
+		lazy.GetType().GetField("_state", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(lazy, null);
+		lazy.GetType().GetField("_value", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(lazy, proxy);
 	}
 }
