2c36faffac210133b3daaaf0f7dc5e88f5b9c78a
mika@stocksharp.com <mika@stocksharp.com>
2022-11-06 09:53:15 +0300

    Brush

---
+++ Drawing/Brush.cs
@@ -0,0 +1,43 @@
+﻿namespace Ecng.Drawing
+{
+	using System;
+	using System.Drawing;
+
+	using Ecng.Common;
+
+	public abstract class Brush
+	{
+		protected Brush()
+		{
+		}
+	}
+
+	public class SolidBrush : Brush
+	{
+		public SolidBrush(Color color)
+		{
+			Color = color;
+		}
+
+		public Color Color { get; }
+	}
+
+	public class LinearGradientBrush : Brush
+	{
+		public LinearGradientBrush(Point stop0, Point stop1, Color color0, Color color1)
+		{
+			LinearColors = new[] { color0, color1 };
+			Rectangle = new(stop0, new((stop1.X - stop0.X).Abs(), (stop1.Y - stop0.Y).Abs()));
+		}
+
+		public LinearGradientBrush(Color[] linearColors, Rectangle rectangle)
+		{
+			LinearColors = linearColors ?? throw new ArgumentNullException(nameof(linearColors));
+			Rectangle = rectangle;
+		}
+
+		public Color[] LinearColors { get; }
+
+		public Rectangle Rectangle { get; }
+	}
+}
