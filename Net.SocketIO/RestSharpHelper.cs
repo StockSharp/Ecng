fe2d25a5df08549d672dab8e0b8f0fca7faf1f3a
Sergey Sokolov <sokolovsa@gmail.com>
2022-11-10 17:54:24 +0400

    ss-188: updated restsharp to v108

--- Net.SocketIO/RestSharpHelper.cs
+++ Net.SocketIO/RestSharpHelper.cs
@@ -1,93 +1,158 @@
-﻿namespace Ecng.Net
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+
+using Ecng.Common;
+using Ecng.Serialization;
+using Ecng.Collections;
+
+using Nito.AsyncEx;
+
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Ecng.Net;
+
+public static class RestSharpHelper
 {
-	using System;
-	using System.Threading;
-	using System.Threading.Tasks;
-	using System.Linq;
-	using System.Net;
-	using System.Reflection;
-
-	using Ecng.Common;
-	using Ecng.Serialization;
-
-	using Nito.AsyncEx;
-
-	using RestSharp;
-
-	public static class RestSharpHelper
+	public class UnexpectedResponseError : InvalidOperationException
 	{
-		public class UnexpectedResponseError : InvalidOperationException
+		public RestResponse Response { get; }
+
+		public UnexpectedResponseError(RestResponse response) : base($"unexpected response code='{response.StatusCode}', msg='{response.ErrorMessage}', desc='{response.StatusDescription}', content='{response.Content}'")
+			=> Response = response;
+	}
+
+	class AuthenticatorWrapper : IAuthenticator
+	{
+		private readonly SynchronizedDictionary<RestRequest, IAuthenticator> _authenticators = new();
+
+		class Holder : Disposable
 		{
-			public IRestResponse Response { get; }
+			private readonly RestRequest _request;
+			private readonly AuthenticatorWrapper _parent;
 
-			public UnexpectedResponseError(IRestResponse response) : base($"unexpected response code='{response.StatusCode}', msg='{response.ErrorMessage}', desc='{response.StatusDescription}', content='{response.Content}'")
-				=> Response = response;
-		}
-
-		public static void AddBodyAsStr(this IRestRequest request, string bodyStr)
-		{
-			if (request is null)
-				throw new ArgumentNullException(nameof(request));
-
-			request.AddParameter(RestSharp.Serialization.ContentType.Json, bodyStr, ParameterType.RequestBody);
-		}
-
-		public static object Invoke(this IRestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, Action<IRestClient> init = null, Func<string, string> contentConverter = null)
-			=> request.Invoke<object>(url, caller, logVerbose, init, contentConverter);
-
-		public static Task<object> InvokeAsync(this IRestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, CancellationToken token, Action<IRestClient> init = null, Func<string, string> contentConverter = null)
-			=> request.InvokeAsync<object>(url, caller, logVerbose, token, init, contentConverter);
-
-		public static T Invoke<T>(this IRestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, Action<IRestClient> init = null, Func<string, string> contentConverter = null)
-			=> AsyncContext.Run(() => request.InvokeAsync<T>(url, caller, logVerbose, CancellationToken.None, init, contentConverter));
-
-		public static async Task<T> InvokeAsync<T>(this IRestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, CancellationToken token, Action<IRestClient> init = null, Func<string, string> contentConverter = null)
-		{
-			if (request is null)
-				throw new ArgumentNullException(nameof(request));
-
-			if (url is null)
-				throw new ArgumentNullException(nameof(url));
-
-			if (caller is null)
-				throw new ArgumentNullException(nameof(caller));
-
-			var asm = caller.GetType().Assembly;
-			var projName = asm.GetAttribute<AssemblyProductAttribute>()?.Product;
-
-			var client = new RestClient(url)
+			public Holder(AuthenticatorWrapper wrapper, RestRequest req, IAuthenticator auth)
 			{
-				UserAgent = $"{projName}/" + asm.GetName().Version
+				_parent = wrapper ?? throw new ArgumentNullException(nameof(wrapper));
+				_request = req ?? throw new ArgumentNullException(nameof(req));
+				_parent._authenticators.Add(_request, auth);
+			}
+
+			protected override void DisposeManaged()
+			{
+				_parent._authenticators.Remove(_request);
+				base.DisposeManaged();
+			}
+		}
+
+		public IDisposable RegisterRequest(RestRequest req, IAuthenticator authenticator) => new Holder(this, req, authenticator);
+
+		ValueTask IAuthenticator.Authenticate(RestClient client, RestRequest request)
+			=> _authenticators.TryGetValue(request, out var auth) && auth != null ? auth.Authenticate(client, request) : default;
+	}
+
+	private static readonly SynchronizedDictionary<object, RestClient> _clients = new();
+
+	private static RestClient GetClient(object key)
+	{
+		return _clients.SafeAdd(key, _ =>
+		{
+			var asm = (key is string ? typeof(RestSharpHelper) : key.GetType()).Assembly;
+			var prod = asm.GetAttribute<AssemblyProductAttribute>()?.Product;
+			var ver = asm.GetName().Version;
+
+			var options = new RestClientOptions
+			{
+				UserAgent = $"{prod}/{ver}",
 			};
 
-			init?.Invoke(client);
+			return new RestClient(options) { Authenticator = new AuthenticatorWrapper() };
+		});
+	}
 
-			logVerbose?.Invoke("Request {0}, '{1}' Args '{2}'.", new object[] { request.Method, url, request.Parameters.Select(p => $"{p.Name}={p.Value}").JoinAnd() });
-			var response = await client.ExecuteAsync<object>(request, token);
+	public static void RemoveWhere(this ParametersCollection collection, Func<Parameter, bool> filter)
+	{
+		foreach (var par in collection.Where(filter).ToArray())
+			collection.RemoveParameter(par);
+	}
 
-			var content = response.Content;
-			logVerbose?.Invoke("Response '{0}' (code {1}).", new object[] { content, response.StatusCode });
+	public static void AddBodyAsStr(this RestRequest request, string bodyStr)
+	{
+		if (request is null)
+			throw new ArgumentNullException(nameof(request));
 
-			// https://restsharp.dev/usage/exceptions.html
-			var networkFailure = response.ResponseStatus != ResponseStatus.Completed;
-			if(networkFailure)
-			{
-				logVerbose?.Invoke("failed to complete reqeust: status={0}, msg={1}, err={2}", new object[] { response.ResponseStatus, response.ErrorMessage, response.ErrorException });
-				throw new InvalidOperationException($"failed to complete request: {response.ResponseStatus}");
-			}
+		request.AddStringBody(bodyStr, DataFormat.Json);
+	}
 
-			if (response.StatusCode != HttpStatusCode.OK || content.IsEmpty())
-				throw new UnexpectedResponseError(response);
+	public static object Invoke(this RestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, Func<string, string> contentConverter = null)
+		=> request.Invoke<object>(url, caller, logVerbose, contentConverter);
 
-			if (contentConverter != null)
-			{
-				content = contentConverter(content);
+	public static Task<object> InvokeAsync(this RestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, CancellationToken token, Func<string, string> contentConverter = null)
+		=> request.InvokeAsync<object>(url, caller, logVerbose, token, contentConverter);
 
-				if (content.IsEmpty())
-					return default;
-			}
+	public static T Invoke<T>(this RestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, Func<string, string> contentConverter = null)
+		=> AsyncContext.Run(() => request.InvokeAsync<T>(url, caller, logVerbose, CancellationToken.None, contentConverter));
 
-			return content.DeserializeObject<T>();
+	public static RestResponse<T> Invoke2<T>(this RestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, Func<string, string> contentConverter = null)
+		=> AsyncContext.Run(() => request.InvokeAsync2<T>(url, caller, logVerbose, CancellationToken.None, contentConverter));
+
+	public static async Task<T> InvokeAsync<T>(this RestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, CancellationToken token, Func<string, string> contentConverter = null)
+		=> (await request.InvokeAsync2<T>(url, caller, logVerbose, token, contentConverter)).Data;
+
+	public static async Task<RestResponse<T>> InvokeAsync2<T>(this RestRequest request, Uri url, object caller, Action<string, object[]> logVerbose, CancellationToken token, Func<string, string> contentConverter = null, IAuthenticator auth = null)
+	{
+		if (request is null)
+			throw new ArgumentNullException(nameof(request));
+
+		if (url is null)
+			throw new ArgumentNullException(nameof(url));
+
+		if (caller is null)
+			throw new ArgumentNullException(nameof(caller));
+
+		request.Resource = url.IsAbsoluteUri ? url.AbsoluteUri : url.OriginalString;
+
+		logVerbose?.Invoke("Request {0}, '{1}' Args '{2}'.", new object[] { request.Method, url, request.Parameters.Select(p => $"{p.Name}={p.Value}").JoinAnd() });
+
+		var client = GetClient(caller);
+		using var _ = ((AuthenticatorWrapper)client.Authenticator!).RegisterRequest(request, auth);
+
+		var response = await client.ExecuteAsync<object>(request, token);
+
+		logVerbose?.Invoke("Response '{0}' (code {1}).", new object[] { response.Content, response.StatusCode });
+
+		// https://restsharp.dev/usage/exceptions.html
+		var networkFailure = response.ResponseStatus != ResponseStatus.Completed;
+		if(networkFailure)
+		{
+			logVerbose?.Invoke("failed to complete reqeust: status={0}, msg={1}, err={2}", new object[] { response.ResponseStatus, response.ErrorMessage, response.ErrorException });
+			throw new InvalidOperationException($"failed to complete request: {response.ResponseStatus}");
 		}
+
+		if (response.StatusCode != HttpStatusCode.OK || response.Content.IsEmpty())
+			throw new UnexpectedResponseError(response);
+
+		var result = RestResponse<T>.FromResponse(response);
+
+		if (contentConverter != null)
+		{
+			result.Content = contentConverter(response.Content);
+
+			if (result.Content.IsEmpty())
+			{
+				result.Content = default;
+				result.Data = default;
+				return result;
+			}
+		}
+
+		result.Data = result.Content.DeserializeObject<T>();
+
+		return result;
 	}
 }
