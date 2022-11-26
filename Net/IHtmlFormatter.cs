a7eb5d2d9cd24070ef0a06d3bc9a88183306469e
mika@stocksharp.com <mika@stocksharp.com>
2022-11-26 09:53:53 +0300

    Fix commit 2ea5524b8f0edafa656c45650a2d3168e9255a3b

--- Net/IHtmlFormatter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ecng.Net;
-
-public interface IHtmlFormatter
-{
-	ValueTask<string> ToHtmlAsync(string text, object context, CancellationToken cancellationToken = default);
-	ValueTask<string> CleanAsync(string text, CancellationToken cancellationToken = default);
-	ValueTask<string> ActivateRuleAsync(string text, object rule, object context, CancellationToken cancellationToken = default);
-}
