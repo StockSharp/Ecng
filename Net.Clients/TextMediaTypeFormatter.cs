bd0f006170d78dd34fad6b322a81912a100ded1e
mika@stocksharp.com <mika@stocksharp.com>
2023-02-04 21:47:26 +0300

    TextMediaTypeFormatter

---
+++ Net.Clients/TextMediaTypeFormatter.cs
@@ -0,0 +1,36 @@
+﻿namespace Ecng.Net;
+
+using System.IO;
+
+using Ecng.Serialization;
+
+public class TextMediaTypeFormatter : MediaTypeFormatter
+{
+	public TextMediaTypeFormatter(IEnumerable<string> mediaTypes)
+	{
+		if (mediaTypes is null)
+			throw new ArgumentNullException(nameof(mediaTypes));
+
+		foreach (var mediaType in mediaTypes)
+			SupportedMediaTypes.Add(new(mediaType));
+	}
+
+	public override Task<object> ReadFromStreamAsync(Type type, Stream readStream, HttpContent content, IFormatterLogger formatterLogger)
+	{
+		return ReadFromStreamAsync(type, readStream, content, formatterLogger, default);
+	}
+
+	public override async Task<object> ReadFromStreamAsync(Type type, Stream readStream, HttpContent content, IFormatterLogger formatterLogger, CancellationToken cancellationToken)
+	{
+		using var streamReader = new StreamReader(readStream);
+		var str = await streamReader.ReadToEndAsync();
+
+		if (type == typeof(string))
+			return str;
+
+		return str.DeserializeObject(type);
+	}
+
+	public override bool CanReadType(Type type) => true;
+	public override bool CanWriteType(Type type) => false;
+}
