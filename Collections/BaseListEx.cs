0f83274ce5530c1d23ef9d85296142cc469f1cdd
mika@stocksharp.com <mika@stocksharp.com>
2023-02-15 10:45:35 +0300

    Revert "DuckTypedList, FixedSynchronizedDictionary, FixedSynchronizedList, IRangeCollection, ModifiedEnumerable, PriorityQueue removed."
    This reverts commit de6eb4cec9a80a208428df623f9bada131b8699b.

--- Collections/BaseListEx.cs
+++ Collections/BaseListEx.cs
@@ -5,8 +5,17 @@ namespace Ecng.Collections
 	using System.Collections.Generic;
 	using System.ComponentModel;
 
-	public abstract class BaseListEx<T> : IList<T>, IList
+	public abstract class BaseListEx<T> : IList<T>, IList, IRangeCollection
 	{
+		#region IRangeCollection Members
+
+		IEnumerable IRangeCollection.GetRange(long startIndex, long count, string sortExpression, ListSortDirection directions)
+		{
+			return GetRange(startIndex, count, sortExpression, directions);
+		}
+
+		#endregion
+
 		#region IListEx<T> Members
 
 		public abstract IEnumerable<T> GetRange(long startIndex, long count, string sortExpression, ListSortDirection directions);
---
+++ Collections/FixedSynchronizedDictionary.cs
@@ -0,0 +1,39 @@
+﻿namespace Ecng.Collections
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class FixedSynchronizedDictionary<TKey, TValue> : SynchronizedDictionary<TKey, TValue>
+	{
+		public FixedSynchronizedDictionary()
+		{
+		}
+
+		public FixedSynchronizedDictionary(int capacity)
+			: base(capacity)
+		{
+		}
+
+		private int _befferSize = int.MaxValue;
+
+		public int BufferSize
+		{
+			get => _befferSize;
+			set
+			{
+				if (value <= 1)
+					throw new ArgumentOutOfRangeException();
+
+				_befferSize = value;
+
+				this.Shrink<SynchronizedDictionary<TKey, TValue>, KeyValuePair<TKey, TValue>>(BufferSize);
+			}
+		}
+
+		public override void Add(TKey key, TValue value)
+		{
+			base.Add(key, value);
+			this.Shrink<SynchronizedDictionary<TKey, TValue>, KeyValuePair<TKey, TValue>>(BufferSize);
+		}
+	}
+}
