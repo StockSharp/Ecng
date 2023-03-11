bbc29d8dd159ffd30c0782e004d6bacf1a5e9c51
mika@stocksharp.com <mika@stocksharp.com>
2023-03-12 01:31:13 +0300

    CodeReference

--- Compilation/ICompilerExtensions.cs
+++ Compilation/ICompilerExtensions.cs
@@ -4,6 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+#if NETCOREAPP
+using System.Runtime.Loader;
+#endif
 
 using Ecng.Common;
 
@@ -25,4 +29,19 @@ public static class ICompilerExtensions
 	/// <returns><see langword="true" /> - If there are errors, <see langword="true" /> - If the compilation is performed without errors.</returns>
 	public static bool HasErrors(this CompilationResult result)
 		=> result.CheckOnNull(nameof(result)).Errors.Any(e => e.Type == CompilationErrorTypes.Error);
+
+#if NETCOREAPP
+	/// <summary>
+	/// To compile the code.
+	/// </summary>
+	/// <param name="compiler">Compiler.</param>
+	/// <param name="context"><see cref="AssemblyLoadContext"/></param>
+	/// <param name="code">Code.</param>
+	/// <param name="name">The reference name.</param>
+	/// <param name="references">References.</param>
+	/// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+	/// <returns>The result of the compilation.</returns>
+	public static CompilationResult CompileCode(this ICompiler compiler, AssemblyLoadContext context, string code, string name, IEnumerable<CodeReference> references, CancellationToken cancellationToken = default)
+		=> compiler.Compile(new(context), name, code, references.Where(r => r.IsValid).Select(r => r.FullLocation).ToArray(), cancellationToken);
+#endif
 }
