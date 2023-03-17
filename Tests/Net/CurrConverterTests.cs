2f81c987bb49824ccaab289266459a949a51f5b3
mika@stocksharp.com <mika@stocksharp.com>
2023-03-17 11:47:22 +0300

    More tests.

--- Tests/Net/CurrConverterTests.cs
+++ Tests/Net/CurrConverterTests.cs
@@ -50,14 +50,30 @@
 		public async Task Coinvert()
 		{
 			ICurrencyConverter converter = new CoinvertCurrencyConverter(Config.HttpClient);
+			
 			var rate = await converter.GetRateAsync(CurrencyTypes.BTC, CurrencyTypes.USD, DateTime.Today);
 			(rate > 1000).AssertTrue();
 			(rate < 100000).AssertTrue();
 
 			rate = await converter.GetRateAsync(CurrencyTypes.BTC, CurrencyTypes.USD, DateTime.Today);
-
 			(rate > 1000).AssertTrue();
 			(rate < 100000).AssertTrue();
+
+			rate = await converter.GetRateAsync(CurrencyTypes.USD, CurrencyTypes.BTC, DateTime.Today);
+			(rate > 0.0000001m).AssertTrue();
+			(rate < 0.0001m).AssertTrue();
+
+			rate = await converter.GetRateAsync(CurrencyTypes.USD, CurrencyTypes.ETH, DateTime.Today);
+			(rate > 0.00001m).AssertTrue();
+			(rate < 0.001m).AssertTrue();
+
+			rate = await converter.GetRateAsync(CurrencyTypes.ETH, CurrencyTypes.BTC, DateTime.Today);
+			(rate > 0.01m).AssertTrue();
+			(rate < 0.1m).AssertTrue();
+			
+			rate = await converter.GetRateAsync(CurrencyTypes.ETH, CurrencyTypes.USD, DateTime.Today);
+			(rate > 100).AssertTrue();
+			(rate < 10000).AssertTrue();
 		}
 	}
 }
