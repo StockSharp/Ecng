8f3a1a77da9a7c2d59393e0fe2fc2e7a173a8d59
mika@stocksharp.com <mika@stocksharp.com>
2023-05-20 00:55:52 +0300

    ExpiredException, TooManyException

---
+++ Common/ExpiredException.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecng.Common;
+
+using System;
+
+public class ExpiredException : InvalidOperationException
+{
+	public ExpiredException(string message)
+		: base(message)
+	{
+	}
+}
---
+++ Common/TooManyException.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecng.Common;
+
+using System;
+
+public class TooManyException : InvalidOperationException
+{
+	public TooManyException(string message)
+		: base(message)
+	{
+	}
+}
