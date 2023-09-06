93be66aa083c2b78d4fa13a5399ac4ca096b36f2
mika@stocksharp.com <mika@stocksharp.com>
2023-09-06 16:02:46 +0300

    WindowsGrandAccess

---
+++ Interop.Windows/WindowsGrandAccess.cs
@@ -0,0 +1,187 @@
+﻿namespace StockSharp.Web.StrategyServer;
+
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+using Windows.Win32;
+
+using Ecng.Common;
+
+// https://stackoverflow.com/a/30687230/8029915
+public static class WindowsGrandAccess
+{
+	private class Token : Disposable
+	{
+		private readonly GenericSecurity _wsSecurity;
+		private readonly GenericSecurity _dsSecurity;
+		private readonly int? _oldWindowStationMaskm;
+		private readonly int? _oldDesktopMask;
+
+		public Token(GenericSecurity wsSecurity, GenericSecurity dsSecurity, int? oldWindowStationMaskm, int? oldDesktopMask)
+		{
+			_wsSecurity = wsSecurity ?? throw new ArgumentNullException(nameof(wsSecurity));
+			_dsSecurity = dsSecurity ?? throw new ArgumentNullException(nameof(dsSecurity));
+			_oldWindowStationMaskm = oldWindowStationMaskm;
+			_oldDesktopMask = oldDesktopMask;
+		}
+
+		protected override void DisposeManaged()
+		{
+			base.DisposeManaged();
+
+			if (_oldWindowStationMaskm is not null)
+				_wsSecurity.RestAccessMask(_oldWindowStationMaskm.Value, _windowStationAllAccess);
+
+			if (_oldDesktopMask is not null)
+				_dsSecurity.RestAccessMask(_oldDesktopMask.Value, _desktopRightsAllAccess);
+		}
+	}
+
+	private const int _desktopRightsAllAccess = 0x000f01ff;
+	private const int _windowStationAllAccess = 0x000f037f;
+
+	public static IDisposable GrantAccessToWindowStationAndDesktop(string username)
+	{
+		var wsHandle = new NoopSafeHandle(PInvoke.GetProcessWindowStation());
+		var dsHandle = new NoopSafeHandle(PInvoke.GetThreadDesktop(PInvoke.GetCurrentThreadId()));
+
+		var account = new NTAccount(username);
+
+		var (oldWindowStationMask, wsSecurity) = ReadAccessMask(account, wsHandle, _windowStationAllAccess);
+		var (oldDesktopMask, dsSecurity) = ReadAccessMask(account, dsHandle, _desktopRightsAllAccess);
+
+		wsSecurity.SetGrandAccess(_windowStationAllAccess);
+		dsSecurity.SetGrandAccess(_desktopRightsAllAccess);
+
+		return new Token(wsSecurity, dsSecurity, oldWindowStationMask, oldDesktopMask);
+	}
+
+	private static (int? mask, GenericSecurity security) ReadAccessMask(NTAccount account, SafeHandle handle, int accessMask)
+	{
+		var security = new GenericSecurity(account, false, ResourceType.WindowObject, handle, AccessControlSections.Access);
+
+		var rules = security.GetAccessRules();
+
+		var username = account.Value;
+		//if (!username.Contains("\\"))
+		//	username = $"{Environment.MachineName}\\{username}";
+
+		var userResult = rules.Cast<GenericAccessRule>().SingleOrDefault(r => r.IdentityReference.Value.EqualsIgnoreCase(username) && accessMask == r.PublicAccessMask);
+		if (userResult is null)
+		{
+			security.AddGrandAccess(accessMask);
+			userResult = rules.Cast<GenericAccessRule>().SingleOrDefault(r => r.IdentityReference.Value.EqualsIgnoreCase(username));
+			
+			if (userResult != null)
+				return (userResult.PublicAccessMask, security);
+		}
+		else
+			return (userResult.PublicAccessMask, security);
+
+		return (null, security);
+	}
+
+	private class GenericAccessRule : AccessRule
+	{
+		public GenericAccessRule(
+			IdentityReference identity, int accessMask, bool isInherited,
+			InheritanceFlags inheritanceFlags, PropagationFlags propagationFlags,
+			AccessControlType type)
+			: base(identity, accessMask, isInherited,
+			inheritanceFlags, propagationFlags, type)
+		{
+		}
+
+		public GenericAccessRule(IdentityReference identity, int accessMask, AccessControlType type)
+			: base(identity, accessMask, false, InheritanceFlags.None, PropagationFlags.None, type)
+		{
+		}
+
+		public int PublicAccessMask => AccessMask;
+	}
+
+	private class GenericSecurity : NativeObjectSecurity
+	{
+		private readonly NTAccount _account;
+		private readonly SafeHandle _handle;
+
+		public GenericSecurity(NTAccount account, bool isContainer, ResourceType resType, SafeHandle handle, AccessControlSections sectionsRequested)
+			: base(isContainer, resType, handle, sectionsRequested)
+		{
+			_account = account ?? throw new ArgumentNullException(nameof(account));
+			_handle = handle ?? throw new ArgumentNullException(nameof(handle));
+		}
+
+		private void Persist() => Persist(_handle, AccessControlSections.Access);
+
+		public AuthorizationRuleCollection GetAccessRules()
+			=> GetAccessRules(true, false, typeof(NTAccount));
+
+		public void AddGrandAccess(int accessMask)
+		{
+			AddAccessRule(new GenericAccessRule(_account, accessMask, AccessControlType.Allow));
+			Persist();
+		}
+
+		public void SetGrandAccess(int accessMask)
+		{
+			SetAccessRule(new GenericAccessRule(_account, accessMask, AccessControlType.Allow));
+			Persist();
+		}
+
+		public void RemoveGrantAccess(int accessMask)
+		{
+			RemoveAccessRule(new GenericAccessRule(_account, accessMask, AccessControlType.Allow));
+			Persist();
+		}
+
+		public void RestAccessMask(int? oldAccessMask, int fullAccessMask)
+		{
+			if (oldAccessMask == null)
+				RemoveGrantAccess(fullAccessMask);
+			else if (oldAccessMask != fullAccessMask)
+				SetGrandAccess(oldAccessMask.Value);
+		}
+
+		#region NativeObjectSecurity Abstract Method Overrides
+
+		public override Type AccessRightType => throw new NotSupportedException();
+		public override Type AuditRuleType => typeof(AuditRule);
+		public override Type AccessRuleType => typeof(AccessRule);
+
+		public override AccessRule AccessRuleFactory(
+			IdentityReference identityReference,
+			int accessMask, bool isInherited, InheritanceFlags inheritanceFlags,
+			PropagationFlags propagationFlags, AccessControlType type)
+		{
+			return new GenericAccessRule(identityReference, accessMask, isInherited, inheritanceFlags, propagationFlags, type);
+		}
+
+		public override AuditRule AuditRuleFactory(
+			IdentityReference identityReference,
+			int accessMask, bool isInherited, InheritanceFlags inheritanceFlags,
+			PropagationFlags propagationFlags, AuditFlags flags)
+		{
+			throw new NotSupportedException();
+		}
+
+		#endregion
+	}
+
+	private class NoopSafeHandle : SafeHandle
+	{
+		public NoopSafeHandle(IntPtr handle)
+			: base(handle, false)
+		{
+		}
+
+		public override bool IsInvalid => false;
+
+		// Handles returned by GetProcessWindowStation and GetThreadDesktop
+		// should not be closed
+		protected override bool ReleaseHandle() => true;
+	}
+}
