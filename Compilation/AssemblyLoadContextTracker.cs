ecbe0d048abb9676cfe0cac7f0b11c9caea702c7
mika@stocksharp.com <mika@stocksharp.com>
2023-09-08 11:23:09 +0300

    AssemblyLoadContextTracker. Unload method

--- Compilation/AssemblyLoadContextTracker.cs
+++ Compilation/AssemblyLoadContextTracker.cs
@@ -1,6 +1,7 @@
 ﻿#if NETCOREAPP
 namespace Ecng.Compilation;
 
+using System;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
@@ -10,16 +11,24 @@ using Ecng.Common;
 public class AssemblyLoadContextTracker
 {
 	private readonly SyncObject _lock = new();
+	private readonly Action<Exception> _uploadingError;
 	private AssemblyLoadContext _context;
 	private byte[] _assembly;
 
-	public Assembly LoadFromStream(byte[] assembly)
+    public AssemblyLoadContextTracker(Action<Exception> uploadingError = default)
+    {
+		_uploadingError = uploadingError;
+	}
+
+    public Assembly LoadFromStream(byte[] assembly)
 	{
 		void init()
 		{
 			_context = new(default, true);
 			_assembly = assembly;
 		}
+
+		Exception error = null;
 
 		lock (_lock)
 		{
@@ -29,12 +38,36 @@ public class AssemblyLoadContextTracker
 			}
 			else if (!_assembly.SequenceEqual(assembly))
 			{
-				_context.Unload();
+				try
+				{
+					_context.Unload();
+				}
+				catch (Exception ex)
+				{
+					error = ex;
+				}
+
 				init();
 			}
 		}
+
+		if (error is not null && _uploadingError is not null)
+			_uploadingError(error);
 		
 		return _context.LoadFromStream(assembly);
+	}
+
+	public void Unload()
+	{
+		lock (_lock)
+		{
+			if (_context is null)
+				return;
+
+			_context.Unload();
+			_context = null;
+			_assembly = null;
+		}
 	}
 }
 #endif
