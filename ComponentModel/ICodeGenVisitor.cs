a874780b97e2080e4e260becc671150c50feaea1
mika@stocksharp.com <mika@stocksharp.com>
2023-11-17 17:28:32 +0300

    ICodeGenVisitor

---
+++ ComponentModel/ICodeGenVisitor.cs
@@ -0,0 +1,53 @@
+﻿namespace Ecng.ComponentModel;
+
+using System;
+using System.Text;
+
+/// <summary>
+/// Code generation visitor.
+/// </summary>
+public interface ICodeGenVisitor
+{
+	/// <summary>
+	/// Add code line.
+	/// </summary>
+	/// <param name="line">Code line.</param>
+	/// <returns><see cref="IDiagramCodeGenVisitor"/></returns>
+	ICodeGenVisitor AddLine(string line);
+}
+
+/// <summary>
+/// <see cref="ICodeGenVisitor"/> implementation for <see cref="StringBuilder"/>.
+/// </summary>
+public class StringBuilderCodeGenVisitor : ICodeGenVisitor
+{
+	private readonly StringBuilder _builder;
+	private string _indent = string.Empty;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="StringBuilderCodeGenVisitor"/>.
+	/// </summary>
+	/// <param name="builder"><see cref="StringBuilder"/></param>
+	public StringBuilderCodeGenVisitor(StringBuilder builder)
+    {
+		_builder = builder ?? throw new ArgumentNullException(nameof(builder));
+	}
+
+	ICodeGenVisitor ICodeGenVisitor.AddLine(string line)
+	{
+		if (line == "}")
+		{
+			if (_indent.Length == 0)
+				throw new InvalidOperationException("Mismatched closing brace.");
+
+			_indent = _indent.Substring(0, _indent.Length - 1);
+		}
+		
+		_builder.AppendLine($"{_indent}{line}");
+
+		if (line == "{")
+			_indent += "\t";
+
+		return this;
+	}
+}
---
+++ Tests/ComponentModel/CodeGenTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Ecng.Tests.ComponentModel;
+
+using System;
+using System.Text;
+
+using Ecng.ComponentModel;
+using Ecng.UnitTesting;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class CodeGenTests
+{
+	[TestMethod]
+	public void CodeGen()
+	{
+		var sb = new StringBuilder();
+
+		ICodeGenVisitor visitor = new StringBuilderCodeGenVisitor(sb);
+
+		visitor
+			.AddLine("class Class1")
+			.AddLine("{")
+			.AddLine("public int Prop1 { get; set; }")
+			.AddLine("}");
+
+		sb.ToString().AssertEqual(@"class Class1
+{
+	public int Prop1 { get; set; }
+}
+");
+	}
+
+	[TestMethod]
+	[ExpectedException(typeof(InvalidOperationException))]
+	public void Braces()
+	{
+		var sb = new StringBuilder();
+
+		ICodeGenVisitor visitor = new StringBuilderCodeGenVisitor(sb);
+
+		visitor
+			.AddLine("{")
+			.AddLine("}")
+			.AddLine("}");
+	}
+}
