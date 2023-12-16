aae2ff4f0a1768e5aa9eb05819aead457c884e33
mika@stocksharp.com <mika@stocksharp.com>
2023-12-16 11:15:08 +0300

    LibGit2Sharp removed.

--- LibGit2Sharp/Core/Codegens/AssemblyCommitIds.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LibGit2Sharp.Core
-{
-	internal static class AssemblyCommitIds
-    {
-        public const string LibGit2CommitSha = "8a0dc6783c340e61a44c179c48f832165ad2053c";
-        public const string LibGit2SharpCommitSha = "22230b74ad93601940b2bb0c6e750b8ee7e9ea18";
-    }
-}
