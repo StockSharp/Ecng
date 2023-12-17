174139ed7b3e90c8f9247b05c49f76497d091f88
mika@stocksharp.com <mika@stocksharp.com>
2023-12-17 15:27:46 +0300

    AppToken

---
+++ ComponentModel/AppToken.cs
@@ -0,0 +1,21 @@
+﻿namespace Ecng.ComponentModel;
+
+using System.Threading;
+
+/// <summary>
+/// Application level <see cref="CancellationToken"/>.
+/// </summary>
+public static class AppToken
+{
+	private static readonly CancellationTokenSource _cts = new();
+
+	/// <summary>
+	/// <see cref="CancellationToken"/>.
+	/// </summary>
+	public static CancellationToken Value => _cts.Token;
+
+	/// <summary>
+	/// Cancel <see cref="Value"/>.
+	/// </summary>
+	public static void Shutdown() => _cts.Cancel();
+}
