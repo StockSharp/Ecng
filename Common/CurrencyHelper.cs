8a38688b628e9d7279368480e172c1c4adcb934d
mika@stocksharp.com <mika@stocksharp.com>
2024-02-11 22:56:15 +0300

    CurrencyHelper. GetPrefix extension.

--- Common/CurrencyHelper.cs
+++ Common/CurrencyHelper.cs
@@ -21,5 +21,22 @@
 		/// <returns>Currency.</returns>
 		public static Currency ToCurrency(this decimal value, CurrencyTypes type)
 			=> new() { Type = type, Value = value };
+
+		public static string GetPrefix(this CurrencyTypes currency)
+			=> currency switch
+			{
+				CurrencyTypes.USD => "$",
+				CurrencyTypes.EUR => "€",
+				CurrencyTypes.RUB => "₽",
+				CurrencyTypes.GBP => "£",
+				CurrencyTypes.JPY or CurrencyTypes.CNY => "¥",
+				CurrencyTypes.THB => "฿",
+				CurrencyTypes.BTC => "₿",
+				CurrencyTypes.CHF => "₣",
+				CurrencyTypes.INR => "₹",
+				CurrencyTypes.AED => "د.إ",
+				CurrencyTypes.KRW => "₩",
+				_ => currency.ToString(),
+			};	
 	}
 }
