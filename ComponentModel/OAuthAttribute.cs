11c9648604ab0e8e24f4cf4e9a20184acf6229de
mika@stocksharp.com <mika@stocksharp.com>
2024-06-12 09:48:43 +0300

    OAuthAttribute

---
+++ ComponentModel/OAuthAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecng.ComponentModel;
+
+using System;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class OAuthAttribute : Attribute
+{
+    public OAuthAttribute(long serviceId)
+    {
+		ServiceId = serviceId;
+	}
+
+	public long ServiceId { get; }
+}
