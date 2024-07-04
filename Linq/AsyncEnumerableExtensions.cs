8919fb80f954c8ddecca7f749324eb377b7f47c7
mika@stocksharp.com <mika@stocksharp.com>
2024-07-04 21:13:36 +0300

    AsyncEnumerableExtensions

---
+++ Linq/AsyncEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Ecng.Linq;
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Ecng.Common;
+
+public static class AsyncEnumerableExtensions
+{
+	public static async ValueTask<T[]> ToArrayAsync<T>(this IAsyncEnumerable<T> enu, CancellationToken cancellationToken)
+	{
+		if (enu is null)
+			throw new ArgumentNullException(nameof(enu));
+
+		var list = new List<T>();
+
+		await foreach (var item in enu.WithEnforcedCancellation(cancellationToken))
+			list.Add(item);
+
+		return list.ToArray();
+	}
+}
