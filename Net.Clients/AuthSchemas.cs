867da38ec97b3b7513fe6e13c80840c721308a7b
mika@stocksharp.com <mika@stocksharp.com>
2024-09-17 23:25:57 +0300

    Fix commit e05fc13c816bdde25b750c35decb6cc7e6ed55fa

--- Net.Clients/AuthSchemas.cs
+++ Net.Clients/AuthSchemas.cs
@@ -5,6 +5,9 @@ public static class AuthSchemas
 	public const string Basic = "Basic";
 	public const string Bearer = "Bearer";
 
+	public static string FormatAuth(this string schema, SecureString token)
+		=> schema.FormatAuth(token.UnSecure());
+
 	public static string FormatAuth(this string schema, string token)
 		=> $"{schema} {token}";
 }
