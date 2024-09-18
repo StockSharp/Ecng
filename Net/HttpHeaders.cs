82e1b8be49f4841c405113179c37096f651f3c3a
mika@stocksharp.com <mika@stocksharp.com>
2024-09-18 12:59:10 +0300

    Fix commit 04d05437f1d9b39f6cf90cd61349e09f95ac8372

--- Net/HttpHeaders.cs
+++ Net/HttpHeaders.cs
@@ -3,7 +3,16 @@
 public static class HttpHeaders
 {
 	public const string Authorization = "Authorization";
-	public const string UserAgent = "User-Agent";
 	public const string AcceptEncoding = "Accept-Encoding";
 	public const string AcceptLanguage = "Accept-Language";
+	public const string CacheControl = "Cache-Control";
+	public const string Connection = "Connection";
+	public const string KeepAlive = "Keep-Alive";
+	public const string LastModified = "Last-Modified";
+	public const string ProxyAuthenticate = "Proxy-Authenticate";
+	public const string ProxyAuthorization = "Proxy-Authorization";
+	public const string ProxyConnection = "Proxy-Connection";
+	public const string UserAgent = "User-Agent";
+	public const string Referer = "Referer";
+	public const string WWWAuthenticate = "WWW-Authenticate";
 }
