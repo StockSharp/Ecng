c8998507b248ec3e2963dbc045206a99085e1a5c
mika@stocksharp.com <mika@stocksharp.com>
2024-10-09 22:23:38 +0300

    JArrayToObjectConverter<T>

--- Serialization/JArrayToObjectConverter.cs
+++ Serialization/JArrayToObjectConverter.cs
@@ -1,35 +1,106 @@
-namespace Ecng.Serialization
+namespace Ecng.Serialization;
+
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class JArrayToObjectConverter : JsonConverter
 {
-	using System;
-	using System.Reflection;
+	public override bool CanConvert(Type objectType) => true;
 
-	using Newtonsoft.Json;
-	using Newtonsoft.Json.Linq;
-
-	public class JArrayToObjectConverter : JsonConverter
+	public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 	{
-		public override bool CanConvert(Type objectType) => true;
-
-		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		existingValue ??= Activator.CreateInstance(objectType);
+		
+		var array = JArray.Load(reader);
+		var fields = objectType.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
+		
+		for (var i = 0; i < fields.Length; i++)
 		{
-			existingValue ??= Activator.CreateInstance(objectType);
-			
-			var array = JArray.Load(reader);
-			var fields = objectType.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
-			
-			for (var i = 0; i < fields.Length; i++)
-			{
-				var field = fields[i];
-				var token = array[i];
-				field.SetValue(existingValue, token.ToObject(field.FieldType));
-			}
-
-			return existingValue;
+			var field = fields[i];
+			var token = array[i];
+			field.SetValue(existingValue, token.ToObject(field.FieldType));
 		}
 
-		public override bool CanWrite => false;
-
-		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-			=> throw new NotSupportedException();
+		return existingValue;
 	}
+
+	public override bool CanWrite => false;
+
+	public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		=> throw new NotSupportedException();
+}
+
+public class JArrayToObjectConverter<T> : JsonConverter
+	where T : new()
+{
+	private static Action<T, object> CreateFieldSetter(FieldInfo fieldInfo)
+	{
+		var method = new DynamicMethod(
+			"Set" + fieldInfo.Name,
+			typeof(void),
+			[typeof(T), typeof(object)],
+			typeof(T), true);
+
+		var il = method.GetILGenerator();
+
+		il.Emit(OpCodes.Ldarg_0);
+
+		if (fieldInfo.FieldType.IsValueType)
+		{
+			il.Emit(OpCodes.Ldarg_1);
+			il.Emit(OpCodes.Unbox_Any, fieldInfo.FieldType);
+		}
+		else
+		{
+			il.Emit(OpCodes.Ldarg_1);
+			il.Emit(OpCodes.Castclass, fieldInfo.FieldType);
+		}
+
+		il.Emit(OpCodes.Stfld, fieldInfo);
+
+		il.Emit(OpCodes.Ret);
+
+		return (Action<T, object>)method.CreateDelegate(typeof(Action<T, object>));
+	}
+
+	private static readonly (Type, Action<T, object>)[] _fields;
+
+	static JArrayToObjectConverter()
+	{
+		_fields = typeof(T)
+			.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance)
+			.Select(f => (f.FieldType, CreateFieldSetter(f)))
+			.ToArray();
+	}
+
+	public override bool CanConvert(Type objectType) => true;
+
+	public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+	{
+		existingValue ??= new T();
+
+		var typed = (T)existingValue;
+
+		var array = JArray.Load(reader);
+
+		for (var i = 0; i < _fields.Length; i++)
+		{
+			var item = array[i];
+
+			var (type, action) = _fields[i];
+			action(typed, item.ToObject(type));
+		}
+
+		return existingValue;
+	}
+
+	public override bool CanWrite => false;
+
+	public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		=> throw new NotSupportedException();
 }
