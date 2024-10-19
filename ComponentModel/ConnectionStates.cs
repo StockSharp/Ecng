5d0d177f56a84396627108b68811f30ab93f6533
mika@stocksharp.com <mika@stocksharp.com>
2024-10-19 20:13:12 +0300

    Fix commit e61c74ef9d6dd2d5c7f8b362d0c801f77d8dc86a

--- ComponentModel/ConnectionStates.cs
+++ ComponentModel/ConnectionStates.cs
@@ -1,12 +1,30 @@
 ﻿namespace Ecng.ComponentModel;
 
+using System.Runtime.Serialization;
+using System;
+
+[Serializable]
+[DataContract]
 public enum ConnectionStates
 {
+	[EnumMember]
 	Disconnected,
+
+	[EnumMember]
 	Disconnecting,
+
+	[EnumMember]
 	Connecting,
+
+	[EnumMember]
 	Connected,
+
+	[EnumMember]
 	Reconnecting,
+
+	[EnumMember]
 	Restored,
+
+	[EnumMember]
 	Failed,
 }
