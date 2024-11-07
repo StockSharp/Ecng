ef27660cf4115eb342b0586432daea8e8c1d75b4
mika@stocksharp.com <mika@stocksharp.com>
2024-11-07 17:28:37 +0300

    FileReference. GetFileBody method.

--- Compilation/FileReference.cs
+++ Compilation/FileReference.cs
@@ -1,5 +1,15 @@
 ﻿namespace Ecng.Compilation;
 
+using System;
+using System.IO;
+
 public class FileReference : BaseFileReference
 {
+	public virtual string GetFileBody()
+	{
+		if (!IsValid)
+			throw new InvalidOperationException();
+
+		return File.ReadAllText(FileName);
+	}
 }
