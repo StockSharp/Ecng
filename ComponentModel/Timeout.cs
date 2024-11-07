ba69df545366528884a29e665aa2a0d78500e65c
mika@stocksharp.com <mika@stocksharp.com>
2024-11-07 19:14:45 +0300

    Timeout class.

---
+++ ComponentModel/Timeout.cs
@@ -0,0 +1,23 @@
+﻿namespace Ecng.ComponentModel;
+
+using System;
+using System.Threading;
+
+public class Timeout
+{
+    public TimeSpan Value { get; }
+
+	public Timeout(TimeSpan value)
+	{
+		if (value <= TimeSpan.Zero)
+			throw new ArgumentOutOfRangeException(nameof(value), value, "Invalid value.");
+
+		Value = value;
+	}
+
+	public void Register(Action action)
+	{
+		var ct = new CancellationTokenSource(Value);
+		ct.Token.Register(action, useSynchronizationContext: false);
+	}
+}
