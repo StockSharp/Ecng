fc64db14bf7954a8f25786954c001308811e9195
mika@stocksharp.com <mika@stocksharp.com>
2024-11-20 00:31:28 +0300

    YandexDisk.Client fork

---
+++ YandexDisk.Client/Http/Clients/MetaInfoClient.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using YandexDisk.Client.Clients;
+using YandexDisk.Client.Protocol;
+
+namespace YandexDisk.Client.Http.Clients
+{
+    internal class MetaInfoClient : DiskClientBase, IMetaInfoClient
+    {
+        internal MetaInfoClient(ApiContext apiContext)
+            : base(apiContext)
+        { }
+
+        public Task<Disk> GetDiskInfoAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return GetAsync<object, Disk>("", /*params*/ null, cancellationToken);
+        }
+
+        public Task<Resource> GetInfoAsync(ResourceRequest request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return GetAsync<ResourceRequest, Resource>("resources", request, cancellationToken);
+        }
+
+        public Task<Resource> GetTrashInfoAsync(ResourceRequest request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return GetAsync<ResourceRequest, Resource>("trash/resources", request, cancellationToken);
+        }
+
+        public Task<FilesResourceList> GetFilesInfoAsync(FilesResourceRequest request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return GetAsync<FilesResourceRequest, FilesResourceList>("resources/files", request, cancellationToken);
+        }
+
+        public Task<LastUploadedResourceList> GetLastUploadedInfoAsync(LastUploadedResourceRequest request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return GetAsync<LastUploadedResourceRequest, LastUploadedResourceList>("resources/last-uploaded", request, cancellationToken);
+        }
+
+        public Task<Resource> AppendCustomProperties(string path, IDictionary<string, string> customProperties, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return PatchAsync<object, object, Resource>("resources", new { path }, new { customProperties }, cancellationToken);
+        }
+        
+        public Task<Link> PublishFolderAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return PutAsync<object, object, Link>("resources/publish", new { path }, /*requestBody*/ null, cancellationToken);
+        }
+
+        public Task<Link> UnpublishFolderAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return PutAsync<object, object, Link>("resources/unpublish", new { path }, /*requestBody*/ null, cancellationToken);
+        }
+    }
+}
---
+++ YandexDisk.Client/README.md
@@ -0,0 +1,3 @@
+# YandexDisk.Client - Updated Fork
+
+This is an updated fork of the [original YandexDisk.Client](https://github.com/raidenyn/yandexdisk.client/) project, which is no longer maintained. The purpose of this fork is to update dependencies and ensure compatibility with modern .NET.
