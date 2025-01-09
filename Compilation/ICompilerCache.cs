d8e7e258be957dd9515d6cbc912210c6aba41306
mika@stocksharp.com <mika@stocksharp.com>
2025-01-09 12:19:26 +0300

    Fix commit 287374152d1d514327857abeb78d3973c51efed7

--- Compilation/ICompilerCache.cs
+++ Compilation/ICompilerCache.cs
@@ -12,9 +12,9 @@ public interface ICompilerCache
 {
 	int Count { get; }
 
-	bool TryGet(IEnumerable<string> sources, IEnumerable<string> refs, out byte[] assembly);
-	void Add(IEnumerable<string> sources, IEnumerable<string> refs, byte[] assembly);
-	bool Remove(IEnumerable<string> sources, IEnumerable<string> refs);
+	bool TryGet(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs, out byte[] assembly);
+	void Add(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs, byte[] assembly);
+	bool Remove(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs);
 	void Clear();
 	void Init();
 }
@@ -43,12 +43,12 @@ public class InMemoryCompilerCache : ICompilerCache
 
 	public virtual int Count => _cache.Count;
 
-	protected static string GetKey(IEnumerable<string> sources, IEnumerable<string> refs)
+	protected static string GetKey(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs)
 	{
 		if (sources is null)	throw new ArgumentNullException(nameof(sources));
 		if (refs is null)		throw new ArgumentNullException(nameof(refs));
 
-		return (sources.JoinN() + refs.JoinN()).UTF8().Sha512();
+		return language + (sources.JoinN() + refs.JoinN()).UTF8().Sha512();
 	}
 
 	protected bool Remove(string key)
@@ -69,14 +69,14 @@ public class InMemoryCompilerCache : ICompilerCache
 		return false;
 	}
 
-	public virtual void Add(IEnumerable<string> sources, IEnumerable<string> refs, byte[] assembly)
-		=> Set(GetKey(sources, refs), assembly);
+	public virtual void Add(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs, byte[] assembly)
+		=> Set(GetKey(language, sources, refs), assembly);
 
-	public virtual bool Remove(IEnumerable<string> sources, IEnumerable<string> refs)
-		=> Remove(GetKey(sources, refs));
+	public virtual bool Remove(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs)
+		=> Remove(GetKey(language, sources, refs));
 
-	public virtual bool TryGet(IEnumerable<string> sources, IEnumerable<string> refs, out byte[] assembly)
-		=> TryGet(GetKey(sources, refs), out assembly);
+	public virtual bool TryGet(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs, out byte[] assembly)
+		=> TryGet(GetKey(language, sources, refs), out assembly);
 
 	public virtual void Clear() => _cache.Clear();
 	public virtual void Init() { }
@@ -110,18 +110,18 @@ public class FileCompilerCache(string path, TimeSpan timeout) : InMemoryCompiler
 	private string GetFileName(string key)
 		=> Path.Combine(_path, $"{key}.{_ext}");
 
-	public override void Add(IEnumerable<string> sources, IEnumerable<string> refs, byte[] assembly)
+	public override void Add(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs, byte[] assembly)
 	{
-		var key = GetKey(sources, refs);
+		var key = GetKey(language, sources, refs);
 		var fileName = GetFileName(key);
 
 		File.WriteAllBytes(fileName, assembly);
 		Set(key, assembly);
 	}
 
-	public override bool Remove(IEnumerable<string> sources, IEnumerable<string> refs)
+	public override bool Remove(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs)
 	{
-		var key = GetKey(sources, refs);
+		var key = GetKey(language, sources, refs);
 		var fileName = GetFileName(key);
 
 		if (File.Exists(fileName))
@@ -130,9 +130,9 @@ public class FileCompilerCache(string path, TimeSpan timeout) : InMemoryCompiler
 		return Remove(key);
 	}
 
-	public override bool TryGet(IEnumerable<string> sources, IEnumerable<string> refs, out byte[] assembly)
+	public override bool TryGet(CompilationLanguages language, IEnumerable<string> sources, IEnumerable<string> refs, out byte[] assembly)
 	{
-		var key = GetKey(sources, refs);
+		var key = GetKey(language, sources, refs);
 
 		if (TryGet(key, out assembly))
 			return true;
