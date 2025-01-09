8f98af9886a1b5a112354a3574b70a7a4c58213a
mika@stocksharp.com <mika@stocksharp.com>
2025-01-09 15:37:10 +0300

    Fix commit 72843cb614354f07e146267a5c8525c78d24e2f1

---
+++ Compilation.All/CompilerProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Ecng.Compilation.All;
+
+using Ecng.Compilation.FSharp;
+using Ecng.Compilation.Python;
+using Ecng.Compilation.Roslyn;
+
+public static class CompilerProviderExtensions
+{
+	public static CompilerProvider CreateCompilerProvider()
+		=> new()
+		{
+			{ CompilationLanguages.CSharp, new CSharpCompiler() },
+			{ CompilationLanguages.VisualBasic, new VisualBasicCompiler() },
+			{ CompilationLanguages.FSharp, new FSharpCompiler() },
+			{ CompilationLanguages.Python, new PythonCompiler() }
+		};
+}
--- Compilation/CompilerProvider.cs
+++ Compilation/CompilerProvider.cs
@@ -1,22 +1,7 @@
 ﻿namespace Ecng.Compilation;
 
-using System;
-
 using Ecng.Collections;
 
-public class CompilerProvider
+public class CompilerProvider : SynchronizedDictionary<CompilationLanguages, ICompiler>
 {
-	private readonly SynchronizedDictionary<CompilationLanguages, ICompiler> _compilers = [];
-
-	public void RegisterCompiler(CompilationLanguages language, ICompiler compiler)
-		=> _compilers.Add(language, compiler ?? throw new ArgumentNullException(nameof(compiler)));
-
-	public ICompiler GetCompiler(CompilationLanguages language)
-		=> _compilers[language];
-
-	public bool TryGetCompiler(CompilationLanguages language, out ICompiler compiler)
-		=> _compilers.TryGetValue(language, out compiler);
-
-	public void UnRegisterCompiler(CompilationLanguages language)
-		=> _compilers.Remove(language);
 }
