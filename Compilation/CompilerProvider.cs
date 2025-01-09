72843cb614354f07e146267a5c8525c78d24e2f1
mika@stocksharp.com <mika@stocksharp.com>
2025-01-09 11:57:47 +0300

    CompilerProvider

---
+++ Compilation/CompilerProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace Ecng.Compilation;
+
+using System;
+
+using Ecng.Collections;
+
+public class CompilerProvider
+{
+	private readonly SynchronizedDictionary<CompilationLanguages, ICompiler> _compilers = [];
+
+	public void RegisterCompiler(CompilationLanguages language, ICompiler compiler)
+		=> _compilers.Add(language, compiler ?? throw new ArgumentNullException(nameof(compiler)));
+
+	public ICompiler GetCompiler(CompilationLanguages language)
+		=> _compilers[language];
+
+	public void UnRegisterCompiler(CompilationLanguages language)
+		=> _compilers.Remove(language);
+}
