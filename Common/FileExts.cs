2d75ebbf0cd3a023ac67b6f9cd7f742c0934f819
mika@stocksharp.com <mika@stocksharp.com>
2025-01-10 12:08:20 +0300

    FileExts

---
+++ Common/FileExts.cs
@@ -0,0 +1,69 @@
+﻿namespace Ecng.Common;
+
+public static class FileExts
+{
+	/// <summary>
+	/// Backup extension for settings file.
+	/// </summary>
+	public const string BackupExt = ".bak";
+
+	/// <summary>
+	/// C# file extension.
+	/// </summary>
+	public const string CsEx = ".cs";
+
+	/// <summary>
+	/// F# file extension.
+	/// </summary>
+	public const string FsEx = ".fs";
+
+	/// <summary>
+	/// Visual Basic file extension.
+	/// </summary>
+	public const string VbEx = ".vb";
+
+	/// <summary>
+	/// Python file extension.
+	/// </summary>
+	public const string PyEx = ".py";
+
+	/// <summary>
+	/// Assembly file extension.
+	/// </summary>
+	public const string DllEx = ".dll";
+
+	/// <summary>
+	/// JSON file extension.
+	/// </summary>
+	public const string JsonEx = ".json";
+
+	/// <summary>
+	/// XML file extension.
+	/// </summary>
+	public const string Xml = ".xml";
+
+	/// <summary>
+	/// CSV file extension.
+	/// </summary>
+	public const string Csv = ".csv";
+
+	/// <summary>
+	/// Text file extension.
+	/// </summary>
+	public const string Txt = ".txt";
+
+	/// <summary>
+	/// Binary file extension.
+	/// </summary>
+	public const string Bin = ".bin";
+
+	/// <summary>
+	/// Xsl file extension.
+	/// </summary>
+	public const string Xls = ".xsl";
+
+	/// <summary>
+	/// Xslx file extension.
+	/// </summary>
+	public const string Xlsx = ".xslx";
+}
