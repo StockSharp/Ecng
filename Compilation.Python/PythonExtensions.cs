a543260cdd9d959d57ba2e84b2dc86a23a30b0db
mika@stocksharp.com <mika@stocksharp.com>
2025-01-11 16:05:43 +0300

    PythonExtensions. GetUnderlyingSystemType

--- Compilation.Python/PythonExtensions.cs
+++ Compilation.Python/PythonExtensions.cs
@@ -12,10 +12,15 @@ using IronPython.Runtime.Types;
 public static class PythonExtensions
 {
 	private static readonly PropertyInfo _nameProp = typeof(PythonType).GetProperty("Name", BindingFlags.Instance | BindingFlags.NonPublic);
+	private static readonly PropertyInfo _underlyingSystemTypeProp = typeof(PythonType).GetProperty("UnderlyingSystemType", BindingFlags.Instance | BindingFlags.NonPublic);
 
 	[CLSCompliant(false)]
 	public static string GetName(this PythonType type)
 		=> (string)_nameProp.GetValue(type ?? throw new ArgumentNullException(nameof(type)));
+
+	[CLSCompliant(false)]
+	public static Type GetUnderlyingSystemType(this PythonType type)
+		=> (Type)_underlyingSystemTypeProp.GetValue(type ?? throw new ArgumentNullException(nameof(type)));
 
 	[CLSCompliant(false)]
 	public static IEnumerable<PythonType> GetTypes(this ScriptScope scope)
