856a82323126f5789359dbaf2e6b6732887111cc
mika@stocksharp.com <mika@stocksharp.com>
2025-02-05 12:24:21 +0300

    Fix commit 8f9c597537f02e351a149af9b7267050e9992b7d

---
+++ ComponentModel/BasicSettingAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecng.ComponentModel;
+
+using System;
+
+/// <summary>
+/// Marks property as basic to be included into basic settings UI.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class BasicSettingAttribute : Attribute
+{
+}
