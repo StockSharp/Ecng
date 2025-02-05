9535937e97ce43c1a7011b3ba631a6aa62a3f478
mika@stocksharp.com <mika@stocksharp.com>
2025-02-05 12:32:38 +0300

    Fix commit bd3b436c81dafc1fd2ea2bb5cbcb36a9229664e5

--- Common/CryptoAttribute.cs
+++ Common/CryptoAttribute.cs
@@ -5,6 +5,7 @@ namespace Ecng.Common
 	/// <summary>
 	/// The member marked as associated with crypto currency.
 	/// </summary>
+	[AttributeUsage(AttributeTargets.All)]
 	public class CryptoAttribute : Attribute
 	{
 	}
