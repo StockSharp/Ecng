d8befa3ff684f6ae7daffc3b1f1ec6095bc5822c
mika@stocksharp.com <mika@stocksharp.com>
2025-03-22 14:48:24 +0300

    TemplateEditorAttribute, FormulaEditorAttribute

---
+++ ComponentModel/FormulaEditorAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace Ecng.ComponentModel;
+
+using System;
+
+using Ecng.Common;
+
+/// <summary>
+/// Formula editor attribute.
+/// </summary>
+public class FormulaEditorAttribute : Attribute
+{
+	/// <summary>
+	/// Type to get variables from.
+	/// </summary>
+	public Type VariablesSource { get; }
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FormulaEditorAttribute"/>.
+	/// </summary>
+	/// <param name="type">Variables source.</param>
+	public FormulaEditorAttribute(Type type)
+	{
+		if (type is null)
+			throw new ArgumentNullException(nameof(type));
+
+		VariablesSource =
+			type.Is<IItemsSource>()
+				? type
+				: type.IsEnum
+					? typeof(ItemsSourceBase<>).Make(type)
+					: throw new ArgumentException($"Type '{type}' must implement the '{typeof(IItemsSource)}' interface or be an enum.", nameof(type));
+	}
+}
---
+++ ComponentModel/TemplateEditorAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecng.ComponentModel;
+
+using System;
+
+/// <summary>
+/// Template editor attribute.
+/// </summary>
+public class TemplateEditorAttribute : Attribute
+{
+	/// <summary>
+	/// Template key.
+	/// </summary>
+	public string TemplateKey { get; set; }
+}
