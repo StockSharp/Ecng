f7faa72b50cb25d63c60434ebdc68ba122c5e961
mika@stocksharp.com <mika@stocksharp.com>
2025-04-09 17:47:10 +0300

    InMemoryLastDirSelector

--- ComponentModel/ILastDirSelector.cs
+++ ComponentModel/ILastDirSelector.cs
@@ -1,5 +1,7 @@
 ﻿namespace Ecng.ComponentModel;
 
+using System.Collections.Generic;
+
 /// <summary>
 /// Interface describes last directory selector.
 /// </summary>
@@ -19,4 +21,18 @@ public interface ILastDirSelector
 	/// <param name="ctrlName">Control name.</param>
 	/// <param name="value">The value.</param>
 	void SetValue(string ctrlName, string value);
+}
+
+/// <summary>
+/// In-memory implementation of <see cref="ILastDirSelector"/>.
+/// </summary>
+public class InMemoryLastDirSelector : ILastDirSelector
+{
+	private readonly Dictionary<string, string> _cache = [];
+
+	void ILastDirSelector.SetValue(string ctrlName, string value)
+		=> _cache[ctrlName] = value;
+
+	bool ILastDirSelector.TryGetValue(string ctrlName, out string value)
+		=> _cache.TryGetValue(ctrlName, out value);
 }
