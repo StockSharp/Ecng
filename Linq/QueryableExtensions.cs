f4126aad700633dcebf54f5911db4c6115552f91
mika@stocksharp.com <mika@stocksharp.com>
2025-06-07 18:56:23 +0300

    build fix.

--- Linq/QueryableExtensions.cs
+++ Linq/QueryableExtensions.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Reflection;
