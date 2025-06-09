be62d7d3ef2aee5cdeefb2a5abd484e1e7961b20
mika@stocksharp.com <mika@stocksharp.com>
2025-06-09 19:35:58 +0300

    More tests.

--- Tests/Reflection/ReflectionTests.cs
+++ Tests/Reflection/ReflectionTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Ecng.Tests.Reflection;
 
+using System.Collections;
 using System.Reflection;
 
 using Ecng.Reflection;
@@ -40,5 +41,269 @@ public class ReflectionTests
 
 		Do(true);
 		Do(false);
+	}
+
+	class Sample : IList<int>
+	{
+		public int Value { get; set; }
+		public int this[int i] { get => i; set { } }
+		public static int StaticValue { get; set; }
+		public event EventHandler TestEvent;
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable IDE0060 // Remove unused parameter
+		public void MethodRef(ref int x) { }
+		public void MethodOut(out int x) { x = 0; }
+		public void MethodIn(in int x) { }
+		public void MethodParams(params int[] items) { }
+		public void Method(int x) { }
+#pragma warning restore IDE0060 // Remove unused parameter
+#pragma warning restore CA1822 // Mark members as static
+
+		// IList implementation
+		public int Count => 0;
+		public bool IsReadOnly => false;
+		public void Add(int item) { }
+		public void Clear() { }
+		public bool Contains(int item) => false;
+		public void CopyTo(int[] array, int arrayIndex) { }
+		public IEnumerator<int> GetEnumerator() => Enumerable.Empty<int>().GetEnumerator();
+		public int IndexOf(int item) => -1;
+		public void Insert(int index, int item) { }
+		public bool Remove(int item) => false;
+		public void RemoveAt(int index) { }
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
+
+	[TestMethod]
+	public void TestGetInvokeMethod()
+	{
+		typeof(Action).GetInvokeMethod().Name.AssertEqual("Invoke");
+	}
+
+	[TestMethod]
+	public void TestIsParams()
+	{
+		var param = typeof(Sample).GetMethod(nameof(Sample.MethodParams)).GetParameters()[0];
+		param.IsParams().AssertTrue();
+
+		var param2 = typeof(Sample).GetMethod(nameof(Sample.Method)).GetParameters()[0];
+		param2.IsParams().AssertFalse();
+	}
+
+	[TestMethod]
+	public void TestGetParameterTypes()
+	{
+		var method = typeof(Sample).GetMethod(nameof(Sample.MethodRef));
+		var types = method.GetParameterTypes();
+		types.Length.AssertEqual(1);
+		types[0].info.Name.AssertEqual("x");
+		types[0].type.AssertEqual(typeof(int).MakeByRefType());
+
+		var types2 = method.GetParameterTypes(true);
+		types2[0].type.AssertEqual(typeof(int));
+	}
+
+	[TestMethod]
+	public void TestGetGenericTypeAndArg()
+	{
+		var listType = typeof(List<string>);
+		var genType = listType.GetGenericType(typeof(List<>));
+		genType.AssertEqual(typeof(List<string>));
+		listType.GetGenericTypeArg(typeof(List<>), 0).AssertEqual(typeof(string));
+	}
+
+	[TestMethod]
+	public void TestGetIndexer()
+	{
+		var prop = typeof(Sample).GetIndexer(typeof(int));
+		prop.Name.AssertEqual("Item");
+	}
+
+	[TestMethod]
+	public void TestGetIndexers()
+	{
+		var props = typeof(Sample).GetIndexers(typeof(int));
+		props.Length.AssertGreater(0);
+		props[0].IsIndexer().AssertTrue();
+	}
+
+	[TestMethod]
+	public void TestGetMemberAndGetMembers()
+	{
+		typeof(Sample).GetMember<ConstructorInfo>().MemberType.AssertEqual(MemberTypes.Constructor);
+		typeof(Sample).GetMember<PropertyInfo>(nameof(Sample.Value)).Name.AssertEqual("Value");
+		typeof(Sample).GetMembers<PropertyInfo>().Any(p => p.Name == "Value").AssertTrue();
+	}
+
+	[TestMethod]
+	public void TestFilterMembers()
+	{
+		var props = typeof(Sample).GetMembers<PropertyInfo>();
+		var filtered = ReflectionHelper.FilterMembers(props, null, typeof(int));
+		filtered.Any(p => p.Name == "Value").AssertTrue();
+	}
+
+	[TestMethod]
+	public void TestIsAbstractIsVirtualIsOverloadable()
+	{
+		var prop = typeof(Sample).GetProperty(nameof(Sample.Value));
+		prop.IsAbstract().AssertFalse();
+		prop.IsVirtual().AssertFalse();
+		prop.IsOverloadable().AssertFalse();
+	}
+
+	[TestMethod]
+	public void TestIsIndexer()
+	{
+		var prop = typeof(Sample).GetProperty("Item");
+		prop.IsIndexer().AssertTrue();
+		ReflectionHelper.IsIndexer(prop).AssertTrue();
+	}
+
+	[TestMethod]
+	public void TestGetIndexerTypes()
+	{
+		var prop = typeof(Sample).GetProperty("Item");
+		var idxTypes = prop.GetIndexerTypes();
+		idxTypes.First().AssertEqual(typeof(int));
+	}
+
+	[TestMethod]
+	public void TestMemberIs()
+	{
+		var prop = typeof(Sample).GetProperty(nameof(Sample.Value));
+		prop.MemberIs(MemberTypes.Property).AssertTrue();
+	}
+
+	[TestMethod]
+	public void TestIsOutput()
+	{
+		var outParam = typeof(Sample).GetMethod(nameof(Sample.MethodOut)).GetParameters()[0];
+		outParam.IsOutput().AssertTrue();
+
+		var refParam = typeof(Sample).GetMethod(nameof(Sample.MethodRef)).GetParameters()[0];
+		refParam.IsOutput().AssertTrue();
+
+		var inParam = typeof(Sample).GetMethod(nameof(Sample.MethodIn)).GetParameters()[0];
+		inParam.IsOutput().AssertTrue();
+
+		var valueParam = typeof(Sample).GetMethod(nameof(Sample.Method)).GetParameters().FirstOrDefault();
+		valueParam.IsOutput().AssertFalse();
+	}
+
+	[TestMethod]
+	public void TestGetMemberType()
+	{
+		var prop = typeof(Sample).GetProperty(nameof(Sample.Value));
+		prop.GetMemberType().AssertEqual(typeof(int));
+	}
+
+	[TestMethod]
+	public void TestIsCollection()
+	{
+		typeof(List<int>).IsCollection().AssertTrue();
+		typeof(int[]).IsCollection().AssertTrue();
+		typeof(int).IsCollection().AssertFalse();
+	}
+
+	[TestMethod]
+	public void TestIsStatic()
+	{
+		typeof(Sample).GetProperty(nameof(Sample.StaticValue)).IsStatic().AssertTrue();
+		typeof(Sample).GetProperty(nameof(Sample.Value)).IsStatic().AssertFalse();
+	}
+
+	[TestMethod]
+	public void TestGetItemType()
+	{
+		var arr = new[] { 1, 2, 3 };
+		arr.GetType().GetItemType().AssertEqual(typeof(int));
+	}
+
+	[TestMethod]
+	public void TestMakePropertyName()
+	{
+		"get_X".MakePropertyName().AssertEqual("X");
+		"set_X".MakePropertyName().AssertEqual("X");
+		"add_X".MakePropertyName().AssertEqual("X");
+		"remove_X".MakePropertyName().AssertEqual("X");
+	}
+
+	[TestMethod]
+	public void TestGetAccessorOwner()
+	{
+		var prop = typeof(Sample).GetProperty(nameof(Sample.Value));
+		var method = prop.GetGetMethod();
+		method.GetAccessorOwner().AssertEqual(prop);
+	}
+
+	[TestMethod]
+	public void TestMake()
+	{
+		var method = typeof(List<>).GetMethod("Add");
+		method.IsGenericMethodDefinition.AssertFalse();
+	}
+
+	[TestMethod]
+	public void TestIsRuntimeType()
+	{
+		// True only for System.RuntimeType (i.e. typeof(string).GetType())
+		var runtimeType = typeof(string).GetType();
+		runtimeType.IsRuntimeType().AssertTrue();
+
+		// False for most ordinary types
+		typeof(string).IsRuntimeType().AssertFalse();
+		typeof(Type).IsRuntimeType().AssertFalse();
+	}
+
+	[TestMethod]
+	public void TestIsAssemblyVerifyAssembly()
+	{
+		"Ecng.Common.dll".IsAssembly().AssertTrue();
+		"testhost.exe".IsAssembly().AssertFalse();
+		"testhost.exe".VerifyAssembly().AssertNull();
+	}
+
+	[TestMethod]
+	public void TestClearCache()
+	{
+		ReflectionHelper.ClearCache();
+	}
+
+	[TestMethod]
+	public void TestFindImplementationsIsRequiredTypeTryFindType()
+	{
+		var list = new[] { typeof(List<int>), typeof(string) };
+		var found = list.TryFindType(t => t == typeof(List<int>), "List`1");
+		found.AssertEqual(typeof(List<int>));
+
+		typeof(List<int>).IsRequiredType<List<int>>().AssertTrue();
+	}
+
+	[TestMethod]
+	public void TestOrderByDeclaration()
+	{
+		var props = typeof(Sample).GetProperties();
+		props.OrderByDeclaration().Count().AssertEqual(props.Length);
+	}
+
+	[TestMethod]
+	public void TestIsModifiable()
+	{
+		var prop = typeof(Sample).GetProperty(nameof(Sample.Value));
+		prop.IsModifiable().AssertTrue();
+	}
+
+	[TestMethod]
+	public void TestIsMatch()
+	{
+		var prop = typeof(Sample).GetProperty(nameof(Sample.Value));
+		prop.IsMatch(BindingFlags.Public | BindingFlags.Instance).AssertTrue();
+
+		var method = typeof(Sample).GetMethod(nameof(Sample.Method));
+		method.IsMatch(BindingFlags.Public | BindingFlags.Instance).AssertTrue();
+
+		var field = typeof(DateTime).GetField("MinValue", BindingFlags.Public | BindingFlags.Static);
+		field.IsMatch(BindingFlags.Public | BindingFlags.Static).AssertTrue();
 	}
 }
--- Tests/Security/SecretTests.cs
+++ Tests/Security/SecretTests.cs
@@ -58,4 +58,209 @@ public class SecretTests
 		txt.Length.AssertEqual(65735);
 		txt.UTF8().Md5().AssertEqual("96FB3B2D0226B0A18903E929E1238557");
 	}
+
+	[TestMethod]
+	public void RsaParameters_ConvertToAndFromBytes()
+	{
+		var rsa = CryptoHelper.GenerateRsa();
+		var bytes = rsa.FromRsa();
+		var restored = bytes.ToRsa();
+		restored.Modulus.AssertEqual(rsa.Modulus);
+		restored.Exponent.AssertEqual(rsa.Exponent);
+	}
+
+	[TestMethod]
+	public void RsaParameters_PublicPart()
+	{
+		var rsa = CryptoHelper.GenerateRsa();
+		var pub = rsa.PublicPart();
+		pub.Exponent.AssertEqual(rsa.Exponent);
+		pub.Modulus.AssertEqual(rsa.Modulus);
+	}
+
+	[TestMethod]
+	public void EncryptDecrypt_Aes()
+	{
+		var plain = "test data".UTF8();
+		var salt = "salt123456789012".ASCII();
+		var iv = "iv12345678901234".ASCII();
+		var pass = "password";
+		var encrypted = plain.Encrypt(pass, salt, iv);
+		var decrypted = encrypted.Decrypt(pass, salt, iv);
+		decrypted.AssertEqual(plain);
+	}
+
+	[TestMethod]
+	public void EncryptDecrypt_TransformAes()
+	{
+		var plain = "test data2".UTF8();
+		var salt = "salt123456789012".ASCII();
+		var iv = "iv12345678901234".ASCII();
+		var pass = "password2";
+		var encrypted = plain.EncryptAes(pass, salt, iv);
+		var decrypted = encrypted.DecryptAes(pass, salt, iv);
+		decrypted.AssertEqual(plain);
+	}
+
+	[TestMethod]
+	public void Encrypt_ThrowsOnNulls()
+	{
+		var salt = "salt123456789012".ASCII();
+		var iv = "iv12345678901234".ASCII();
+		Assert.ThrowsExactly<ArgumentNullException>(() => ((byte[])null).Encrypt("pass", salt, iv));
+		Assert.ThrowsExactly<ArgumentNullException>(() => "data".UTF8().Encrypt(null, salt, iv));
+	}
+
+	[TestMethod]
+	public void IsValid_SecureString_ThrowsOnNull()
+	{
+		Secret secret = _correctPwd.CreateSecret();
+		Assert.ThrowsExactly<ArgumentNullException>(() => secret.IsValid((System.Security.SecureString)null));
+	}
+
+	[TestMethod]
+	public void IsValid_String_ThrowsOnNull()
+	{
+		Secret secret = _correctPwd.CreateSecret();
+		Assert.ThrowsExactly<ArgumentNullException>(() => secret.IsValid((string)null));
+	}
+
+	[TestMethod]
+	public void CreateSecret_WithSecretArg()
+	{
+		var secret1 = _correctPwd.CreateSecret();
+		var secret2 = _correctPwd.CreateSecret(secret1);
+		secret2.IsValid(_correctPwd).AssertTrue();
+	}
+
+	[TestMethod]
+	public void CreateSecret_ThrowsOnNull()
+	{
+		System.Security.SecureString s = null;
+		Assert.ThrowsExactly<ArgumentNullException>(() => s.CreateSecret());
+	}
+
+	[TestMethod]
+	public void Md5_Sha256_Sha512_Correctness()
+	{
+		var data = "abc".UTF8();
+		// Проверка корректности хэшей
+		data.Md5().AssertEqual("900150983CD24FB0D6963F7D28E17F72");
+		data.Sha256().AssertEqual("BA7816BF8F01CFEA414140DE5DAE2223B00361A396177A9CB410FF61F20015AD");
+		data.Sha512().AssertEqual("DDAF35A193617ABACC417349AE20413112E6FA4E89A97EA20A9EEEE64B55D39A2192992A274FC1A836BA3C23A3FEEBBD454D4423643CE80E2A9AC94FA54CA49F");
+	}
+
+	[TestMethod]
+	public void CreateSecret_CopiesSaltAndAlgo()
+	{
+		var salt = TypeHelper.GenerateSalt(Secret.DefaultSaltSize);
+		var algo = CryptoAlgorithm.Create(AlgorithmTypes.Hash);
+		var secret1 = _correctPwd.CreateSecret(salt, algo);
+		var secret2 = _correctPwd.CreateSecret(secret1);
+		secret2.Salt.AssertEqual(secret1.Salt);
+		(secret2.Algo?.GetType()).AssertEqual(secret1.Algo?.GetType());
+	}
+
+	[TestMethod]
+	public void CreateSecret_EmptyString_Throws()
+	{
+		Assert.ThrowsExactly<ArgumentNullException>(() => "".CreateSecret());
+	}
+
+	[TestMethod]
+	public void CreateSecret_NullString_Throws()
+	{
+		Assert.ThrowsExactly<ArgumentNullException>(() => ((string)null).CreateSecret());
+	}
+
+	[TestMethod]
+	public void CreateSecret_NullSalt_Throws()
+	{
+		Assert.ThrowsExactly<ArgumentNullException>(() => _correctPwd.CreateSecret(null));
+	}
+
+	[TestMethod]
+	public void CreateSecret_NullSecret_Throws()
+	{
+		Assert.ThrowsExactly<ArgumentNullException>(() => _correctPwd.CreateSecret((Secret)null));
+	}
+
+	[TestMethod]
+	public void CreateSecret_SecureString_Correct()
+	{
+		var s = _correctPwd.Secure();
+		var secret = s.CreateSecret();
+		secret.IsValid(_correctPwd).AssertTrue();
+	}
+
+	[TestMethod]
+	public void CreateSecret_SecureString_Null_Throws()
+	{
+		System.Security.SecureString s = null;
+		Assert.ThrowsExactly<ArgumentNullException>(() => s.CreateSecret());
+	}
+
+	[TestMethod]
+	public void IsValid_NullSecret_Throws()
+	{
+		Assert.ThrowsExactly<ArgumentNullException>(() => ((Secret)null).IsValid(_correctPwd));
+	}
+
+	[TestMethod]
+	public void IsValid_NullPassword_Throws()
+	{
+		var secret = _correctPwd.CreateSecret();
+		Assert.ThrowsExactly<ArgumentNullException>(() => secret.IsValid((string)null));
+	}
+
+	[TestMethod]
+	public void IsValid_NullSecureString_Throws()
+	{
+		var secret = _correctPwd.CreateSecret();
+		Assert.ThrowsExactly<ArgumentNullException>(() => secret.IsValid((System.Security.SecureString)null));
+	}
+
+	[TestMethod]
+	public void EncryptDecryptAes_RandomData()
+	{
+		var plain = System.Text.Encoding.UTF8.GetBytes("random data");
+		var salt = System.Text.Encoding.ASCII.GetBytes("salt123456789012");
+		var iv = System.Text.Encoding.ASCII.GetBytes("iv12345678901234");
+		var pass = "password";
+		var encrypted = plain.EncryptAes(pass, salt, iv);
+		var decrypted = encrypted.DecryptAes(pass, salt, iv);
+		decrypted.AssertEqual(plain);
+	}
+
+	[TestMethod]
+	public void EncryptDecrypt_NullPlain_Throws()
+	{
+		var salt = "salt123456789012".ASCII();
+		var iv = "iv12345678901234".ASCII();
+		Assert.ThrowsExactly<ArgumentNullException>(() => ((byte[])null).Encrypt("pass", salt, iv));
+	}
+
+	[TestMethod]
+	public void EncryptDecrypt_NullPass_Throws()
+	{
+		var salt = "salt123456789012".ASCII();
+		var iv = "iv12345678901234".ASCII();
+		Assert.ThrowsExactly<ArgumentNullException>(() => "data".UTF8().Encrypt(null, salt, iv));
+	}
+
+	[TestMethod]
+	public void ToRsa_Null_Throws()
+	{
+		Assert.ThrowsExactly<ArgumentNullException>(() => ((byte[])null).ToRsa());
+	}
+
+	[TestMethod]
+	public void FromRsa_ToRsa_Correctness()
+	{
+		var rsa = CryptoHelper.GenerateRsa();
+		var bytes = rsa.FromRsa();
+		var restored = bytes.ToRsa();
+		restored.Modulus.AssertEqual(rsa.Modulus);
+		restored.Exponent.AssertEqual(rsa.Exponent);
+	}
 }
