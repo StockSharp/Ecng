fc9b96be4f01ec002e8a08cf12b182ed85dc8821
mika@stocksharp.com <mika@stocksharp.com>
2025-06-16 00:21:04 +0300

    AssertHelper. Optional msg.

--- UnitTesting/AssertHelper.cs
+++ UnitTesting/AssertHelper.cs
@@ -16,22 +16,25 @@ public static class AssertHelper
 	/// Asserts that the value is true.
 	/// </summary>
 	/// <param name="value">Value.</param>
-	public static void AssertTrue(this bool value)
-		=> Assert.IsTrue(value);
+	/// <param name="message">Error message.</param>
+	public static void AssertTrue(this bool value, string message = null)
+		=> Assert.IsTrue(value, message);
 
 	/// <summary>
 	/// Asserts that the value is false.
 	/// </summary>
 	/// <param name="value">Value.</param>
-	public static void AssertFalse(this bool value)
-		=> Assert.IsFalse(value);
+	/// <param name="message">Error message.</param>
+	public static void AssertFalse(this bool value, string message = null)
+		=> Assert.IsFalse(value, message);
 
 	/// <summary>
 	/// Asserts that the value is null.
 	/// </summary>
 	/// <param name="value">Value.</param>
-	public static void AssertNull(this object value)
-		=> Assert.IsNull(value);
+	/// <param name="message">Error message.</param>
+	public static void AssertNull(this object value, string message = null)
+		=> Assert.IsNull(value, message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
@@ -47,24 +50,27 @@ public static class AssertHelper
 	/// Asserts that the value is not null.
 	/// </summary>
 	/// <param name="value">Value.</param>
-	public static void AssertNotNull(this object value)
-		=> Assert.IsNotNull(value);
+	/// <param name="message">Error message.</param>
+	public static void AssertNotNull(this object value, string message = null)
+		=> Assert.IsNotNull(value, message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
 	/// </summary>
 	/// <typeparam name="T">Type of the value.</typeparam>
 	/// <param name="value">Value.</param>
-	public static void AssertOfType<T>(this object value)
-		=> Assert.IsInstanceOfType(value, typeof(T));
+	/// <param name="message">Error message.</param>
+	public static void AssertOfType<T>(this object value, string message = null)
+		=> Assert.IsInstanceOfType(value, typeof(T), message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
 	/// </summary>
 	/// <typeparam name="T">Type of the value.</typeparam>
 	/// <param name="value">Value.</param>
-	public static void AssertNotOfType<T>(this object value)
-		=> Assert.IsNotInstanceOfType(value, typeof(T));
+	/// <param name="message">Error message.</param>
+	public static void AssertNotOfType<T>(this object value, string message = null)
+		=> Assert.IsNotInstanceOfType(value, typeof(T), message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
@@ -72,8 +78,9 @@ public static class AssertHelper
 	/// <typeparam name="T">Type of the value.</typeparam>
 	/// <param name="value">Value.</param>
 	/// <param name="expected">Expected value.</param>
-	public static void AreEqual<T>(this T value, T expected)
-		=> value.AssertEqual(expected);
+	/// <param name="message">Error message.</param>
+	public static void AreEqual<T>(this T value, T expected, string message = null)
+		=> value.AssertEqual(expected, message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
@@ -81,12 +88,13 @@ public static class AssertHelper
 	/// <typeparam name="T">Type of the value.</typeparam>
 	/// <param name="value">Value.</param>
 	/// <param name="expected">Expected value.</param>
-	public static void AssertEqual<T>(this T value, T expected)
+	/// <param name="message">Error message.</param>
+	public static void AssertEqual<T>(this T value, T expected, string message = null)
 	{
 		if (value is SecureString str)
-			str.IsEqualTo(expected.To<SecureString>()).AssertTrue();
+			str.IsEqualTo(expected.To<SecureString>()).AssertTrue(message);
 		else
-			Assert.AreEqual(expected, value);
+			Assert.AreEqual(expected, value, message);
 	}
 
 	/// <summary>
@@ -95,8 +103,9 @@ public static class AssertHelper
 	/// <param name="value">Value.</param>
 	/// <param name="expected">Expected value.</param>
 	/// <param name="delta">Delta for comparing floating point numbers.</param>
-	public static void AssertEqual(this double value, double expected, double delta)
-		=> Assert.AreEqual(expected, value, delta);
+	/// <param name="message">Error message.</param>
+	public static void AssertEqual(this double value, double expected, double delta, string message = null)
+		=> Assert.AreEqual(expected, value, delta, message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
@@ -104,8 +113,9 @@ public static class AssertHelper
 	/// <param name="value">Value.</param>
 	/// <param name="expected">Expected value.</param>
 	/// <param name="delta">Delta for comparing floating point numbers.</param>
-	public static void AssertEqual(this float value, float expected, float delta)
-		=> Assert.AreEqual(expected, value, delta);
+	/// <param name="message">Error message.</param>
+	public static void AssertEqual(this float value, float expected, float delta, string message = null)
+		=> Assert.AreEqual(expected, value, delta, message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
@@ -113,8 +123,9 @@ public static class AssertHelper
 	/// <typeparam name="T">Type of the value.</typeparam>
 	/// <param name="value">Value.</param>
 	/// <param name="expected">Expected value.</param>
-	public static void AssertNotEqual<T>(this T value, T expected)
-		=> Assert.AreNotEqual(expected, value);
+	/// <param name="message">Error message.</param>
+	public static void AssertNotEqual<T>(this T value, T expected, string message = null)
+		=> Assert.AreNotEqual(expected, value, message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
@@ -122,8 +133,9 @@ public static class AssertHelper
 	/// <typeparam name="T">Type of the value.</typeparam>
 	/// <param name="value">Value.</param>
 	/// <param name="expected">Expected value.</param>
-	public static void AssertSame<T>(this T value, T expected)
-		=> Assert.AreSame(expected, value);
+	/// <param name="message">Error message.</param>
+	public static void AssertSame<T>(this T value, T expected, string message = null)
+		=> Assert.AreSame(expected, value, message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
@@ -131,8 +143,9 @@ public static class AssertHelper
 	/// <typeparam name="T">Type of the value.</typeparam>
 	/// <param name="value">Value.</param>
 	/// <param name="expected">Expected value.</param>
-	public static void AssertNotSame<T>(this T value, T expected)
-		=> Assert.AreNotSame(expected, value);
+	/// <param name="message">Error message.</param>
+	public static void AssertNotSame<T>(this T value, T expected, string message = null)
+		=> Assert.AreNotSame(expected, value, message);
 
 	/// <summary>
 	/// Asserts that the value is not null.
@@ -145,9 +158,8 @@ public static class AssertHelper
 		if (nullAsEmpty && value.IsEmpty() && expected.IsEmpty())
 			return;
 
-		value.AssertEqual(expected);
+		Assert.AreEqual(expected, value);
 	}
-
 
 	/// <summary>
 	/// Asserts that two arrays are equal by comparing their lengths and elements.
@@ -155,20 +167,21 @@ public static class AssertHelper
 	/// <typeparam name="T">The type of the array elements.</typeparam>
 	/// <param name="value">The actual array to be tested. Can be null.</param>
 	/// <param name="expected">The expected array. If <paramref name="value"/> is null, then <paramref name="expected"/> must also be null.</param>
-	public static void AssertEqual<T>(this T[] value, T[] expected)
+	/// <param name="message">Error message.</param>
+	public static void AssertEqual<T>(this T[] value, T[] expected, string message = null)
 	{
 		if (value is null)
 		{
-			expected.AssertNull();
+			expected.AssertNull(message);
 			return;
 		}
 
-		expected.AssertNotNull();
+		expected.AssertNotNull(message);
 
-		value.Length.AssertEqual(expected.Length);
+		value.Length.AssertEqual(expected.Length, message);
 
 		for (var i = 0; i < value.Length; i++)
-			value[i].AssertEqual(expected[i]);
+			value[i].AssertEqual(expected[i], message);
 	}
 
 	/// <summary>
@@ -177,10 +190,11 @@ public static class AssertHelper
 	/// <typeparam name="T">Type implementing <see cref="IComparable{T}"/>.</typeparam>
 	/// <param name="value">Actual value.</param>
 	/// <param name="expected">Value to compare against.</param>
-	public static void AssertGreater<T>(this T value, T expected)
+	/// <param name="message">Error message.</param>
+	public static void AssertGreater<T>(this T value, T expected, string message = null)
 		where T : IComparable<T>
 	{
-		(value.CompareTo(expected) > 0).AssertTrue();
+		(value.CompareTo(expected) > 0).AssertTrue(message);
 	}
 
 	/// <summary>
@@ -189,10 +203,11 @@ public static class AssertHelper
 	/// <typeparam name="T">Type implementing <see cref="IComparable{T}"/>.</typeparam>
 	/// <param name="value">Actual value.</param>
 	/// <param name="expected">Value to compare against.</param>
-	public static void AssertLess<T>(this T value, T expected)
+	/// <param name="message">Error message.</param>
+	public static void AssertLess<T>(this T value, T expected, string message = null)
 		where T : IComparable<T>
 	{
-		(value.CompareTo(expected) < 0).AssertTrue();
+		(value.CompareTo(expected) < 0).AssertTrue(message);
 	}
 
 	/// <summary>
@@ -202,10 +217,11 @@ public static class AssertHelper
 	/// <param name="value">Actual value.</param>
 	/// <param name="min">Minimum value (exclusive).</param>
 	/// <param name="max">Maximum value (exclusive).</param>
-	public static void AssertInRange<T>(this T value, T min, T max)
+	/// <param name="message">Error message.</param>
+	public static void AssertInRange<T>(this T value, T min, T max, string message = null)
 		where T : IComparable<T>
 	{
-		value.AssertGreater(min);
-		value.AssertLess(max);
+		value.AssertGreater(min, message);
+		value.AssertLess(max, message);
 	}
 }
