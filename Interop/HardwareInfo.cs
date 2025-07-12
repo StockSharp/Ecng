de81f2353ebb53438a38e14aeea562517dd7d9ab
mika@stocksharp.com <mika@stocksharp.com>
2025-07-12 23:29:29 +0300

    HardwareInfo. MacOS supported.

--- Interop/HardwareInfo.cs
+++ Interop/HardwareInfo.cs
@@ -37,8 +37,12 @@ public static class HardwareInfo
 
 		if (OperatingSystemEx.IsWindows())
 			id = await GetIdWindows(cancellationToken);
-		else
+		else if (OperatingSystemEx.IsMacOS())
+			id = await GetIdMacOSAsync(cancellationToken);
+		else if (OperatingSystemEx.IsLinux())
 			id = await GetIdLinuxAsync(cancellationToken);
+		else
+			throw new PlatformNotSupportedException($"Platform {Environment.OSVersion.Platform} is not supported.");
 
 		if (id.IsEmpty())
 			throw new InvalidOperationException("Cannot generate HDDID.");
@@ -78,13 +82,13 @@ public static class HardwareInfo
 
 	private static async Task<string> GetIdLinuxAsync(CancellationToken cancellationToken)
 	{
-		var macs = GetLinuxMacs();
+		var macs = GetNetworkMacs();
 		var volId = await GetLinuxVolumeIdAsync(cancellationToken);
 
-		return macs.Join(string.Empty) + volId;
+		return macs + volId;
 	}
 
-	private static List<string> GetLinuxMacs()
+	private static string GetNetworkMacs()
 	{
 		var result = new HashSet<string>();
 		var ifaces = NetworkInterface
@@ -101,7 +105,7 @@ public static class HardwareInfo
 			.Where(s => s != "ffffffffffff" && s != "000000000000")
 			.ToList();
 
-		return list;
+		return list.Join(string.Empty);
 	}
 
 	private static readonly Regex _lsblkRegex = new(@"^\s*/\s+([\da-f-]+)\s*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
@@ -112,19 +116,51 @@ public static class HardwareInfo
 		var result = new List<string>();
 
 		var res = await IOHelper.ExecuteAsync("lsblk", "-r -o MOUNTPOINT,UUID", str =>
-			{
-				var m = _lsblkRegex.Match(str);
-				if (m.Success)
-					result.Add(m.Groups[1].Value);
-			},
-			errors.Add,
-			info => info.EnvironmentVariables["PATH"] = "/bin:/sbin:/usr/bin:/usr/sbin", cancellationToken: cancellationToken).NoWait();
+		{
+			var m = _lsblkRegex.Match(str);
+			if (m.Success)
+				result.Add(m.Groups[1].Value);
+		},
+		errors.Add,
+		info => info.EnvironmentVariables["PATH"] = "/bin:/sbin:/usr/bin:/usr/sbin", cancellationToken: cancellationToken).NoWait();
 
 		if (res != 0 || errors.Any())
 			throw new InvalidOperationException($"Unable to execute lsblk. Return code {res}.\n{errors.JoinNL()}");
 
 		//if (result.Count != 1)
 		//	throw new InvalidOperationException($"invalid lsblk result. got {result.Count} values: {result.JoinComma()}");
+
+		return result.FirstOrDefault()?.Remove("-").ToLowerInvariant();
+	}
+
+	private static async Task<string> GetIdMacOSAsync(CancellationToken cancellationToken)
+	{
+		var macs = GetNetworkMacs();
+		var volId = await GetMacOSVolumeIdAsync(cancellationToken);
+
+		return macs + volId;
+	}
+
+	private static async Task<string> GetMacOSVolumeIdAsync(CancellationToken cancellationToken)
+	{
+		var errors = new List<string>();
+		var result = new List<string>();
+
+		var res = await IOHelper.ExecuteAsync("diskutil", "info /", str =>
+		{
+			if (!str.ContainsIgnoreCase("Volume UUID:"))
+				return;
+
+			var uuid = str.Split(':')[1].Trim();
+
+			if (!uuid.IsEmpty())
+				result.Add(uuid);
+		},
+		errors.Add,
+		cancellationToken: cancellationToken).NoWait();
+
+		if (res != 0 || errors.Any())
+			throw new InvalidOperationException($"Unable to execute diskutil. Return code {res}.\n{errors.JoinNL()}");
 
 		return result.FirstOrDefault()?.Remove("-").ToLowerInvariant();
 	}
