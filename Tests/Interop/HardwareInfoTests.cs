3b3c54b33c0014917c0296ff772acca2b24da75c
mika@stocksharp.com <mika@stocksharp.com>
2025-07-14 22:51:38 +0300

    workflows. mac os.

--- Tests/Interop/HardwareInfoTests.cs
+++ Tests/Interop/HardwareInfoTests.cs
@@ -8,6 +8,9 @@ public class HardwareInfoTests
 	[TestMethod]
 	public async Task HddId()
 	{
+		if (OperatingSystemEx.IsMacOS())
+			return;
+
 		((await HardwareInfo.GetIdAsync()).Length > 10).AssertTrue();
 	}
 }
