0ac39d27cfac5034bfacbc28b51adcb2cdc1d0e6
mika@stocksharp.com <mika@stocksharp.com>
2025-10-27 07:42:59 +0300

    NugetProviderFactoryTests

---
+++ Tests/Nuget/NugetProviderFactoryTests.cs
@@ -0,0 +1,62 @@
+namespace Ecng.Tests.Nuget;
+
+using System.Security;
+
+using Ecng.Logging;
+using Ecng.Nuget;
+
+[TestClass]
+public class NugetProviderFactoryTests
+{
+	[TestMethod]
+	public void Constructor_NullLog_ThrowsArgumentNullException()
+	{
+		// Arrange
+		ILogReceiver log = null;
+		var token = "t".Secure().ReadOnly();
+
+		// Act & Assert
+		Assert.ThrowsExactly<ArgumentNullException>(() => new NugetProviderFactory(log, token));
+	}
+
+	[TestMethod]
+	public void Constructor_NullToken_ThrowsArgumentNullException()
+	{
+		// Arrange
+		var log = new LogReceiver("test");
+		SecureString token = null;
+
+		// Act & Assert
+		Assert.ThrowsExactly<ArgumentNullException>(() => new NugetProviderFactory(log, token));
+	}
+
+	[TestMethod]
+	public void Constructor_ValidParameters_CreatesInstance()
+	{
+		// Arrange
+		var log = new LogReceiver("test");
+		var token = "test".Secure().ReadOnly();
+
+		// Act
+		var factory = new NugetProviderFactory(log, token);
+
+		// Assert
+		factory.AssertNotNull();
+	}
+
+	[TestMethod]
+	public void GetCoreV3_ReturnsProviders()
+	{
+		// Arrange
+		var log = new LogReceiver("test");
+		var token = "t".Secure().ReadOnly();
+		var factory = new NugetProviderFactory(log, token);
+
+		// Act
+		var providers = factory.GetCoreV3().ToArray();
+
+		// Assert
+		providers.AssertNotNull();
+		(providers.Length > 0).AssertTrue();
+	}
+}
