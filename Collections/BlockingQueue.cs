be2203b43d476c1a5870f7d117f4e6a3530dba13
mika@stocksharp.com <mika@stocksharp.com>
2025-11-24 06:10:34 +0300

    BlockingQueue made obsolete.

--- Collections/BlockingQueue.cs
+++ Collections/BlockingQueue.cs
@@ -1,9 +1,12 @@
 ﻿namespace Ecng.Collections;
 
+using System;
+
 /// <summary>
 /// Represents a thread-safe blocking queue implementation for storing and retrieving items of type <typeparamref name="T"/>.
 /// </summary>
 /// <typeparam name="T">The type of elements in the queue.</typeparam>
+[Obsolete("Use Channels instead.")]
 public sealed class BlockingQueue<T> : BaseBlockingQueue<T, QueueEx<T>>
 {
 	/// <summary>
