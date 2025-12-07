c1cc48eb6c3747d60e452f1f74200879cd251be2
mika@stocksharp.com <mika@stocksharp.com>
2025-12-07 22:33:13 +0300

    prev commit fix.

--- UnitTesting/BaseTestClass.cs
+++ UnitTesting/BaseTestClass.cs
@@ -35,7 +35,7 @@ public abstract class BaseTestClass
 	/// <param name="action">Delegate to code to be tested and which is expected to throw exception.</param>
 	/// <param name="message">The message to include in the exception when action does not throw.</param>
 	/// <returns>The exception that was thrown.</returns>
-	protected static T Throws<T>(Action action, string message = default)
+	protected static T Throws<T>(Action action, string message = "")
 		where T : Exception
 		=> Assert.Throws<T>(action, message);
 
@@ -46,7 +46,7 @@ public abstract class BaseTestClass
 	/// <param name="action">Delegate to code to be tested and which is expected to throw exception.</param>
 	/// <param name="message">The message to include in the exception when action does not throw.</param>
 	/// <returns>The exception that was thrown.</returns>
-	protected static T ThrowsExactly<T>(Action action, string message = default)
+	protected static T ThrowsExactly<T>(Action action, string message = "")
 		where T : Exception
 		=> Assert.ThrowsExactly<T>(action, message);
 
@@ -57,7 +57,7 @@ public abstract class BaseTestClass
 	/// <param name="action">Delegate to async code to be tested and which is expected to throw exception.</param>
 	/// <param name="message">The message to include in the exception when action does not throw.</param>
 	/// <returns>The exception that was thrown.</returns>
-	protected static Task<T> ThrowsAsync<T>(Func<Task> action, string message = default)
+	protected static Task<T> ThrowsAsync<T>(Func<Task> action, string message = "")
 		where T : Exception
 		=> Assert.ThrowsAsync<T>(action, message);
 
@@ -68,7 +68,7 @@ public abstract class BaseTestClass
 	/// <param name="action">Delegate to async code to be tested and which is expected to throw exception.</param>
 	/// <param name="message">The message to include in the exception when action does not throw.</param>
 	/// <returns>The exception that was thrown.</returns>
-	protected static Task<T> ThrowsExactlyAsync<T>(Func<Task> action, string message = default)
+	protected static Task<T> ThrowsExactlyAsync<T>(Func<Task> action, string message = "")
 		where T : Exception
 		=> Assert.ThrowsExactlyAsync<T>(action, message);
 }
