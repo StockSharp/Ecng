79605f889d316c4ce1b871f641f1d5222d436f1b
mika@stocksharp.com <mika@stocksharp.com>
2025-12-08 20:47:23 +0300

    Code cleanup.

--- Nuget/NugetProviderFactory.cs
+++ Nuget/NugetProviderFactory.cs
@@ -1,7 +1,5 @@
 ﻿namespace Ecng.Nuget;
 
-using Newtonsoft.Json.Linq;
-
 /// <summary>
 /// The factory for the Nuget provider.
 /// </summary>
@@ -9,69 +7,6 @@ using Newtonsoft.Json.Linq;
 /// <param name="privateNugetToken">The private Nuget token.</param>
 public class NugetProviderFactory(ILogReceiver log, SecureString privateNugetToken) : Repository.ProviderFactory
 {
-	private class NugetRegistrationResourceProvider : ResourceProvider
-	{
-		private class NugetRegistrationResourceV3 : RegistrationResourceV3
-		{
-			private readonly LogReceiver _log = new(nameof(NugetRegistrationResourceV3));
-
-			public NugetRegistrationResourceV3(ILogSource parentLog, HttpSource client, Uri baseUrl) : base(client, baseUrl)
-			{
-				_log.Parent = parentLog;
-				_log.LogDebug("created");
-			}
-
-			public override async Task<JObject> GetPackageMetadata(PackageIdentity identity, SourceCacheContext cacheContext, NuGet.Common.ILogger log, CancellationToken token)
-			{
-				var results = (await GetPackageMetadata(identity.Id, new VersionRange(identity.Version, true, identity.Version, true), true, true, cacheContext, log, token).NoWait()).ToArray();
-
-				if (results.Length < 2)
-					return results.SingleOrDefault();
-
-				_log.AddWarningLog("ERROR! GetPackageMetadata returned multiple results for '{0}':\n{1}", identity, results.Select(r => r.ToString()).Join("\n=======================\n"));
-
-				var groups = results.GroupBy(r => r["packageContent"].Value<string>()).ToArray();
-				if (groups.Length != 1)
-				{
-					_log.AddErrorLog("Multiple addresses for the same package version '{0}':\n{1}", identity, groups.Select(g => g.Key).JoinN());
-					return results.SingleOrDefault(); // will throw
-				}
-
-				var contentAddr = groups[0].Key;
-				if (!contentAddr.ContainsIgnoreCase(NugetRepoProvider.PrivateRepoKey))
-					_log.AddWarningLog("unexpected package source: {0}", groups[0].Key);
-
-				return results.First();
-			}
-		}
-
-		private readonly LogReceiver _log = new(nameof(NugetRegistrationResourceProvider));
-
-		public NugetRegistrationResourceProvider(ILogSource parentLog)
-			: base(typeof(RegistrationResourceV3), nameof(RegistrationResourceV3), NuGetResourceProviderPositions.First)
-		{
-			_log.Parent = parentLog;
-			_log.LogDebug("created");
-		}
-
-		public override async Task<Tuple<bool, INuGetResource>> TryCreate(SourceRepository source, CancellationToken token)
-		{
-			RegistrationResourceV3 regResource = null;
-			var serviceIndex = await source.GetResourceAsync<ServiceIndexResourceV3>(token).NoWait();
-
-			if (serviceIndex != null)
-			{
-				var baseUrl = serviceIndex.GetServiceEntryUri(ServiceTypes.RegistrationsBaseUrl);
-
-				var httpSourceResource = await source.GetResourceAsync<HttpSourceResource>(token).NoWait();
-
-				regResource = new NugetRegistrationResourceV3(_log, httpSourceResource.HttpSource, baseUrl);
-			}
-
-			return new Tuple<bool, INuGetResource>(regResource != null, regResource);
-		}
-	}
-
 	private class NugetHttpHandlerProvider(ILogReceiver log, SecureString privateNugetToken) : ResourceProvider(typeof(HttpHandlerResource), nameof(NugetHttpHandlerProvider), NuGetResourceProviderPositions.First)
 	{
 		private class NugetAuthHandler(HttpClientHandler clientHandler, SecureString privateNugetToken) : DelegatingHandler(clientHandler)
@@ -96,7 +31,7 @@ public class NugetProviderFactory(ILogReceiver log, SecureString privateNugetTok
 		public override async Task<Tuple<bool, INuGetResource>> TryCreate(SourceRepository source, CancellationToken token)
 		{
 			var res = await _inner.TryCreate(source, token).NoWait();
-			if (!res.Item1 || !source.PackageSource.Source.ToLowerInvariant().Contains(NugetRepoProvider.PrivateRepoKey))
+			if (!res.Item1 || !source.PackageSource.Source.ContainsIgnoreCase(NugetRepoProvider.PrivateRepoKey))
 				return res;
 
 			if (res.Item2 is not HttpHandlerResourceV3 oldHandler)
