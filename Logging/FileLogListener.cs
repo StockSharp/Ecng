4855539ee9f016a3e494d2d2aac0c8f212e3c80d
mika@stocksharp.com <mika@stocksharp.com>
2025-12-10 14:53:17 +0300

    FileLogListener memory usage reduced.

--- Logging/FileLogListener.cs
+++ Logging/FileLogListener.cs
@@ -3,6 +3,7 @@ namespace Ecng.Logging;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
+using System.Threading;
 
 using Ecng.Localization;
 
@@ -58,6 +59,10 @@ public enum FileLogHistoryPolicies
 /// </summary>
 public class FileLogListener : LogListener
 {
+	private const int _maxHistoryDaysToScan = 365;
+	private static readonly int _maxDateChars = "yyyy/MM/dd".Length;	// 10
+	private static readonly int _maxTimeChars = "HH:mm:ss.fff".Length;	// 12
+
 	private static readonly char[] _digitChars;
 
 	static FileLogListener()
@@ -342,7 +347,6 @@ public class FileLogListener : LogListener
 			case FileLogHistoryPolicies.None:
 				return;
 			case FileLogHistoryPolicies.Delete:
-				break;
 			case FileLogHistoryPolicies.Compression:
 				break;
 			case FileLogHistoryPolicies.Move:
@@ -358,11 +362,21 @@ public class FileLogListener : LogListener
 				throw new ArgumentOutOfRangeException(nameof(HistoryPolicy), policy, "Invalid value.".Localize());
 		}
 
+		var files = CollectHistoryFiles(isDir);
+
+		if (files.Count == 0)
+			return;
+
+		ApplyHistoryPolicy(policy, files, isDir);
+	}
+
+	private List<string> CollectHistoryFiles(bool isDir)
+	{
 		var files = new List<string>();
 
 		var start = DateTime.Today - HistoryAfter;
 
-		for (var i = 0; i < 365; i++)
+		for (var i = 0; i < _maxHistoryDaysToScan; i++)
 		{
 			var dateStr = (start - TimeSpan.FromDays(i)).ToString(DirectoryDateFormat);
 
@@ -377,79 +391,83 @@ public class FileLogListener : LogListener
 				files.AddRange(Directory.GetFiles(LogDirectory, $"{dateStr}_*{Extension}"));
 		}
 
-		if (files.Count == 0)
-			return;
+		return files;
+	}
 
+	private void ApplyHistoryPolicy(FileLogHistoryPolicies policy, List<string> files, bool isDir)
+	{
 		switch (policy)
 		{
 			case FileLogHistoryPolicies.Delete:
-			{
-				if (isDir)
-				{
-					foreach (var file in files)
-						Directory.Delete(file, true);
-				}
-				else
-				{
-					foreach (var file in files)
-						File.Delete(file);
-				}
-
+				DeleteHistoryFiles(files, isDir);
 				break;
-			}
 			case FileLogHistoryPolicies.Compression:
-			{
-				if (isDir)
-				{
-					foreach (var file in files)
-					{
-						ZipFile.CreateFromDirectory(file, Path.Combine(LogDirectory, $"{Path.GetFileName(file)}.zip"), HistoryCompressionLevel, false);
-						Directory.Delete(file, true);
-					}
-				}
-				else
-				{
-					foreach (var file in files)
-					{
-						using (var zipFile = new ZipArchive(File.Open(Path.Combine(LogDirectory, $"{Path.GetFileNameWithoutExtension(file)}.zip"), FileMode.Create), ZipArchiveMode.Create))
-							zipFile.CreateEntryFromFile(file, Path.GetFileName(file), HistoryCompressionLevel);
-
-						File.Delete(file);
-					}
-				}
-
+				CompressHistoryFiles(files, isDir);
 				break;
-			}
 			case FileLogHistoryPolicies.Move:
-			{
-				try
-				{
-					Directory.CreateDirectory(HistoryMove);
-
-					if (isDir)
-					{
-						foreach (var srcDir in files)
-							Directory.Move(srcDir, Path.Combine(HistoryMove, Path.GetFileName(srcDir)));
-					}
-					else
-					{
-						foreach (var file in files)
-							File.Move(file, Path.Combine(HistoryMove, Path.GetFileName(file)));
-					}
-				}
-				catch (Exception ex)
-				{
-					Trace.WriteLine(ex);
-				}
-
+				MoveHistoryFiles(files, isDir);
 				break;
+		}
+	}
+
+	private static void DeleteHistoryFiles(List<string> files, bool isDir)
+	{
+		foreach (var file in files)
+		{
+			if (isDir)
+				Directory.Delete(file, true);
+			else
+				File.Delete(file);
+		}
+	}
+
+	private void CompressHistoryFiles(List<string> files, bool isDir)
+	{
+		foreach (var file in files)
+		{
+			if (isDir)
+			{
+				ZipFile.CreateFromDirectory(file, Path.Combine(LogDirectory, $"{Path.GetFileName(file)}.zip"), HistoryCompressionLevel, false);
+				Directory.Delete(file, true);
 			}
+			else
+			{
+				using (var zipFile = new ZipArchive(File.Open(Path.Combine(LogDirectory, $"{Path.GetFileNameWithoutExtension(file)}.zip"), FileMode.Create), ZipArchiveMode.Create))
+					zipFile.CreateEntryFromFile(file, Path.GetFileName(file), HistoryCompressionLevel);
+
+				File.Delete(file);
+			}
+		}
+	}
+
+	private void MoveHistoryFiles(List<string> files, bool isDir)
+	{
+		try
+		{
+			Directory.CreateDirectory(HistoryMove);
+
+			foreach (var file in files)
+			{
+				var destPath = Path.Combine(HistoryMove, Path.GetFileName(file));
+
+				if (isDir)
+					Directory.Move(file, destPath);
+				else
+					File.Move(file, destPath);
+			}
+		}
+		catch (Exception ex)
+		{
+			Trace.WriteLine(ex);
 		}
 	}
 
 	/// <inheritdoc />
 	protected override void OnWriteMessages(IEnumerable<LogMessage> messages)
 	{
+		if (IsDisposed)
+			return;
+
 		if (!_triedHistoryPolicy)
 		{
 			TryDoHistoryPolicy();
@@ -467,7 +485,7 @@ public class FileLogListener : LogListener
 
 		foreach (var group in messages.GroupBy(m =>
 		{
-			if (isDisposing || m.IsDispose)
+			if (m.IsDispose)
 			{
 				isDisposing = true;
 				return null;
@@ -482,9 +500,6 @@ public class FileLogListener : LogListener
 
 			if (!_writers.TryGetValue(key, out var writer))
 			{
-				if (isDisposing)
-					return null;
-
 				if (_writers.Count > 0 && date != default)
 				{
 					var outOfDate = _writers.Where(p => p.Key.Item2 < date).ToArray();
@@ -505,15 +520,13 @@ public class FileLogListener : LogListener
 			prevFileName = fileName;
 			prevWriter = writer;
 			return writer;
-		}).AsParallel())
+		}))
 		{
-			if (isDisposing)
-			{
-				Dispose();
-				return;
-			}
-
 			var writer = group.Key;
+
+			// dispose message
+			if (writer is null)
+				continue;
 
 			try
 			{
@@ -562,6 +575,9 @@ public class FileLogListener : LogListener
 				writer.Flush();
 			}
 		}
+
+		if (isDisposing)
+			Dispose();
 	}
 
 	private static string GetRollingFileName(string fileName, int index)
@@ -585,6 +601,103 @@ public class FileLogListener : LogListener
 		return name;
 	}
 
+#if NET6_0_OR_GREATER
+	private void WriteMessage(TextWriter writer, LogMessage message)
+	{
+		var includeDateInLog = SeparateByDates == SeparateByDateModes.None;
+		var bufferSize = (includeDateInLog ? (_maxDateChars + 1) : 0) + _maxTimeChars;
+
+		Span<char> timeChars = stackalloc char[bufferSize];
+		var length = FormatDateTime(timeChars, ConvertToLocalTime(message.TimeUtc), includeDateInLog);
+
+		writer.Write(timeChars[..length]);
+		writer.Write('|');
+
+		var level = message.Level;
+		if (level != LogLevels.Info)
+		{
+			Span<char> levelBuffer = stackalloc char[7];
+			var levelStr = level.ToString();
+
+			levelStr.AsSpan().CopyTo(levelBuffer);
+
+			for (var i = levelStr.Length; i < 7; i++)
+				levelBuffer[i] = ' ';
+
+			writer.Write(levelBuffer);
+		}
+		else
+		{
+			writer.Write("       ");
+		}
+
+		writer.Write('|');
+		WritePadded(writer, message.Source.Name, 10);
+		writer.Write('|');
+
+		if (WriteSourceId)
+		{
+			WritePadded(writer, message.Source.Id.ToString(), 20);
+			writer.Write('|');
+		}
+
+		writer.WriteLine(message.Message);
+	}
+
+	private static void WritePadded(TextWriter writer, string value, int width)
+	{
+		writer.Write(value);
+		var padding = width - value.Length;
+		for (var i = 0; i < padding; i++)
+			writer.Write(' ');
+	}
+
+	private static int FormatDateTime(Span<char> buffer, DateTime time, bool includeDate)
+	{
+		var offset = 0;
+
+		if (includeDate)
+		{
+			var year = time.Year;
+			var month = time.Month;
+			var day = time.Day;
+
+			buffer[0] = _digitChars[year / 1000];
+			buffer[1] = _digitChars[year % 1000 / 100];
+			buffer[2] = _digitChars[year % 100 / 10];
+			buffer[3] = _digitChars[year % 10];
+			buffer[4] = '/';
+			buffer[5] = _digitChars[month / 10];
+			buffer[6] = _digitChars[month % 10];
+			buffer[7] = '/';
+			buffer[8] = _digitChars[day / 10];
+			buffer[9] = _digitChars[day % 10];
+			buffer[10] = ' ';
+
+			offset = _maxDateChars + 1;
+		}
+
+		var hour = time.Hour;
+		var minute = time.Minute;
+		var second = time.Second;
+		var millisecond = time.Millisecond;
+
+		buffer[offset + 0] = _digitChars[hour / 10];
+		buffer[offset + 1] = _digitChars[hour % 10];
+		buffer[offset + 2] = ':';
+		buffer[offset + 3] = _digitChars[minute / 10];
+		buffer[offset + 4] = _digitChars[minute % 10];
+		buffer[offset + 5] = ':';
+		buffer[offset + 6] = _digitChars[second / 10];
+		buffer[offset + 7] = _digitChars[second % 10];
+		buffer[offset + 8] = '.';
+		buffer[offset + 9] = _digitChars[millisecond / 100];
+		buffer[offset + 10] = _digitChars[millisecond % 100 / 10];
+		buffer[offset + 11] = _digitChars[millisecond % 10];
+
+		return offset + _maxTimeChars;
+	}
+#else
 	private void WriteMessage(TextWriter writer, LogMessage message)
 	{
 		writer.Write(ToFastDateCharArray(ConvertToLocalTime(message.TimeUtc)));
@@ -608,7 +721,7 @@ public class FileLogListener : LogListener
 	{
 		var includeDateInLog = SeparateByDates == SeparateByDateModes.None;
 
-		var timeChars = new char[12 + (includeDateInLog ? 11 : 0)];
+		var timeChars = new char[_maxTimeChars + (includeDateInLog ? (_maxDateChars + 1) : 0)];
 
 		var offset = 0;
 
@@ -630,7 +743,7 @@ public class FileLogListener : LogListener
 			timeChars[9] = _digitChars[day % 10];
 			timeChars[10] = ' ';
 
-			offset = 11;
+			offset = _maxDateChars + 1;
 		}
 
 		var hour = time.Hour;
@@ -653,6 +766,7 @@ public class FileLogListener : LogListener
 
 		return timeChars;
 	}
+#endif
 
 	/// <inheritdoc />
 	public override void Load(SettingsStorage storage)
@@ -681,21 +795,23 @@ public class FileLogListener : LogListener
 	{
 		base.Save(storage);
 
-		storage.SetValue(nameof(FileName), FileName);
-		storage.SetValue(nameof(MaxLength), MaxLength);
-		storage.SetValue(nameof(MaxCount), MaxCount);
-		storage.SetValue(nameof(Append), Append);
-		storage.SetValue(nameof(LogDirectory), LogDirectory);
-		storage.SetValue(nameof(WriteChildDataToRootFile), WriteChildDataToRootFile);
-		storage.SetValue(nameof(Extension), Extension);
-		storage.SetValue(nameof(WriteSourceId), WriteSourceId);
-		storage.SetValue(nameof(DirectoryDateFormat), DirectoryDateFormat);
-		storage.SetValue(nameof(SeparateByDates), SeparateByDates.To<string>());
+		storage
+			.Set(nameof(FileName), FileName)
+			.Set(nameof(MaxLength), MaxLength)
+			.Set(nameof(MaxCount), MaxCount)
+			.Set(nameof(Append), Append)
+			.Set(nameof(LogDirectory), LogDirectory)
+			.Set(nameof(WriteChildDataToRootFile), WriteChildDataToRootFile)
+			.Set(nameof(Extension), Extension)
+			.Set(nameof(WriteSourceId), WriteSourceId)
+			.Set(nameof(DirectoryDateFormat), DirectoryDateFormat)
+			.Set(nameof(SeparateByDates), SeparateByDates.To<string>())
 
-		storage.SetValue(nameof(HistoryPolicy), HistoryPolicy);
-		storage.SetValue(nameof(HistoryAfter), HistoryAfter);
-		storage.SetValue(nameof(HistoryMove), HistoryMove);
-		storage.SetValue(nameof(HistoryCompressionLevel), HistoryCompressionLevel);
+			.Set(nameof(HistoryPolicy), HistoryPolicy)
+			.Set(nameof(HistoryAfter), HistoryAfter)
+			.Set(nameof(HistoryMove), HistoryMove)
+			.Set(nameof(HistoryCompressionLevel), HistoryCompressionLevel)
+		;
 	}
 
 	/// <summary>
