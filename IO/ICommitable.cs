c4cce23f09356cdba4a4eea524fc21394566afe5
mika@stocksharp.com <mika@stocksharp.com>
2026-01-03 00:20:29 +0300

    ICommitable interface. CsvFileWriter/Reader BaseStream property.

---
+++ IO/ICommitable.cs
@@ -0,0 +1,12 @@
+namespace Ecng.IO;
+
+/// <summary>
+/// Represents an object that supports committing changes.
+/// </summary>
+public interface ICommitable
+{
+	/// <summary>
+	/// Commits the changes.
+	/// </summary>
+	void Commit();
+}
