4dc1feb328d9563ec2993f4d8445519193d8d110
mika@stocksharp.com <mika@stocksharp.com>
2026-01-04 02:25:06 +0300

    IDatabaseBatchInserter abstraction for database export operations.
    - IDatabaseBatchInserter<T> interface for batch insert/bulk copy
    - IDatabaseBatchInserterProvider factory interface
    - IDatabaseMappingBuilder<T> for table/column configuration
    - IDatabaseColumnBuilder<T> for column properties
    - DatabaseDataType enum for database types
    - Linq2dbBatchInserterProvider implementation
    - Integration tests with MSSQL

---
+++ Data/IDatabaseBatchInserter.cs
@@ -0,0 +1,242 @@
+namespace Ecng.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Provides batch insert operations for a database table.
+/// </summary>
+/// <typeparam name="T">The type of entities to insert.</typeparam>
+public interface IDatabaseBatchInserter<T> : IDisposable
+	where T : class
+{
+	/// <summary>
+	/// Inserts a single item into the database.
+	/// </summary>
+	/// <param name="item">The item to insert.</param>
+	/// <param name="cancellationToken">Cancellation token.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
+	Task InsertAsync(T item, CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Performs a bulk copy of multiple items into the database.
+	/// </summary>
+	/// <param name="items">The items to insert.</param>
+	/// <param name="cancellationToken">Cancellation token.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
+	Task BulkCopyAsync(IEnumerable<T> items, CancellationToken cancellationToken);
+}
+
+/// <summary>
+/// Factory for creating database batch inserters.
+/// </summary>
+public interface IDatabaseBatchInserterProvider
+{
+	/// <summary>
+	/// Creates a batch inserter for the specified entity type.
+	/// </summary>
+	/// <typeparam name="T">The type of entities to insert.</typeparam>
+	/// <param name="connection">Database connection settings.</param>
+	/// <param name="tableName">Name of the target table.</param>
+	/// <param name="configureMapping">Callback to configure table mapping.</param>
+	/// <returns>A batch inserter instance.</returns>
+	IDatabaseBatchInserter<T> Create<T>(
+		DatabaseConnectionPair connection,
+		string tableName,
+		Action<IDatabaseMappingBuilder<T>> configureMapping)
+		where T : class;
+}
+
+/// <summary>
+/// Builder for configuring database table mappings.
+/// </summary>
+/// <typeparam name="T">The entity type.</typeparam>
+public interface IDatabaseMappingBuilder<T>
+	where T : class
+{
+	/// <summary>
+	/// Sets the table name.
+	/// </summary>
+	/// <param name="name">Table name.</param>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseMappingBuilder<T> HasTableName(string name);
+
+	/// <summary>
+	/// Marks all columns as required by default.
+	/// </summary>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseMappingBuilder<T> IsColumnRequired();
+
+	/// <summary>
+	/// Configures a property mapping.
+	/// </summary>
+	/// <typeparam name="TProperty">The property type.</typeparam>
+	/// <param name="propertyExpression">Expression selecting the property.</param>
+	/// <returns>A column builder for further configuration.</returns>
+	IDatabaseColumnBuilder<T> Property<TProperty>(Expression<Func<T, TProperty>> propertyExpression);
+
+	/// <summary>
+	/// Configures a dynamic property by name.
+	/// </summary>
+	/// <param name="propertyName">The property name.</param>
+	/// <returns>A column builder for further configuration.</returns>
+	IDatabaseColumnBuilder<T> DynamicProperty(string propertyName);
+
+	/// <summary>
+	/// Sets dynamic property accessors for reading and writing dynamic properties.
+	/// </summary>
+	/// <param name="getter">Expression to get a dynamic property value.</param>
+	/// <param name="setter">Expression to set a dynamic property value.</param>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseMappingBuilder<T> DynamicPropertyAccessors(
+		Expression<Func<T, string, object, object>> getter,
+		Expression<Action<T, string, object>> setter);
+
+	/// <summary>
+	/// Registers a value converter.
+	/// </summary>
+	/// <typeparam name="TFrom">Source type.</typeparam>
+	/// <typeparam name="TTo">Target type.</typeparam>
+	/// <param name="converter">Conversion function.</param>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseMappingBuilder<T> SetConverter<TFrom, TTo>(Func<TFrom, TTo> converter);
+
+	/// <summary>
+	/// Sets a parameter value converter that transforms values before sending to database.
+	/// </summary>
+	/// <param name="converter">Function that converts parameter values.</param>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseMappingBuilder<T> SetParameterValueConverter(Func<object, object> converter);
+}
+
+/// <summary>
+/// Builder for configuring a database column.
+/// </summary>
+/// <typeparam name="T">The entity type.</typeparam>
+public interface IDatabaseColumnBuilder<T>
+	where T : class
+{
+	/// <summary>
+	/// Sets the maximum length for string columns.
+	/// </summary>
+	/// <param name="length">Maximum length.</param>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseColumnBuilder<T> HasLength(int length);
+
+	/// <summary>
+	/// Sets the scale for decimal columns.
+	/// </summary>
+	/// <param name="scale">Number of decimal places.</param>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseColumnBuilder<T> HasScale(int scale);
+
+	/// <summary>
+	/// Sets the column name.
+	/// </summary>
+	/// <param name="name">Column name.</param>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseColumnBuilder<T> HasColumnName(string name);
+
+	/// <summary>
+	/// Sets the database data type.
+	/// </summary>
+	/// <param name="dataType">The data type.</param>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseColumnBuilder<T> HasDataType(DatabaseDataType dataType);
+
+	/// <summary>
+	/// Marks the column as not nullable.
+	/// </summary>
+	/// <returns>The builder for chaining.</returns>
+	IDatabaseColumnBuilder<T> IsNotNull();
+
+	/// <summary>
+	/// Configures another property.
+	/// </summary>
+	/// <typeparam name="TProperty">The property type.</typeparam>
+	/// <param name="propertyExpression">Expression selecting the property.</param>
+	/// <returns>A column builder for the new property.</returns>
+	IDatabaseColumnBuilder<T> Property<TProperty>(Expression<Func<T, TProperty>> propertyExpression);
+
+	/// <summary>
+	/// Configures a dynamic property by name.
+	/// </summary>
+	/// <param name="propertyName">The property name.</param>
+	/// <returns>A column builder for further configuration.</returns>
+	IDatabaseColumnBuilder<T> DynamicProperty(string propertyName);
+
+	/// <summary>
+	/// Marks all columns as required by default.
+	/// </summary>
+	/// <returns>The mapping builder for chaining.</returns>
+	IDatabaseMappingBuilder<T> IsColumnRequired();
+
+	/// <summary>
+	/// Sets dynamic property accessors for reading and writing dynamic properties.
+	/// </summary>
+	/// <param name="getter">Expression to get a dynamic property value.</param>
+	/// <param name="setter">Expression to set a dynamic property value.</param>
+	/// <returns>The mapping builder for chaining.</returns>
+	IDatabaseMappingBuilder<T> DynamicPropertyAccessors(
+		Expression<Func<T, string, object, object>> getter,
+		Expression<Action<T, string, object>> setter);
+}
+
+/// <summary>
+/// Database data types.
+/// </summary>
+public enum DatabaseDataType
+{
+	/// <summary>
+	/// Variable-length Unicode string.
+	/// </summary>
+	NVarChar,
+
+	/// <summary>
+	/// Variable-length non-Unicode string.
+	/// </summary>
+	VarChar,
+
+	/// <summary>
+	/// Fixed-length string.
+	/// </summary>
+	Char,
+
+	/// <summary>
+	/// Integer.
+	/// </summary>
+	Int,
+
+	/// <summary>
+	/// Big integer.
+	/// </summary>
+	BigInt,
+
+	/// <summary>
+	/// Decimal number.
+	/// </summary>
+	Decimal,
+
+	/// <summary>
+	/// Date and time.
+	/// </summary>
+	DateTime,
+
+	/// <summary>
+	/// Boolean.
+	/// </summary>
+	Boolean,
+
+	/// <summary>
+	/// Binary data.
+	/// </summary>
+	Binary,
+
+	/// <summary>
+	/// Large text.
+	/// </summary>
+	Text,
+}
---
+++ Tests/Data/DatabaseBatchInserterTests.cs
@@ -0,0 +1,239 @@
+namespace Ecng.Tests.Data;
+
+using Ecng.Data;
+
+using LinqToDB;
+
+[TestClass]
+[TestCategory("Integration")]
+public class DatabaseBatchInserterTests : BaseTestClass
+{
+	private const string TestTableName = "ecng_batch_test";
+
+	private DatabaseConnectionPair GetSqlServerConnection()
+	{
+		return new DatabaseConnectionPair
+		{
+			Provider = ProviderName.SqlServer + ".MS",
+			ConnectionString = GetSecret("DB_MSSQL_CONNECTION_STRING"),
+		};
+	}
+
+	private static string GetUniqueTableName()
+		=> $"{TestTableName}_{Guid.NewGuid():N}";
+
+	[TestMethod]
+	public async Task InsertAsync_SingleItem_Success()
+	{
+		var provider = new Linq2dbBatchInserterProvider();
+		var connection = GetSqlServerConnection();
+		var tableName = GetUniqueTableName();
+
+		try
+		{
+			using var inserter = provider.Create<TestEntity>(
+				connection,
+				tableName,
+				b => ConfigureMapping(b, tableName));
+
+			var entity = new TestEntity
+			{
+				Id = 1,
+				Name = "Test Item",
+				Value = 123.45m,
+				CreatedAt = DateTime.UtcNow,
+			};
+
+			await inserter.InsertAsync(entity, CancellationToken);
+		}
+		finally
+		{
+			await DropTableAsync(connection, tableName);
+		}
+	}
+
+	[TestMethod]
+	public async Task BulkCopyAsync_MultipleItems_Success()
+	{
+		var provider = new Linq2dbBatchInserterProvider();
+		var connection = GetSqlServerConnection();
+		var tableName = GetUniqueTableName();
+
+		try
+		{
+			using var inserter = provider.Create<TestEntity>(
+				connection,
+				tableName,
+				b => ConfigureMapping(b, tableName));
+
+			var entities = Enumerable.Range(1, 100)
+				.Select(i => new TestEntity
+				{
+					Id = i,
+					Name = $"Item {i}",
+					Value = i * 1.5m,
+					CreatedAt = DateTime.UtcNow.AddMinutes(-i),
+				})
+				.ToList();
+
+			await inserter.BulkCopyAsync(entities, CancellationToken);
+		}
+		finally
+		{
+			await DropTableAsync(connection, tableName);
+		}
+	}
+
+	[TestMethod]
+	public async Task BulkCopyAsync_LargeDataset_Success()
+	{
+		var provider = new Linq2dbBatchInserterProvider();
+		var connection = GetSqlServerConnection();
+		var tableName = GetUniqueTableName();
+
+		try
+		{
+			using var inserter = provider.Create<TestEntity>(
+				connection,
+				tableName,
+				b => ConfigureMapping(b, tableName));
+
+			var entities = Enumerable.Range(1, 10000)
+				.Select(i => new TestEntity
+				{
+					Id = i,
+					Name = $"Bulk Item {i}",
+					Value = i * 0.01m,
+					CreatedAt = DateTime.UtcNow,
+				})
+				.ToList();
+
+			await inserter.BulkCopyAsync(entities, CancellationToken);
+		}
+		finally
+		{
+			await DropTableAsync(connection, tableName);
+		}
+	}
+
+	[TestMethod]
+	public async Task Create_WithDynamicProperties_Success()
+	{
+		var provider = new Linq2dbBatchInserterProvider();
+		var connection = GetSqlServerConnection();
+		var tableName = GetUniqueTableName();
+
+		try
+		{
+			using var inserter = provider.Create<DynamicTestEntity>(
+				connection,
+				tableName,
+				b => ConfigureDynamicMapping(b, tableName));
+
+			var entity = new DynamicTestEntity
+			{
+				Id = 1,
+				Name = "Dynamic Test",
+			};
+			entity.SetDynamic("CustomField", "CustomValue");
+
+			await inserter.InsertAsync(entity, CancellationToken);
+		}
+		finally
+		{
+			await DropTableAsync(connection, tableName);
+		}
+	}
+
+	[TestMethod]
+	public void Create_NullConnection_ThrowsArgumentNullException()
+	{
+		var provider = new Linq2dbBatchInserterProvider();
+
+		Throws<ArgumentNullException>(() =>
+			provider.Create<TestEntity>(null, "table", _ => { }));
+	}
+
+	[TestMethod]
+	public void Create_EmptyTableName_ThrowsArgumentNullException()
+	{
+		var provider = new Linq2dbBatchInserterProvider();
+		var connection = GetSqlServerConnection();
+
+		Throws<ArgumentNullException>(() =>
+			provider.Create<TestEntity>(connection, "", _ => { }));
+	}
+
+	[TestMethod]
+	public void Create_NullConfigureMapping_ThrowsArgumentNullException()
+	{
+		var provider = new Linq2dbBatchInserterProvider();
+		var connection = GetSqlServerConnection();
+
+		Throws<ArgumentNullException>(() =>
+			provider.Create<TestEntity>(connection, "table", null));
+	}
+
+	private static void ConfigureMapping(IDatabaseMappingBuilder<TestEntity> builder, string tableName)
+	{
+		builder
+			.HasTableName(tableName)
+			.IsColumnRequired()
+			.Property(e => e.Id).HasColumnName("id").HasDataType(DatabaseDataType.Int)
+			.Property(e => e.Name).HasColumnName("name").HasLength(100).HasDataType(DatabaseDataType.NVarChar)
+			.Property(e => e.Value).HasColumnName("value").HasScale(2).HasDataType(DatabaseDataType.Decimal)
+			.Property(e => e.CreatedAt).HasColumnName("created_at").HasDataType(DatabaseDataType.DateTime);
+	}
+
+	private static void ConfigureDynamicMapping(IDatabaseMappingBuilder<DynamicTestEntity> builder, string tableName)
+	{
+		builder
+			.HasTableName(tableName)
+			.IsColumnRequired()
+			.Property(e => e.Id).HasColumnName("id").HasDataType(DatabaseDataType.Int)
+			.Property(e => e.Name).HasColumnName("name").HasLength(100).HasDataType(DatabaseDataType.NVarChar)
+			.DynamicProperty("CustomField").HasColumnName("custom_field").HasLength(200).HasDataType(DatabaseDataType.NVarChar)
+			.DynamicPropertyAccessors(
+				(e, name, def) => e.GetDynamic(name) ?? def,
+				(e, name, value) => e.SetDynamic(name, value));
+	}
+
+	private static Task DropTableAsync(DatabaseConnectionPair connection, string tableName)
+	{
+		try
+		{
+			using var db = connection.CreateConnection();
+			using var cmd = db.CreateCommand();
+			cmd.CommandText = $"IF OBJECT_ID('{tableName}', 'U') IS NOT NULL DROP TABLE [{tableName}]";
+			cmd.ExecuteNonQuery();
+		}
+		catch
+		{
+			// Ignore cleanup errors
+		}
+
+		return Task.CompletedTask;
+	}
+
+	public class TestEntity
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public decimal Value { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+
+	public class DynamicTestEntity
+	{
+		private readonly Dictionary<string, object> _dynamicProperties = new();
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public object GetDynamic(string name)
+			=> _dynamicProperties.TryGetValue(name, out var value) ? value : null;
+
+		public void SetDynamic(string name, object value)
+			=> _dynamicProperties[name] = value;
+	}
+}
