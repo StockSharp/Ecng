08c6019ca94ecd4a2fa32a5ca1a38928b8069672
mika@stocksharp.com <mika@stocksharp.com>
2026-01-04 12:38:43 +0300

    Code cleanup.

--- Data.Linq2db/DatabaseProviderRegistry.cs
+++ Data.Linq2db/DatabaseProviderRegistry.cs
@@ -1,28 +1,12 @@
 ﻿namespace Ecng.Data;
 
-using System;
-
 using LinqToDB;
 
 /// <summary>
 /// Provides a registry for database providers and verifies database connections.
 /// </summary>
-/// <remarks>
-/// Initializes a new instance of the <see cref="DatabaseProviderRegistry"/> class.
-/// </remarks>
-/// <param name="batchInserterProvider">The batch inserter provider to use for verification.</param>
-public class DatabaseProviderRegistry(IDatabaseBatchInserterProvider batchInserterProvider) : IDatabaseProviderRegistry
+public class DatabaseProviderRegistry : IDatabaseProviderRegistry
 {
-	private readonly IDatabaseBatchInserterProvider _batchInserterProvider = batchInserterProvider ?? throw new ArgumentNullException(nameof(batchInserterProvider));
-
-	/// <summary>
-	/// Initializes a new instance of the <see cref="DatabaseProviderRegistry"/> class.
-	/// </summary>
-	public DatabaseProviderRegistry()
-		: this(new Linq2dbBatchInserterProvider())
-	{
-	}
-
 	/// <summary>
 	/// Gets the list of available database providers.
 	/// </summary>
