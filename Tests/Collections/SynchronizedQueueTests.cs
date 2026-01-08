f0eb895957a6b0495b420dd3a8069125c3b3f5c2
mika@stocksharp.com <mika@stocksharp.com>
2026-01-09 01:13:29 +0300

    Add comprehensive tests for SynchronizedStack and SynchronizedQueue

--- Tests/Collections/SynchronizedQueueTests.cs
+++ Tests/Collections/SynchronizedQueueTests.cs
@@ -3,6 +3,8 @@ namespace Ecng.Tests.Collections;
 [TestClass]
 public class SynchronizedQueueTests : BaseTestClass
 {
+	#region Basic Operations
+
 	[TestMethod]
 	public void BasicOperations()
 	{
@@ -33,4 +35,235 @@ public class SynchronizedQueueTests : BaseTestClass
 		ThrowsExactly<NotSupportedException>(() => list.Insert(0, 1));
 		ThrowsExactly<NotSupportedException>(() => list.RemoveAt(0));
 	}
+
+	#endregion
+
+	#region Count and Clear
+
+	[TestMethod]
+	public void Count_ReturnsCorrectValue()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Count.AssertEqual(0);
+
+		q.Enqueue(1);
+		q.Count.AssertEqual(1);
+
+		q.Enqueue(2);
+		q.Enqueue(3);
+		q.Count.AssertEqual(3);
+
+		q.Dequeue();
+		q.Count.AssertEqual(2);
+	}
+
+	[TestMethod]
+	public void Clear_RemovesAllElements()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Enqueue(1);
+		q.Enqueue(2);
+		q.Enqueue(3);
+		q.Count.AssertEqual(3);
+
+		q.Clear();
+
+		q.Count.AssertEqual(0);
+	}
+
+	#endregion
+
+	#region Contains
+
+	[TestMethod]
+	public void Contains_FindsExistingElement()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Enqueue(10);
+		q.Enqueue(20);
+		q.Enqueue(30);
+
+		q.Contains(20).AssertTrue();
+		q.Contains(10).AssertTrue();
+		q.Contains(30).AssertTrue();
+	}
+
+	[TestMethod]
+	public void Contains_ReturnsFalseForMissingElement()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Enqueue(10);
+		q.Enqueue(20);
+
+		q.Contains(99).AssertFalse();
+	}
+
+	#endregion
+
+	#region ToArray / CopyTo / CopyAndClear
+
+	[TestMethod]
+	public void ToArray_ReturnsAllElements()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Enqueue(1);
+		q.Enqueue(2);
+		q.Enqueue(3);
+
+		var array = q.ToArray();
+
+		array.Length.AssertEqual(3);
+		// Queue returns elements in FIFO order
+		array[0].AssertEqual(1);
+		array[1].AssertEqual(2);
+		array[2].AssertEqual(3);
+	}
+
+	[TestMethod]
+	public void ToArray_EmptyQueue_ReturnsEmptyArray()
+	{
+		var q = new SynchronizedQueue<int>();
+
+		var array = q.ToArray();
+
+		array.Length.AssertEqual(0);
+	}
+
+	[TestMethod]
+	public void CopyTo_CopiesAllElements()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Enqueue(1);
+		q.Enqueue(2);
+		q.Enqueue(3);
+
+		var array = new int[3];
+		q.CopyTo(array, 0);
+
+		array[0].AssertEqual(1);
+		array[1].AssertEqual(2);
+		array[2].AssertEqual(3);
+	}
+
+	[TestMethod]
+	public void CopyTo_WithOffset()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Enqueue(1);
+		q.Enqueue(2);
+
+		var array = new int[5];
+		q.CopyTo(array, 2);
+
+		array[0].AssertEqual(0);
+		array[1].AssertEqual(0);
+		array[2].AssertEqual(1);
+		array[3].AssertEqual(2);
+		array[4].AssertEqual(0);
+	}
+
+	[TestMethod]
+	public void CopyAndClear_ReturnsAllElementsAndClears()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Enqueue(1);
+		q.Enqueue(2);
+		q.Enqueue(3);
+
+		var array = q.CopyAndClear();
+
+		array.Length.AssertEqual(3);
+		q.Count.AssertEqual(0);
+	}
+
+	#endregion
+
+	#region ICollection.Add
+
+	[TestMethod]
+	public void ICollection_Add_EnqueuesElement()
+	{
+		var q = new SynchronizedQueue<int>();
+		var collection = (ICollection<int>)q;
+
+		collection.Add(10);
+		collection.Add(20);
+
+		q.Count.AssertEqual(2);
+		q.Peek().AssertEqual(10); // FIFO - first added is first out
+	}
+
+	#endregion
+
+	#region Enumeration
+
+	[TestMethod]
+	public void Enumeration_IteratesInFifoOrder()
+	{
+		var q = new SynchronizedQueue<int>();
+		q.Enqueue(1);
+		q.Enqueue(2);
+		q.Enqueue(3);
+
+		var result = new List<int>();
+		foreach (var item in q)
+			result.Add(item);
+
+		result.Count.AssertEqual(3);
+		result[0].AssertEqual(1);
+		result[1].AssertEqual(2);
+		result[2].AssertEqual(3);
+	}
+
+	#endregion
+
+	#region TryDequeue / TryPeek extension methods
+
+	[TestMethod]
+	public void TryDequeue_ReturnsElementIfExists()
+	{
+		var q = new SynchronizedQueue<string>();
+		q.Enqueue("first");
+		q.Enqueue("second");
+
+		var result = q.TryDequeue();
+
+		result.AssertEqual("first");
+		q.Count.AssertEqual(1);
+	}
+
+	[TestMethod]
+	public void TryDequeue_ReturnsDefaultIfEmpty()
+	{
+		var q = new SynchronizedQueue<string>();
+
+		var result = q.TryDequeue();
+
+		result.AssertEqual(default);
+	}
+
+	[TestMethod]
+	public void TryPeek_ReturnsElementIfExists()
+	{
+		var q = new SynchronizedQueue<string>();
+		q.Enqueue("first");
+		q.Enqueue("second");
+
+		var result = q.TryPeek();
+
+		result.AssertEqual("first");
+		q.Count.AssertEqual(2); // does not remove
+	}
+
+	[TestMethod]
+	public void TryPeek_ReturnsDefaultIfEmpty()
+	{
+		var q = new SynchronizedQueue<string>();
+
+		var result = q.TryPeek();
+
+		result.AssertEqual(default);
+	}
+
+	#endregion
 }
--- Tests/Collections/SynchronizedStackTests.cs
+++ Tests/Collections/SynchronizedStackTests.cs
@@ -3,6 +3,8 @@ namespace Ecng.Tests.Collections;
 [TestClass]
 public class SynchronizedStackTests : BaseTestClass
 {
+	#region Basic Operations
+
 	[TestMethod]
 	public void BasicOperations()
 	{
@@ -35,4 +37,185 @@ public class SynchronizedStackTests : BaseTestClass
 		ThrowsExactly<NotSupportedException>(() => list.Insert(0, 1));
 		ThrowsExactly<NotSupportedException>(() => list.RemoveAt(0));
 	}
+
+	#endregion
+
+	#region Count and Clear
+
+	[TestMethod]
+	public void Count_ReturnsCorrectValue()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Count.AssertEqual(0);
+
+		st.Push(1);
+		st.Count.AssertEqual(1);
+
+		st.Push(2);
+		st.Push(3);
+		st.Count.AssertEqual(3);
+
+		st.Pop();
+		st.Count.AssertEqual(2);
+	}
+
+	[TestMethod]
+	public void Clear_RemovesAllElements()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Push(1);
+		st.Push(2);
+		st.Push(3);
+		st.Count.AssertEqual(3);
+
+		st.Clear();
+
+		st.Count.AssertEqual(0);
+	}
+
+	#endregion
+
+	#region Contains
+
+	[TestMethod]
+	public void Contains_FindsExistingElement()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Push(10);
+		st.Push(20);
+		st.Push(30);
+
+		st.Contains(20).AssertTrue();
+		st.Contains(10).AssertTrue();
+		st.Contains(30).AssertTrue();
+	}
+
+	[TestMethod]
+	public void Contains_ReturnsFalseForMissingElement()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Push(10);
+		st.Push(20);
+
+		st.Contains(99).AssertFalse();
+	}
+
+	#endregion
+
+	#region ToArray / CopyTo / CopyAndClear
+
+	[TestMethod]
+	public void ToArray_ReturnsAllElements()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Push(1);
+		st.Push(2);
+		st.Push(3);
+
+		var array = st.ToArray();
+
+		array.Length.AssertEqual(3);
+		// Stack returns elements in LIFO order
+		array[0].AssertEqual(3);
+		array[1].AssertEqual(2);
+		array[2].AssertEqual(1);
+	}
+
+	[TestMethod]
+	public void ToArray_EmptyStack_ReturnsEmptyArray()
+	{
+		var st = new SynchronizedStack<int>();
+
+		var array = st.ToArray();
+
+		array.Length.AssertEqual(0);
+	}
+
+	[TestMethod]
+	public void CopyTo_CopiesAllElements()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Push(1);
+		st.Push(2);
+		st.Push(3);
+
+		var array = new int[3];
+		st.CopyTo(array, 0);
+
+		array[0].AssertEqual(3);
+		array[1].AssertEqual(2);
+		array[2].AssertEqual(1);
+	}
+
+	[TestMethod]
+	public void CopyTo_WithOffset()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Push(1);
+		st.Push(2);
+
+		var array = new int[5];
+		st.CopyTo(array, 2);
+
+		array[0].AssertEqual(0);
+		array[1].AssertEqual(0);
+		array[2].AssertEqual(2);
+		array[3].AssertEqual(1);
+		array[4].AssertEqual(0);
+	}
+
+	[TestMethod]
+	public void CopyAndClear_ReturnsAllElementsAndClears()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Push(1);
+		st.Push(2);
+		st.Push(3);
+
+		var array = st.CopyAndClear();
+
+		array.Length.AssertEqual(3);
+		st.Count.AssertEqual(0);
+	}
+
+	#endregion
+
+	#region ICollection.Add
+
+	[TestMethod]
+	public void ICollection_Add_PushesElement()
+	{
+		var st = new SynchronizedStack<int>();
+		var collection = (ICollection<int>)st;
+
+		collection.Add(10);
+		collection.Add(20);
+
+		st.Count.AssertEqual(2);
+		st.Peek().AssertEqual(20);
+	}
+
+	#endregion
+
+	#region Enumeration
+
+	[TestMethod]
+	public void Enumeration_IteratesInLifoOrder()
+	{
+		var st = new SynchronizedStack<int>();
+		st.Push(1);
+		st.Push(2);
+		st.Push(3);
+
+		var result = new List<int>();
+		foreach (var item in st)
+			result.Add(item);
+
+		result.Count.AssertEqual(3);
+		result[0].AssertEqual(3);
+		result[1].AssertEqual(2);
+		result[2].AssertEqual(1);
+	}
+
+	#endregion
 }
